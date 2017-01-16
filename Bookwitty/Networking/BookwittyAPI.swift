e780a5ead4024dddf13d1f69ac58b79461ac9982
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-01-16 12:43:07 +0200

    Create 'BookwittyAPI' file that will contain the developed API endpoints

---
+++ Bookwitty/Networking/BookwittyAPI.swift
@@ -0,0 +1,13 @@
+//
+//  BookwittyAPI.swift
+//  Bookwitty
+//
+//  Created by Marwan  on 1/16/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+public enum BookwittyAPI {
+
+}
