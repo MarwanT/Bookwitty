0dec14dd8c4787e8fc5452a4a9fe200a30f0b8b5
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-01-18 18:23:00 +0200

    Create the Objective-C bridging header

---
+++ Bookwitty/Bookwitty-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Bookwitty-Bridging-Header.h
+//  Bookwitty
+//
+//  Created by Marwan  on 1/18/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+#ifndef Bookwitty_Bridging_Header_h
+#define Bookwitty_Bridging_Header_h
+
+
+#endif /* Bookwitty_Bridging_Header_h */
