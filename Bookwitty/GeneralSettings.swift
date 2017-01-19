0ad2c848ef69091a2598ac1d4bedf5aa7b8946df
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-01-19 12:36:17 +0200

    Create app general settings class

---
+++ Bookwitty/GeneralSettings.swift
@@ -0,0 +1,12 @@
+//
+//  GeneralSettings.swift
+//  Bookwitty
+//
+//  Created by Marwan  on 1/19/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+public class GeneralSettings {
+}
