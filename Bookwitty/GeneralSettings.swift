5851e316cb35ff2c3271933c736ef2e6ce976444
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-01-19 13:34:05 +0200

    Add property to general settings to retrieving and setting the user preference regarding sending usage data

--- Bookwitty/GeneralSettings.swift
+++ Bookwitty/GeneralSettings.swift
@@ -15,8 +15,21 @@ public class GeneralSettings {
   public struct Keys {
     public static let SendUsageData = "SendUsageData"
   }
+
+  private let defaults = UserDefaults.standard
+  private let defaultValues: [String:Any] = [
+    Keys.SendUsageData : true
+  ]
   
   public static let sharedInstance: GeneralSettings = GeneralSettings()
   private init() {
+    defaults.register(defaults: defaultValues)
+    shouldSendUsageData = defaults.bool(forKey: Keys.SendUsageData)
+  }
+  
+  public var shouldSendUsageData: Bool {
+    didSet {
+      defaults.set(self.shouldSendUsageData, forKey: Keys.SendUsageData)
+    }
   }
 }
