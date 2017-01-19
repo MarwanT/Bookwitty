8bb875fe36b75411017ff9fca08350c991322a85
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-01-19 14:05:04 +0200

    Initialize Google Analytics in the analytics layer

--- Bookwitty/Utilities/Analytics/Analytics.swift
+++ Bookwitty/Utilities/Analytics/Analytics.swift
@@ -9,6 +9,9 @@
 import Foundation
 
 public final class Analytics {
+
+  fileprivate let dispatchInterval: TimeInterval = 20
+  
   struct Event {
     var category: String
     var action: String
@@ -16,12 +19,40 @@ public final class Analytics {
     var value: Double
   }
   
+  var enabled: Bool {
+    return GeneralSettings.sharedInstance.shouldSendUsageData
+  }
+  
   public static let sharedInstance: Analytics = Analytics()
   private init() {
+    self.initializeGoogleAnalytics()
   }
   func send(event: Event) {
   }
   
   func send(screenName: String) {
+  }
+}
+
+// MARK: - Google Analytics
+
+extension Analytics {
+  private var trackingIdentifier: String {
+    // TODO: Retrieve the value from cocoapods keys
+    return "TRACKING_IDENTIFIER"
+  }
+  
+  fileprivate func initializeGoogleAnalytics() {
+    GAI.sharedInstance().tracker(withTrackingId: self.trackingIdentifier)
+    GAI.sharedInstance().trackUncaughtExceptions = true
+    GAI.sharedInstance().dispatchInterval = self.dispatchInterval
+    GAI.sharedInstance().optOut = !self.enabled
+    
+    switch Environment.current {
+    case .localDevice, .mock, .preproduction:
+      GAI.sharedInstance().logger.logLevel = GAILogLevel.verbose
+    default:
+      GAI.sharedInstance().logger.logLevel = GAILogLevel.none
+    }
   }
 }
