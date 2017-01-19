3f558bbceef869f98e510f90d71041b5f0453583
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-01-19 13:41:25 +0200

    Create an empty ‘analytics’ layer
    this layer role is managing analytic related logic across the app and the potentially used tools

---
+++ Bookwitty/Utilities/Analytics/Analytics.swift
@@ -0,0 +1,13 @@
+//
+//  Analytics.swift
+//  Bookwitty
+//
+//  Created by Marwan on 1/18/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+public final class Analytics {
+
+}
