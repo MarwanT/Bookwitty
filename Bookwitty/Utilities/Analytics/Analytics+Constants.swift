71af76d072605fc77e6f862cc5635d22bb632d72
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-01-19 14:20:45 +0200

    Create categories and actions structures for holding desired data once defined

---
+++ Bookwitty/Utilities/Analytics/Analytics+Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Analytics+Constants.swift
+//  Bookwitty
+//
+//  Created by Marwan on 1/18/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+extension Analytics {
+  struct Category {
+  }
+  
+  struct Action {
+  }
+}
