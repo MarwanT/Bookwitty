e054b4e389e6a5a7392bab7180846842d14b0135
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-01-20 17:26:11 +0200

    Retrieve bookwitty server base url from app keys

--- Bookwitty/Networking/Environment.swift
+++ Bookwitty/Networking/Environment.swift
@@ -23,16 +23,8 @@ public enum Environment {
   
   var baseURL: URL {
     switch self {
-    case .localDevice:
-      return URL(string: "https://private-7bb9fa-dannyhajj.apiary-mock.com/api")!
-    case .mock:
-      return URL(string: "https://private-7bb9fa-dannyhajj.apiary-mock.com/api")!
-    case .preproduction:
-      // TODO: Set the correct Preproduction base URL
-      return URL(string: "https://private-7bb9fa-dannyhajj.apiary-mock.com/api")!
-    case .production:
-      // TODO: Set the correct Production base URL
-      return URL(string: "https://private-7bb9fa-dannyhajj.apiary-mock.com/api")!
+    case .localDevice, .mock, .preproduction, .production:
+      return URL(string: AppKeys.shared.bookwittyServerBaseURLAbsoluteString)!
     }
   }
 }
