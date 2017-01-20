5274f4dc5aa4c626fc3949e221b7918021c5cc7a
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-01-20 09:33:46 +0200

    Log the app activation event to facebook

--- Bookwitty/AppDelegate.swift
+++ Bookwitty/AppDelegate.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Fabric
 import Crashlytics
+import FacebookCore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -39,6 +40,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func applicationDidBecomeActive(_ application: UIApplication) {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    AppEventsLogger.activate(application)
   }
 
   func applicationWillTerminate(_ application: UIApplication) {
