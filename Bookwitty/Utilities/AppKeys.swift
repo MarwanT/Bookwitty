8443cf17547d7e21f82de3c215d882733b226c47
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-01-20 16:56:01 +0200

    Create app keys wrapper for cocoapods keys

---
+++ Bookwitty/Utilities/AppKeys.swift
@@ -0,0 +1,26 @@
+//
+//  Keys.swift
+//  Bookwitty
+//
+//  Created by Marwan  on 1/20/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+import Keys
+
+internal final class AppKeys {
+  let googleAnalyticsIdentifier: String
+  
+  private init(googleAnalyticsIdentifier: String) {
+    self.googleAnalyticsIdentifier = googleAnalyticsIdentifier
+  }
+  
+  private init(keys: BookwittyKeys) {
+    self.googleAnalyticsIdentifier = keys.bookwittyGoogleAnalyticsIdentifier
+  }
+  
+  private convenience init() {
+    self.init(keys: BookwittyKeys())
+  }
+}
