c1fd1a3c66b79a5c0586601cbb2b9dcdc1bfbeb2
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-01-20 10:00:02 +0200

    Connect facebook app delegate
    To post-process the results from actions that require you to switch to the native Facebook app or Safari, such as Facebook Login or Facebook Dialogs, you need to connect your AppDelegate class to the FBSDKApplicationDelegate object

--- Bookwitty/AppDelegate.swift
+++ Bookwitty/AppDelegate.swift
@@ -21,6 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // Override point for customization after application launch.
     Environment.initialize()
     Fabric.with([Crashlytics.self])
+    SDKApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
     return true
   }
 
@@ -45,6 +46,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func applicationWillTerminate(_ application: UIApplication) {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+  }
+  
+  func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+    let handled = SDKApplicationDelegate.shared.application(app, open: url, options: options)
+    return handled
   }
 }
 
