d14ed4d0512f5a9864e46fe3314feddea36e8d41
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-01-20 18:25:36 +0200

    Set bookwitty environment in app keys

--- Bookwitty/Utilities/AppKeys.swift
+++ Bookwitty/Utilities/AppKeys.swift
@@ -10,12 +10,14 @@ import Foundation
 import Keys
 
 internal final class AppKeys {
+  let environmentString: String
   let bookwittyServerBaseURLAbsoluteString: String
   let googleAnalyticsIdentifier: String
   
   static let shared = AppKeys()
   
   private init(keys: BookwittyKeys) {
+    self.environmentString = keys.bookwittyEnvironment
     self.bookwittyServerBaseURLAbsoluteString = keys.bookwittyGoogleAnalyticsIdentifier
     self.googleAnalyticsIdentifier = keys.bookwittyGoogleAnalyticsIdentifier
   }
