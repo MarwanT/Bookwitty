a8c008ba1a26365d225e464d71c0b60fac159c19
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-01-24 21:20:25 +0200

    Create introduction view model

---
+++ Bookwitty/ViewModels/IntroductionViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  IntroductionViewModel.swift
+//  Bookwitty
+//
+//  Created by Marwan  on 1/24/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+struct IntroductionViewModel {
+}
