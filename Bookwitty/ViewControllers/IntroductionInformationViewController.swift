7f761bd470a10de245c6925c651958fa03f7025d
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-01-24 12:22:46 +0200

    Create vc that displays info in the introduction vc

---
+++ Bookwitty/ViewControllers/IntroductionInformationViewController.swift
@@ -0,0 +1,23 @@
+//
+//  IntroductionInformationViewController.swift
+//  Bookwitty
+//
+//  Created by Marwan  on 1/24/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class IntroductionInformationViewController: UIPageViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+}
