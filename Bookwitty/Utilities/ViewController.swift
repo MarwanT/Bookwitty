e88bd986d5777fcbce3540c9c1a5946f9f6f0e04
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-01-24 20:56:57 +0200

    Develop method for wrapping the process of adding a child view controller

--- Bookwitty/Utilities/ViewController.swift
+++ Bookwitty/Utilities/ViewController.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+
+extension UIViewController {
+  func add(asChildViewController viewController: UIViewController, toView view: UIView) -> UIView {
+    guard let childView = viewController.view else {
+      return UIView()
+    }
+    
+    // Add Child View Controller
+    addChildViewController(viewController)
+    
+    // Add Child View As Subview
+    view.addSubview(childView)
+    
+    // Notify Child View Controller
+    viewController.didMove(toParentViewController: self)
+    
+    return childView
+  }
+}
