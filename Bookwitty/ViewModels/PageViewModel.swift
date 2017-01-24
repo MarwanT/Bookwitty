7ded97c1f8c373f29d62fb313192132b75fe5cd1
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-01-24 15:11:05 +0200

    Create page vc view model

---
+++ Bookwitty/ViewModels/PageViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  PageViewModel.swift
+//  Bookwitty
+//
+//  Created by Marwan  on 1/24/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+typealias InstructionData = (title: String?, description: String?, iamge: UIImage)
+
+struct PageViewModel {
+  var instructionsData: [InstructionData] = [InstructionData]()
+}
