b501e7f8ed9b9df0ae6e985192e976a89b153c2c
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-01-24 15:09:08 +0200

    Developed data source logic for instructions pages vc

--- Bookwitty/ViewControllers/PageViewController.swift
+++ Bookwitty/ViewControllers/PageViewController.swift
@@ -10,26 +10,52 @@ import UIKit
 
 class PageViewController: UIPageViewController {
   
+  fileprivate var orderedViewControllers: [UIViewController] = [UIViewController]()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    // Do any additional setup after loading the view.
+    dataSource = self
   }
   
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+}
+
+extension PageViewController: UIPageViewControllerDataSource {
+  func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+    guard let currentViewControllerIndex = orderedViewControllers.index(of: viewController) else {
+      return nil
+    }
+    let previousIndex = currentViewControllerIndex-1
+    guard previousIndex >= 0, previousIndex < orderedViewControllers.count else {
+      return nil
+    }
+    return orderedViewControllers[previousIndex]
+  }
   
+  func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+    guard let currentViewControllerIndex = orderedViewControllers.index(of: viewController) else {
+      return nil
+    }
+    let nextIndex = currentViewControllerIndex+1
+    guard nextIndex < orderedViewControllers.count else {
+      return nil
+    }
+    return orderedViewControllers[nextIndex]
+  }
   
-  /*
-   // MARK: - Navigation
-   
-   // In a storyboard-based application, you will often want to do a little preparation before navigation
-   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-   // Get the new view controller using segue.destinationViewController.
-   // Pass the selected object to the new view controller.
-   }
-   */
+  func presentationCount(for pageViewController: UIPageViewController) -> Int {
+    return orderedViewControllers.count
+  }
   
+  func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+    guard let currentViewController = viewControllers?.first,
+      let currentViewControllerIndex = orderedViewControllers.index(of: currentViewController) else {
+        return 0
+    }
+    return currentViewControllerIndex
+  }
 }
