8ee351aea947c24f1579f6a46b584ea3ee32e1e6
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-01-26 19:34:35 +0200

    Add storyboard manager for managing storyboards and view controllers

---
+++ Bookwitty/Storyboards/Storyboard.swift
@@ -0,0 +1,12 @@
+//
+//  Storyboard.swift
+//  Bookwitty
+//
+//  Created by Marwan  on 1/26/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+enum Storyboard: String {
+}
