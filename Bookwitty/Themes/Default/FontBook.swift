871552a84b563576e30cad6a2b8e0bbe5ebd0ae0
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-01-26 16:22:27 +0200

    Define the app dynamic types

--- Bookwitty/Themes/Default/FontBook.swift
+++ Bookwitty/Themes/Default/FontBook.swift
@@ -1,5 +1,5 @@
 //
-//  FontBook.swift
+//  FontDynamicType.swift
 //  Bookwitty
 //
 //  Created by Marwan  on 1/26/17.
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+enum FontDynamicType: String {
+  case headlineLarge = "headlineLarge"
+  case headlineMenium = "headlineMenium"
+  case headlineSmall = "headlineSmall"
+  case headlineXSmall = "headlineXSmall"
+  case body = "body"
+  case titleLarge = "titleLarge"
+  case titleMedium = "titleMedium"
+  case titleSmall = "titleSmall"
+  case labelHighlight = "labelHighlight"
+  case label = "label"
+  case captions = "captions"
+}
