c7521e3d0d185d3d8d3863888060caeee42e6a3f
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-01-27 15:57:45 +0200

    Create Sign in view controller

---
+++ Bookwitty/ViewControllers/Access/SignInViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SignInViewController.swift
+//  Bookwitty
+//
+//  Created by Marwan  on 1/27/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+}
