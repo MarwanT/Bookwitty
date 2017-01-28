d7bd08f70b64e2566590f0c8acabb40585fb4787
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-01-28 14:49:08 +0200

    Create input field custom view

---
+++ Bookwitty/Components/Views/InputField.swift
@@ -0,0 +1,12 @@
+//
+//  InputField.swift
+//  Bookwitty
+//
+//  Created by Marwan  on 1/27/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class InputField: UIView {
+}
