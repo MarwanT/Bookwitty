e5e8fa42e4799b8b3e1d4a3ab391a6cbc34dc31f
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-01-28 14:39:59 +0200

    Add method for loading a custom view
    This method is used in case the custom view was loaded after being placed in a storyboard as a UIView instance

--- Bookwitty/Utilities/View.swift
+++ Bookwitty/Utilities/View.swift
@@ -23,4 +23,54 @@ extension UIView {
     }
     return viewInstance
   }
+  
+  /** 
+   This method is to be called from
+   
+   `func awakeAfter(using aDecoder: NSCoder) -> Any?`
+   
+   Load a view from its relative nib file in case the view was 
+   integrated in the storyboard. Otherwise the view will be blank.
+  */
+  func viewForNibNameIfNeeded(nibName: String) -> Any? {
+    let isJustAPlaceholder = self.subviews.count == 0
+    
+    guard isJustAPlaceholder else {
+      return self
+    }
+    
+    guard let view = Bundle.main.loadNibNamed(nibName, owner: nil, options: nil)?[0] as? UIView else {
+      fatalError("Couldn't load view with nibName '\(nibName)'")
+    }
+    
+    view.autoresizingMask = self.autoresizingMask
+    view.translatesAutoresizingMaskIntoConstraints = self.translatesAutoresizingMaskIntoConstraints
+    
+    for constraint in self.constraints {
+      var firstItem = constraint.firstItem as? NSObject
+      if firstItem == self {
+        firstItem = view
+      }
+      
+      var secondItem = constraint.secondItem as? NSObject
+      if secondItem == self {
+        secondItem = view
+      }
+      
+      let newConstraint = NSLayoutConstraint(
+        item: firstItem!,
+        attribute: constraint.firstAttribute,
+        relatedBy: constraint.relation,
+        toItem: secondItem,
+        attribute: constraint.secondAttribute,
+        multiplier: constraint.multiplier,
+        constant: constraint.constant)
+      
+      view.addConstraint(newConstraint)
+    }
+    
+    self.removeConstraints(self.constraints)
+    
+    return view
+  }
 }
