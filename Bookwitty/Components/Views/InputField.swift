10250a6fc48bc9ddc341abb48acb2b83c19c4eef
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-01-29 18:31:40 +0200

    Add input field configuration structure

--- Bookwitty/Components/Views/InputField.swift
+++ Bookwitty/Components/Views/InputField.swift
@@ -8,9 +8,22 @@
 
 import UIKit
 
+struct InputFieldConfiguration {
+  var descriptionLabelText: String? = nil
+  var desriptionLabelDefaultTextColor: UIColor = UIColor.black
+  var desriptionLabelInvalidTextColor: UIColor = UIColor.red
+  var textFieldPlaceholder: String? = nil
+  var textFieldDefaultTextColor: UIColor = UIColor.black
+  var textFieldInvalidTextColor: UIColor = UIColor.red
+  var textFieldDefaultText: String? = nil
+  var invalidationIcon: UIImage? = nil
+}
+
 class InputField: UIView {
   @IBOutlet weak var descriptionLabel: UILabel!
   @IBOutlet weak var textField: UITextField!
+  
+  var configuration: InputFieldConfiguration = InputFieldConfiguration()
   
   override func awakeFromNib() {
     super.awakeFromNib()
