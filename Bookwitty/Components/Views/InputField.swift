7292b13c104adb9804d7a4f3636a33ec57c8294b
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-01-29 11:19:15 +0200

    Load view from nib in case it wasn’t
    Load view from nib in case it was loaded from code with an initializer, or loaded from the storyboard

--- Bookwitty/Components/Views/InputField.swift
+++ Bookwitty/Components/Views/InputField.swift
@@ -11,4 +11,12 @@ import UIKit
 class InputField: UIView {
   @IBOutlet weak var descriptionLabel: UILabel!
   @IBOutlet weak var textField: UITextField!
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+  }
+  
+  override func awakeAfter(using aDecoder: NSCoder) -> Any? {
+    return viewForNibNameIfNeeded(nibName: InputField.defaultNib)
+  }
 }
