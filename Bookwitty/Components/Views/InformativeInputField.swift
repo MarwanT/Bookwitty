7f738504d503ec3c4464c5c12c9fad2fd6176108
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-01-30 19:26:45 +0200

    Display right indicator for informative input fields

--- Bookwitty/Components/Views/InformativeInputField.swift
+++ Bookwitty/Components/Views/InformativeInputField.swift
@@ -9,5 +9,35 @@
 import UIKit
 
 class InformativeInputField: InputField {
-
+  var indicatorImageView: UIImageView!
+  
+  override func setupLayout() {
+    super.setupLayout()
+    
+    indicatorImageView = UIImageView(frame:CGRect(
+      x: 0, y: 0, width: configuration.rightSideViewWidth,
+       height: configuration.rightSideViewHeight))
+    indicatorImageView.backgroundColor = UIColor.bwAliceBlue
+    
+    textField.rightViewMode = UITextFieldViewMode.always
+  }
+  
+  override func refreshViewForStatus() {
+    UIView.animate(withDuration: 0.55) {
+      switch self.status {
+      case .empty:
+        self.descriptionLabel.textColor = self.configuration.desriptionLabelDefaultTextColor
+        self.textField.textColor = self.configuration.textFieldDefaultTextColor
+        self.textField.rightView = self.indicatorImageView
+      case .valid:
+        self.descriptionLabel.textColor = self.configuration.desriptionLabelDefaultTextColor
+        self.textField.textColor = self.configuration.textFieldDefaultTextColor
+        self.textField.rightView = self.indicatorImageView
+      case .inValid:
+        self.descriptionLabel.textColor = self.configuration.desriptionLabelInvalidTextColor
+        self.textField.textColor = self.configuration.textFieldInvalidTextColor
+        self.textField.rightView = self.invalidationImageView
+      }
+    }
+  }
 }
