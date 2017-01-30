836c52d3e9ab5ce32815d1b0e73324c6094393c4
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-01-30 17:17:19 +0200

    Add Password input field, a subclass of ‘Input Field’

---
+++ Bookwitty/Components/Views/PasswordInputField.swift
@@ -0,0 +1,12 @@
+//
+//  PasswordInputField.swift
+//  Bookwitty
+//
+//  Created by Marwan  on 1/30/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class PasswordInputField: InputField {
+}
