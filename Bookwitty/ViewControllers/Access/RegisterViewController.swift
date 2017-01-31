874fce170fb0597a6a5e2297bbe23fb42ed786eb
Shafic Hariri <shafic.hariri@keeward.com>
2017-01-31 17:59:18 +0200

    Create and add Register View controller and view model files to project

---
+++ Bookwitty/ViewControllers/Access/RegisterViewController.swift
@@ -0,0 +1,9 @@
+//
+//  RegisterViewController.swift
+//  Bookwitty
+//
+//  Created by Shafic Hariri on 1/31/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
---
+++ Bookwitty/ViewModels/Access/RegisterViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  RegisterViewModel.swift
+//  Bookwitty
+//
+//  Created by Shafic Hariri on 1/31/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
