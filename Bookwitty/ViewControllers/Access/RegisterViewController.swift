5c0d86de2de007ed5c7616d0b179f7dc0d56f63b
Shafic Hariri <shafic.hariri@keeward.com>
2017-01-31 20:00:24 +0200

    Connect views IBOutlets

--- Bookwitty/ViewControllers/Access/RegisterViewController.swift
+++ Bookwitty/ViewControllers/Access/RegisterViewController.swift
@@ -9,6 +9,17 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
+  @IBOutlet weak var stackView: UIStackView!
+  @IBOutlet weak var stackViewBackgroundView: UIView!
+  @IBOutlet weak var firstNameField: InputField!
+  @IBOutlet weak var lastNameField: InputField!
+  @IBOutlet weak var emailField: InputField!
+  @IBOutlet weak var passwordField: PasswordInputField!
+  @IBOutlet weak var countryField: InformativeInputField!
+  @IBOutlet weak var continueButton: UIButton!
+  @IBOutlet weak var termsLabel: UILabel!
+  @IBOutlet var separators: [UIView]!
+
   let viewModel: RegisterViewModel = RegisterViewModel()
 
   override func viewDidLoad() {
