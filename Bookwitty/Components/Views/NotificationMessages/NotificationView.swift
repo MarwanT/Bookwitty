25d52ab843b2453f207b4d4d6a2233ac71bef66e
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-02-01 11:18:48 +0200

    Add notification view

---
+++ Bookwitty/Components/Views/NotificationMessages/NotificationView.swift
@@ -0,0 +1,15 @@
+//
+//  ErrorNotificationView.swift
+//  Bookwitty
+//
+//  Created by Marwan  on 1/31/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+import SwiftMessages
+import FLKAutoLayout
+
+class NotificationView: MessageView {
+  @IBOutlet weak var stackView: UIStackView!
+}
