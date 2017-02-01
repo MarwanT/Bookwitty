0a79b1a52290498e3d7daa61212e94242646399e
Shafic Hariri <shafic.hariri@keeward.com>
2017-02-01 19:53:24 +0200

    Develop AttributedStringBuilder logic [(Font:Size),Color,Underline]

--- Bookwitty/Utilities/AttributedStringBuilder.swift
+++ Bookwitty/Utilities/AttributedStringBuilder.swift
@@ -9,4 +9,21 @@
 import Foundation
 
 class AttributedStringBuilder {
+  let attributedString = NSMutableAttributedString()
+  private let fontDynamicType: FontDynamicType
+
+  init(fontDynamicType: FontDynamicType) {
+    self.fontDynamicType = fontDynamicType
+  }
+
+  func append(text: String, fontDynamicType: FontDynamicType? = nil, color: UIColor =  ThemeManager.shared.currentTheme.defaultTextColor(),
+              underlineStyle: NSUnderlineStyle = NSUnderlineStyle.styleNone) -> Self {
+    let atrString = NSAttributedString(string: text, attributes: [ NSFontAttributeName : fontDynamicType?.font ?? self.fontDynamicType.font,
+                                                                   NSForegroundColorAttributeName : color,
+                                                                   NSUnderlineStyleAttributeName: underlineStyle.rawValue])
+
+    attributedString.append(atrString)
+    return self
+  }
+
 }
