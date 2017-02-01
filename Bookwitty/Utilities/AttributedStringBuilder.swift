123c3f525f046e96998a9896f2b8ec3f997ec381
Shafic Hariri <shafic.hariri@keeward.com>
2017-02-01 17:13:40 +0200

    Create and add the attributed string builder file

---
+++ Bookwitty/Utilities/AttributedStringBuilder.swift
@@ -0,0 +1,12 @@
+//
+//  AttributedStringBuilder.swift
+//  Bookwitty
+//
+//  Created by Shafic Hariri on 2/1/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+class AttributedStringBuilder {
+}
