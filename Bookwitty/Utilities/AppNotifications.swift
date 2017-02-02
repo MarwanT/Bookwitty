d0e58c4749bddeb618c698f1a70ed4fafe00bc2c
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-02-02 16:11:12 +0200

    Create app notification names structure

---
+++ Bookwitty/Utilities/AppNotifications.swift
@@ -0,0 +1,15 @@
+//
+//  AppNotifications.swift
+//  Bookwitty
+//
+//  Created by Marwan  on 2/1/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+struct AppNotification {
+  struct Name {
+    public static let failToRefreshToken: Notification.Name = Notification.Name(rawValue: "AppNotification.Name.failToRefreshToken")
+  }
+}
