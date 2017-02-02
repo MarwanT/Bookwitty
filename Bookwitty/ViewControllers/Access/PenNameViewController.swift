dc1a705bd7749a8a593947db86472825392ea7a8
Shafic Hariri <shafic.hariri@keeward.com>
2017-02-02 22:41:09 +0200

    Create PenName View controller and view model

---
+++ Bookwitty/ViewControllers/Access/PenNameViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PenNameViewController.swift
+//  Bookwitty
+//
+//  Created by Shafic Hariri on 2/2/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class PenNameViewController: UIViewController {
+
+}
---
+++ Bookwitty/ViewModels/Access/PenNameViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  PenNameViewModel.swift
+//  Bookwitty
+//
+//  Created by Shafic Hariri on 2/2/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+final class PenNameViewModel {
+
+}
