2c0d62397367be3d2294ee6a1cd0859fad686c36
Shafic Hariri <shafic.hariri@keeward.com>
2017-02-02 11:16:42 +0200

    Add paragraph styling option to the AttributedStringBuilder

--- Bookwitty/Utilities/AttributedStringBuilder.swift
+++ Bookwitty/Utilities/AttributedStringBuilder.swift
@@ -9,10 +9,11 @@
 import Foundation
 
 class AttributedStringBuilder {
-  let attributedString = NSMutableAttributedString()
+  let attributedString: NSMutableAttributedString
   private let fontDynamicType: FontDynamicType
 
   init(fontDynamicType: FontDynamicType) {
+    self.attributedString = NSMutableAttributedString()
     self.fontDynamicType = fontDynamicType
   }
 
@@ -20,10 +21,27 @@ class AttributedStringBuilder {
               underlineStyle: NSUnderlineStyle = NSUnderlineStyle.styleNone) -> Self {
     let atrString = NSAttributedString(string: text, attributes: [ NSFontAttributeName : fontDynamicType?.font ?? self.fontDynamicType.font,
                                                                    NSForegroundColorAttributeName : color,
-                                                                   NSUnderlineStyleAttributeName: underlineStyle.rawValue])
+                                                                   NSUnderlineStyleAttributeName: underlineStyle.rawValue
+      ])
 
     attributedString.append(atrString)
     return self
   }
 
+  /**
+   * Discussion:
+   * Use this function as the last part of your builder to apply the paragraph styling on all parts.
+   * Note: If this function was used in the beginning it will not work
+   */
+  func applyParagraphStyling(lineSpacing: CGFloat = 10, alignment: NSTextAlignment = NSTextAlignment.natural) -> Self {
+    let paragraphStyle: NSMutableParagraphStyle = NSMutableParagraphStyle()
+    paragraphStyle.lineSpacing = lineSpacing
+    paragraphStyle.alignment = alignment
+    
+    let range = NSRange(location: 0, length: attributedString.string.characters.count)
+
+    attributedString.addAttribute(NSParagraphStyleAttributeName, value: paragraphStyle, range: range)
+    return self
+  }
+  
 }
