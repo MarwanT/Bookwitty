671b6a75272674d1b3d88bd7c698618866282869
Shafic Hariri <shafic.hariri@keeward.com>
2017-02-02 23:16:14 +0200

    Conform to themeable, make circular and Apply themes

--- Bookwitty/ViewControllers/Access/PenNameViewController.swift
+++ Bookwitty/ViewControllers/Access/PenNameViewController.swift
@@ -11,4 +11,22 @@ import UIKit
 class PenNameViewController: UIViewController {
   @IBOutlet weak var circularView: UIView!
 
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    applyTheme()
+  }
+}
+
+extension PenNameViewController: Themeable {
+  func applyTheme() {
+    circularView.backgroundColor = ThemeManager.shared.currentTheme.colorNumber11()
+    makeViewCircular(view: circularView, borderColor: ThemeManager.shared.currentTheme.colorNumber18(), borderWidth: 1.0)
+  }
+
+  func makeViewCircular(view: UIView,borderColor: UIColor, borderWidth: CGFloat) {
+    view.layer.cornerRadius = view.frame.size.width/2
+    view.clipsToBounds = true
+    view.layer.borderColor = borderColor.cgColor
+    view.layer.borderWidth = 1.0
+  }
 }
