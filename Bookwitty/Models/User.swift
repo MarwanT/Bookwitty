992cc1843a24a9a8ca24ea9771f3a609935f4468
Shafic Hariri <shafic.hariri@keeward.com>
2017-02-02 21:56:45 +0200

    Add the user model properties

--- Bookwitty/Models/User.swift
+++ Bookwitty/Models/User.swift
@@ -9,4 +9,18 @@
 import Foundation
 
 class User {
+  //TODO: Conform to Spine's Resource
+  var firstName: String? = nil
+  var lastName: String? = nil
+  var dateOfBirth: String? = nil
+  var password: String? = nil
+  var email: String? = nil
+  var country: String? = nil
+  var createdAt: String? = nil
+  var updatedAt: String? = nil
+  var badges: [String: Any]? = nil
+  var preferences: [String: Any]? = nil
+
+  //TODO: override Spine's resourceType with type: 'users'
+  //TODO: override fields and map user's properties
 }
