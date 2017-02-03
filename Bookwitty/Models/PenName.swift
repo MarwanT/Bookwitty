0d0cc5479804b74e3484b02f3f3f254b65f55dd5
Shafic Hariri <shafic.hariri@keeward.com>
2017-02-03 18:48:40 +0200

    Create and Add the PenName model [Empty]

---
+++ Bookwitty/Models/PenName.swift
@@ -0,0 +1,13 @@
+//
+//  PenName.swift
+//  Bookwitty
+//
+//  Created by Shafic Hariri on 2/3/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+class PenName {
+
+}
