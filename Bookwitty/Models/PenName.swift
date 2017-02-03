122bb905fee149f6a2da436e9610f6ad15b2ae72
Shafic Hariri <shafic.hariri@keeward.com>
2017-02-03 19:12:42 +0200

    Add PenName properties

--- Bookwitty/Models/PenName.swift
+++ Bookwitty/Models/PenName.swift
@@ -9,5 +9,19 @@
 import Foundation
 
 class PenName {
-
+  var name: String?
+  var biography: String?
+  var avatarUrl: String?
+  var facebookUrl: String?
+  var tumblrUrl: String?
+  var googlePlusUrl: String?
+  var twitterUrl: String?
+  var instagramUrl: String?
+  var pinterestUrl: String?
+  var youtubeUrl: String?
+  var linkedinUrl: String?
+  var wordpressUrl: String?
+  var websiteUrl: String?
+  var followersCount: Int?
+  var followingCount: Int?
 }
