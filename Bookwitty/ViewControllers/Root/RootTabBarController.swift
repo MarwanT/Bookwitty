5d8efddfc3348ee250e3034ca989aa7d688cd9c5
Shafic Hariri <shafic.hariri@keeward.com>
2017-02-08 11:55:59 +0200

    Create the Root tab bar controller file

---
+++ Bookwitty/ViewControllers/Root/RootTabBarController.swift
@@ -0,0 +1,13 @@
+//
+//  RootTabbarViewController.swift
+//  Bookwitty
+//
+//  Created by Shafic Hariri on 2/8/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+class RootTabBarController: UITabBarController {
+
+}
