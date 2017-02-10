a4334a64ec098c40675f1478b2deab7f898e9812
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-02-10 09:34:40 +0200

    Create category manager

---
+++ Bookwitty/Models/Managers/CategoryManager.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryManager.swift
+//  Bookwitty
+//
+//  Created by Marwan  on 2/10/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+class CategoryManager {
+}
