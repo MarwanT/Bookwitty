0184a6b37bcba8b9873d9a33a5da4761e7baa3c9
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-02-10 09:37:49 +0200

    Retrieve array of categories out of the son file

--- Bookwitty/Models/Managers/CategoryManager.swift
+++ Bookwitty/Models/Managers/CategoryManager.swift
@@ -13,4 +13,31 @@ class CategoryManager {
   static let shared: CategoryManager = CategoryManager()
   private init() {
   }
+  
+  
+  func categoriesFromDictionary(fromDictionary dictionary: [String: Any], parentKeys: [String]? = nil) -> [Category] {
+    var categories = [Category]()
+    
+    for (key, value) in dictionary {
+      guard let valuesDictionary = value as? [String: Any],
+        let name = valuesDictionary["name"] as? String else {
+          continue
+      }
+      
+      var currentKeys = parentKeys ?? [String]()
+      currentKeys.append(key)
+      let categoryKey = currentKeys.joined(separator: ".")
+      
+      var subcategories: [Category]? = nil
+      var subcategoriesDictionary = valuesDictionary
+      subcategoriesDictionary.removeValue(forKey: "name")
+      if subcategoriesDictionary.count > 0 {
+        subcategories = categoriesFromDictionary(fromDictionary: subcategoriesDictionary, parentKeys: currentKeys)
+      }
+      
+      categories.append(Category(key: categoryKey, value: name, subcategories: subcategories))
+    }
+    
+    return categories
+  }
 }
