f0407b0a35d006730446290a62cee880a96874e1
Shafic Hariri <shafic.hariri@keeward.com>
2017-02-11 20:25:57 +0200

    Layout the items in the node

--- Bookwitty/Components/Nodes/CardActionBarNode.swift
+++ Bookwitty/Components/Nodes/CardActionBarNode.swift
@@ -15,6 +15,8 @@ class CardActionBarNode: ASCellNode {
   var commentButton: ASButtonNode
   var shareButton: ASButtonNode
 
+  let buttonHeight: CGFloat = 34.0
+
   private override init() {
     witButton = ASButtonNode()
     commentButton = ASButtonNode()
@@ -24,4 +26,35 @@ class CardActionBarNode: ASCellNode {
     addSubnode(commentButton)
     addSubnode(shareButton)
   }
+
+  private func spacer(flexGrow: CGFloat = 1.0) -> ASLayoutSpec {
+    return ASLayoutSpec().styled { (style) in
+      style.flexGrow = flexGrow
+    }
+  }
+
+  override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
+    //Setup Dynamic width Wit Button
+    witButton.titleNode.maximumNumberOfLines = 1
+    witButton.contentEdgeInsets = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+    witButton.style.height = ASDimensionMake(buttonHeight)
+
+    //Setup other buttons
+    commentButton.style.preferredSize = CGSize(width: buttonHeight, height: buttonHeight)
+    shareButton.style.preferredSize = CGSize(width: buttonHeight, height: buttonHeight)
+
+    shareButton.style.spacingBefore = 50
+
+    let horizontalStackSpec = ASStackLayoutSpec(direction: .horizontal,
+                                                spacing: 0,
+                                                justifyContent: .spaceAround,
+                                                alignItems: .stretch,
+                                                children: [witButton,
+                                                           spacer(),
+                                                           commentButton,
+                                                           shareButton])
+
+    return horizontalStackSpec
+  }
+
 }
