e74735264ff6f30cea9f57b707db308dac4df3ef
Shafic Hariri <shafic.hariri@keeward.com>
2017-02-11 14:47:38 +0200

    Set node elements Constraints: Implement layoutSpecThatFits

--- Bookwitty/Components/Nodes/CardPostInfoNode.swift
+++ Bookwitty/Components/Nodes/CardPostInfoNode.swift
@@ -23,4 +23,22 @@ class CardPostInfoNode: ASCellNode {
     postDateTextNode = ASTextNode()
     super.init()
   }
+  override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
+    //Add the User Profile Image - Vertical Stack [Name - Date] - Image ArrowDown
+    let verticalStack = ASStackLayoutSpec.vertical()
+    verticalStack.style.flexShrink = 1.0
+    verticalStack.style.flexGrow = 1.0
+    verticalStack.justifyContent = .center
+    verticalStack.alignItems = .start
+    verticalStack.children = [userNameTextNode, postDateTextNode]
+
+    let horizontalStackSpec = ASStackLayoutSpec(direction: .horizontal,
+                                                      spacing: 10,
+                                                      justifyContent: .center,
+                                                      alignItems: .stretch,
+                                                      children: [userProfileImageNode, verticalStack, arrowDownImageNode])
+
+    return ASInsetLayoutSpec(insets: UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0), child: horizontalStackSpec)
+  }
+
 }
