758f2a024bfa165d058ecf589bb08f5a0f76737e
Shafic Hariri <shafic.hariri@keeward.com>
2017-02-13 00:49:27 +0200

    Setup image and text nodes with their values

--- Bookwitty/Components/Nodes/ArticleCardPostCellNode.swift
+++ Bookwitty/Components/Nodes/ArticleCardPostCellNode.swift
@@ -27,6 +27,50 @@ class ArticleCardContentNode: ASDisplayNode {
   var titleNode: ASTextNode
   var descriptionNode: ASTextNode
   var commentsSummaryNode: ASTextNode
+
+  var articleTitle: String? {
+    didSet {
+      if let articleTitle = articleTitle {
+        titleNode.attributedText = AttributedStringBuilder(fontDynamicType: .title2)
+          .append(text: articleTitle, color: ThemeManager.shared.currentTheme.colorNumber20()).attributedString
+      }
+    }
+  }
+  var articleDescription: String? {
+    didSet {
+      if let articleDescription = articleDescription {
+        descriptionNode.attributedText = AttributedStringBuilder(fontDynamicType: .body)
+          .append(text: articleDescription, color: ThemeManager.shared.currentTheme.colorNumber20()).attributedString
+      }
+    }
+  }
+  var articleCommentsSummary: String? {
+    didSet {
+      if let articleCommentsSummary = articleCommentsSummary {
+        commentsSummaryNode.attributedText = AttributedStringBuilder(fontDynamicType: .caption2)
+          .append(text: articleCommentsSummary, color: ThemeManager.shared.currentTheme.colorNumber15()).attributedString
+      }
+    }
+  }
+  var imageUrl: String? {
+    didSet {
+      if let imageUrl = imageUrl {
+        imageNode.url = URL(string: imageUrl)
+      }
+    }
+  }
+
+  var hasImage: Bool {
+    get {
+      return !(imageUrl?.isEmpty ?? true)
+    }
+  }
+
+  var hasComments: Bool {
+    get {
+      return !(articleCommentsSummary?.isEmpty ?? true)
+    }
+  }
   
   override init() {
     imageNode = ASNetworkImageNode()
