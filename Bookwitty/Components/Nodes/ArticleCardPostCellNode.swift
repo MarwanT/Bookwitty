82fa957f8e3187298c5b483925ec2236b054564b
Shafic Hariri <shafic.hariri@keeward.com>
2017-02-13 00:44:35 +0200

    Setup cell basics: conform to BaseCardPostNode cell and override needed BaseCardPostNodeContentProvider protocol properties

--- Bookwitty/Components/Nodes/ArticleCardPostCellNode.swift
+++ Bookwitty/Components/Nodes/ArticleCardPostCellNode.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+import AsyncDisplayKit
+
+class ArticleCardPostCellNode: BaseCardPostNode {
+
+  let node: ArticleCardContentNode
+  override var shouldShowInfoNode: Bool { return true }
+  override var contentShouldExtendBorders: Bool { return true }
+  override var contentNode: ASDisplayNode { return node }
+
+  override init() {
+    node = ArticleCardContentNode()
+    super.init()
+  }
+}
+
+class ArticleCardContentNode: ASDisplayNode {
+  override init() {
+    super.init()
+  }
+}
