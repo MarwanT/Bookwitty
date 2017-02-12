1ae3b05088428a358fe4f4dee3f88352299953f1
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-02-12 03:28:07 +0200

    Create categories table view controller

---
+++ Bookwitty/ViewModels/Books/CategoriesTableViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  CategoriesTableViewModel.swift
+//  Bookwitty
+//
+//  Created by Marwan  on 2/12/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+final class CategoriesTableViewModel {
+  
+}
