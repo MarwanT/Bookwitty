a755cc64cd12cc26d0a6963573dcf2a91a473653
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-02-13 13:33:16 +0200

    Add the empty view model

---
+++ Bookwitty/ViewModels/Account/AccountViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  AccountViewModel.swift
+//  Bookwitty
+//
+//  Created by charles on 2/13/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+final class AccountViewModel {
+
+}
