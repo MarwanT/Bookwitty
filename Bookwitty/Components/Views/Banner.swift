26665261807dd4f93ca4ff40933665a2167c1155
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-02-13 20:44:24 +0200

    Create Banner custom view

---
+++ Bookwitty/Components/Views/Banner.swift
@@ -0,0 +1,24 @@
+//
+//  Banner.swift
+//  Bookwitty
+//
+//  Created by Marwan  on 2/13/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class Banner: UIView {
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    initialize()
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    initialize()
+  }
+  
+  private func initialize() {
+  }
+}
