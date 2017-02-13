460f9cbaa77b5716b631265cef1d0fc53643dd89
Shafic Hariri <shafic.hariri@keeward.com>
2017-02-13 16:21:34 +0200

    Create and Initialize LinkCard Cell and Content nodes

--- Bookwitty/Components/Nodes/LinkCardPostCellNode.swift
+++ Bookwitty/Components/Nodes/LinkCardPostCellNode.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+import AsyncDisplayKit
+
+class LinkCardPostCellNode: BaseCardPostNode {
+  
+  let node: LinkCardPostContentNode
+  override var shouldShowInfoNode: Bool { return true }
+  override var contentShouldExtendBorders: Bool { return true }
+  override var contentNode: ASDisplayNode { return node }
+
+  override init() {
+    node = LinkCardPostContentNode()
+    super.init()
+  }
+}
+
+class LinkCardPostContentNode: ASDisplayNode {
+
+  var imageNode: ASNetworkImageNode
+  var titleNode: ASTextNode
+  var descriptionNode: ASTextNode
+  var commentsSummaryNode: ASTextNode
+
+  override init() {
+    imageNode = ASNetworkImageNode()
+    titleNode = ASTextNode()
+    descriptionNode = ASTextNode()
+    commentsSummaryNode = ASTextNode()
+    super.init()
+    addSubnode(imageNode)
+    addSubnode(titleNode)
+    addSubnode(descriptionNode)
+    addSubnode(commentsSummaryNode)
+  }
+}
