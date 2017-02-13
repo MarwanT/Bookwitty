adc603c9e3c1bbde07de167ff9932c64f1caf0fd
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-02-13 20:52:39 +0200

    Set layout constraints for banner subviews

--- Bookwitty/Components/Views/Banner.swift
+++ Bookwitty/Components/Views/Banner.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FLKAutoLayout
 
 class Banner: UIView {
   let imageView = UIImageView(frame: CGRect.zero)
@@ -27,6 +28,7 @@ class Banner: UIView {
   
   private func initialize() {
     addSubviews()
+    setConstraints()
   }
   
   private func addSubviews() {
@@ -36,5 +38,25 @@ class Banner: UIView {
     self.addSubview(labelsContainerView)
     
     self.insertSubview(dimView, aboveSubview: imageView)
+  }
+  
+  func setConstraints() {
+    let descriptionTopSpace: CGFloat = ThemeManager.shared.currentTheme.titleMargin()
+    let minimumHeight: CGFloat = 150
+    
+    imageView.alignTop("0@500", leading: "0", bottom: "0@500", trailing: "0", toView: self)
+    self.constrainHeight("\(minimumHeight)@750")
+    
+    titleLabel.alignLeading("0", trailing: "0", toView: labelsContainerView)
+    descriptionLabel.alignLeading("0", trailing: "0", toView: labelsContainerView)
+    titleLabel.alignTopEdge(withView: labelsContainerView, predicate: "0")
+    descriptionLabel.constrainTopSpace(toView: titleLabel, predicate: "\(descriptionTopSpace)")
+    labelsContainerView.alignBottomEdge(withView: descriptionLabel, predicate: "0")
+    
+    labelsContainerView.alignLeading("0", trailing: "0", toView: self)
+    labelsContainerView.alignCenterY(withView: self, predicate: "0")
+    labelsContainerView.alignTopEdge(withView: self, predicate: ">=10")
+    
+    dimView.alignTop("0", leading: "0", bottom: "0", trailing: "0", toView: imageView)
   }
 }
