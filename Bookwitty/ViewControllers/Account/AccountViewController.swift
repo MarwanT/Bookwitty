6ae60b442cee3db212243923816548b6b5f2f718
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-02-13 13:30:11 +0200

    Add the account view controller swift file

---
+++ Bookwitty/ViewControllers/Account/AccountViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AccountViewController.swift
+//  Bookwitty
+//
+//  Created by charles on 2/13/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class AccountViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+}
