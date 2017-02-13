373cdf398bfe25e40e2dcf31cd6ecfad01659756
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-02-13 11:58:57 +0200

    Remove useless code and comments

--- Bookwitty/ViewControllers/Books/CategoriesTableViewController.swift
+++ Bookwitty/ViewControllers/Books/CategoriesTableViewController.swift
@@ -20,25 +20,17 @@ class CategoriesTableViewController: UITableViewController {
     // Uncomment the following line to preserve selection between presentations
     clearsSelectionOnViewWillAppear = true
     
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     applyTheme()
   }
   
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
   
   // MARK: - Table view data source
   
   override func numberOfSections(in tableView: UITableView) -> Int {
-    // #warning Incomplete implementation, return the number of sections
     return viewModel.numberOfSections
   }
   
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    // #warning Incomplete implementation, return the number of rows
     return viewModel.numberOfRowsForSection(section: section)
   }
   
@@ -67,6 +59,5 @@ extension CategoriesTableViewController: Themeable {
     tableView.separatorColor = ThemeManager.shared.currentTheme.defaultSeparatorColor()
     tableView.separatorInset = UIEdgeInsets(
       top: 0, left: leftMargin, bottom: 0, right: 0)
-    
   }
 }
