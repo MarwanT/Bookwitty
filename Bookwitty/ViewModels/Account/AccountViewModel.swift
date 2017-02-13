1cc3b5d992f1e8d88bca8d9a61f4f9ca07828436
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-02-13 18:25:18 +0200

    Add the localized strings

--- Bookwitty/ViewModels/Account/AccountViewModel.swift
+++ Bookwitty/ViewModels/Account/AccountViewModel.swift
@@ -9,5 +9,18 @@
 import Foundation
 
 final class AccountViewModel {
+  let viewControllerTitle: String = localizedString(key: "account", defaultValue: "Account")
+  let myOrdersText: String = localizedString(key: "my_orders", defaultValue: "My Orders")
+  let addressBookText: String = localizedString(key: "address_book", defaultValue: "Address Book")
+  let paymentMethodsText: String = localizedString(key: "payment_methods", defaultValue: "Payment Methods")
+  let settingsText: String = localizedString(key: "settings", defaultValue: "Settings")
+  let penNamesText: String = localizedString(key: "pen_names", defaultValue: "Pen Names")
+  let draftsText: String = localizedString(key: "drafts", defaultValue: "Drafts")
+  let interestsText: String = localizedString(key: "interests", defaultValue: "Interests")
+  let readingListsText: String = localizedString(key: "reading_lists", defaultValue: "Reading Lists")
+  let createNewPenNameText: String = localizedString(key: "create_new_pen_names", defaultValue: "Create New Pen Name")
+  let customerServiceText: String = localizedString(key: "customer_service", defaultValue: "Customer Service")
+  let helpText: String = localizedString(key: "help", defaultValue: "Help")
+  let contactUsText: String = localizedString(key: "contact_us", defaultValue: "Contact Us")
 
 }
