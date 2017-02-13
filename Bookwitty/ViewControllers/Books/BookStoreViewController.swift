595320607a2763dc2431acec9eea5b721e106eec
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-02-13 16:12:24 +0200

    Add Book Store View Controller

---
+++ Bookwitty/ViewControllers/Books/BookStoreViewController.swift
@@ -0,0 +1,15 @@
+//
+//  BookStoreViewController.swift
+//  Bookwitty
+//
+//  Created by Marwan  on 2/13/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class BookStoreViewController: UIViewController {
+  @IBOutlet weak var stackView: UIStackView!
+  
+  let viewModel = BookStoreViewModel()
+}
---
+++ Bookwitty/ViewModels/Books/BookStoreViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  BookStoreViewModel.swift
+//  Bookwitty
+//
+//  Created by Marwan  on 2/13/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+final class BookStoreViewModel {
+  
+}
