f6f6a602f3dbeeb98444f145314390b08f319ece
Shafic Hariri <shafic.hariri@keeward.com>
2017-02-14 18:21:24 +0200

    Initialize TopicCardPost Cell and Node

--- Bookwitty/Components/Nodes/TopicCardPostCellNode.swift
+++ Bookwitty/Components/Nodes/TopicCardPostCellNode.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+import AsyncDisplayKit
+
+class TopicCardPostCellNode: BaseCardPostNode {
+
+  let node: TopicCardPostContentNode
+  override var shouldShowInfoNode: Bool { return true }
+  override var contentShouldExtendBorders: Bool { return true }
+  override var contentNode: ASDisplayNode { return node }
+
+  override init() {
+    node = TopicCardPostContentNode()
+    super.init()
+  }
+}
+
+class TopicCardPostContentNode: ASDisplayNode {
+  override init() {
+    super.init()
+  }
+}
