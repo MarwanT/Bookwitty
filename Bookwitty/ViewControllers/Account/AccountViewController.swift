b5e5adbbb6f0e95f967634728ee7fb351ab769fa
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-02-14 12:13:32 +0200

    Add the primordial table view datasource and delegate implementation

--- Bookwitty/ViewControllers/Account/AccountViewController.swift
+++ Bookwitty/ViewControllers/Account/AccountViewController.swift
@@ -27,7 +27,53 @@ class AccountViewController: UIViewController {
   }
 }
 
-extension AccountViewController {
+extension AccountViewController: UITableViewDataSource, UITableViewDelegate {
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return viewModel.numberOfSections()
+  }
 
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return viewModel.numberOfRowsIn(section: section)
+  }
 
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    if case AccountViewModel.Sections.PenNames.rawValue = indexPath.section, 0 == indexPath.row {
+      return 60.0
+    }
+
+    return 45.0
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    var reuseIdentifier: String
+
+    if case AccountViewModel.Sections.PenNames.rawValue = indexPath.section, 0 == indexPath.row {
+      reuseIdentifier = AccountPenNameTableViewCell.reuseIdentifier
+    } else {
+      reuseIdentifier = DisclosureTableViewCell.identifier
+    }
+
+    return tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+  }
+
+  func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+
+  }
+
+  func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    return viewModel.titleFor(section: section)
+  }
+
+  func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+    switch section {
+    case AccountViewModel.Sections.UserInformation.rawValue:
+      return 0
+    default:
+      return 45.0
+    }
+  }
+
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
+  }
 }
