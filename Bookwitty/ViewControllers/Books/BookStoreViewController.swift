992ea0b5ff41659e51347aaafe4b1b828e65285f
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-02-14 12:48:16 +0200

    Add method to add banner to view

--- Bookwitty/ViewControllers/Books/BookStoreViewController.swift
+++ Bookwitty/ViewControllers/Books/BookStoreViewController.swift
@@ -12,4 +12,13 @@ class BookStoreViewController: UIViewController {
   @IBOutlet weak var stackView: UIStackView!
   
   let viewModel = BookStoreViewModel()
+  
+  func loadBannerSection() -> Bool {
+    let banner = Banner()
+    banner.image = #imageLiteral(resourceName: "Illustrtion")
+    banner.title = "Bookwitty's Finest"
+    banner.subtitle = "The perfect list for everyone on your list"
+    stackView.addArrangedSubview(banner)
+    return true
+  }
 }
