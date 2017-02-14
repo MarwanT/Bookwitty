f5f61f1bdbee8de18ef6ccd0b9d893aac5518569
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-02-14 14:06:32 +0200

    Add the reuse identifier and the nib

--- Bookwitty/Components/Views/TableViewSectionHeader/TableViewSectionHeaderView.swift
+++ Bookwitty/Components/Views/TableViewSectionHeader/TableViewSectionHeaderView.swift
@@ -10,8 +10,25 @@ import UIKit
 import TTTAttributedLabel
 
 class TableViewSectionHeaderView: UITableViewHeaderFooterView {
+  static let reuseIdentifier = "TableViewSectionHeaderViewReuseIdentifier"
+  static let nib: UINib = UINib(nibName: "TableViewSectionHeaderView", bundle: nil)
+
   @IBOutlet weak var label: TTTAttributedLabel!
 
+  override func awakeFromNib() {
+    super.awakeFromNib()
+
+    applyTheme()
+  }
 }
 
+extension TableViewSectionHeaderView: Themeable {
+  func applyTheme() {
+    let leftMargin = ThemeManager.shared.currentTheme.generalExternalMargin()
 
+    contentView.layoutMargins = UIEdgeInsets(top: 0, left: leftMargin, bottom: 0, right: 0)
+
+    label.font = FontDynamicType.subheadline.font
+    label.textColor = ThemeManager.shared.currentTheme.defaultTextColor()
+  }
+}
