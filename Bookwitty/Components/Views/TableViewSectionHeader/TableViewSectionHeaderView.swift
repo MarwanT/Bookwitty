b59023f4c700c63aaee7db1c2c4d1c87bf221569
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-02-14 13:44:35 +0200

    Add the table view section header view

---
+++ Bookwitty/Components/Views/TableViewSectionHeader/TableViewSectionHeaderView.swift
@@ -0,0 +1,15 @@
+//
+//  TableViewSectionHeaderView.swift
+//  Bookwitty
+//
+//  Created by charles on 2/14/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class TableViewSectionHeaderView: UITableViewHeaderFooterView {
+
+}
+
+
