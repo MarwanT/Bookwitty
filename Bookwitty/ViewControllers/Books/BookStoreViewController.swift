3e4f8e7b3e012241b0cefc4468b135a048d8c280
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-02-14 12:50:55 +0200

    Add method to add featured content collection view

--- Bookwitty/ViewControllers/Books/BookStoreViewController.swift
+++ Bookwitty/ViewControllers/Books/BookStoreViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FLKAutoLayout
 
 class BookStoreViewController: UIViewController {
   @IBOutlet weak var stackView: UIStackView!
@@ -19,6 +20,31 @@ class BookStoreViewController: UIViewController {
     banner.title = "Bookwitty's Finest"
     banner.subtitle = "The perfect list for everyone on your list"
     stackView.addArrangedSubview(banner)
+    return true
+  }
+  
+  func loadFeaturedContentSection() -> Bool {
+    let itemSize = FeaturedContentCollectionViewCell.defaultSize
+    let interItemSpacing: CGFloat = 10
+    let contentInset = UIEdgeInsets(
+      top: 15, left: ThemeManager.shared.currentTheme.generalExternalMargin(),
+      bottom: 10, right: ThemeManager.shared.currentTheme.generalExternalMargin())
+    
+    let flowLayout = UICollectionViewFlowLayout()
+    flowLayout.scrollDirection = UICollectionViewScrollDirection.horizontal
+    flowLayout.itemSize = itemSize
+    flowLayout.minimumInteritemSpacing = interItemSpacing
+    
+    let collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: flowLayout)
+    collectionView.register(FeaturedContentCollectionViewCell.nib, forCellWithReuseIdentifier: FeaturedContentCollectionViewCell.reuseIdentifier)
+    collectionView.backgroundColor = UIColor.orange
+    collectionView.contentInset = contentInset
+    collectionView.showsHorizontalScrollIndicator = false
+    collectionView.constrainHeight("\(itemSize.height + contentInset.top + contentInset.bottom)")
+    collectionView.constrainWidth("\(self.view.frame.width)")
+    
+    stackView.addArrangedSubview(collectionView)
+    
     return true
   }
 }
