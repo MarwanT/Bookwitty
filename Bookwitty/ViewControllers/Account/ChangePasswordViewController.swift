05756516ec8a8c69f5bba93a8bfa8f470c720b07
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-02-15 13:52:07 +0200

    Set the view controller title

--- Bookwitty/ViewControllers/Account/ChangePasswordViewController.swift
+++ Bookwitty/ViewControllers/Account/ChangePasswordViewController.swift
@@ -20,6 +20,7 @@ class ChangePasswordViewController: UIViewController {
     super.viewDidLoad()
 
     // Do any additional setup after loading the view.
+    self.title = viewModel.changePasswordText
   }
 
   private func initializeComponents() {
