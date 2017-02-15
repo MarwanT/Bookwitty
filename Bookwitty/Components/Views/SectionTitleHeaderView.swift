d332e5c17419dfd1d0b4453122fdd3d1130b9a30
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-02-15 12:24:38 +0200

    Create section header, a global component that will be used across the app

---
+++ Bookwitty/Components/Views/SectionTitleHeaderView.swift
@@ -0,0 +1,15 @@
+//
+//  SectionTitleHeaderView.swift
+//  Bookwitty
+//
+//  Created by Marwan  on 2/15/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class SectionTitleHeaderView: UITableViewHeaderFooterView {
+  @IBOutlet weak var verticalBarView: UIView!
+  @IBOutlet weak var horizontalBarView: UIView!
+  @IBOutlet weak var label: UILabel!
+}
