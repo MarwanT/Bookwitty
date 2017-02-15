a5e121a8156014b209b4ca2be08f995570d53d88
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-02-15 12:55:52 +0200

    Add the view model

---
+++ Bookwitty/ViewModels/Account/ChangePasswordViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ChangePasswordViewModel.swift
+//  Bookwitty
+//
+//  Created by charles on 2/15/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+class ChangePasswordViewModel {
+
+}
