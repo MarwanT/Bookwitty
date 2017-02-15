7ac3849da9594e07ed620519f48b46017e103907
Shafic Hariri <shafic.hariri@keeward.com>
2017-02-15 11:27:01 +0200

    Initialize ReadingListCardPost content and cell node

--- Bookwitty/Components/Nodes/ReadingListCardPostCellNode.swift
+++ Bookwitty/Components/Nodes/ReadingListCardPostCellNode.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+import AsyncDisplayKit
+
+class ReadingListCardPostCellNode: BaseCardPostNode {
+  let node: ReadingListCardContentNode
+  override var shouldShowInfoNode: Bool { return true }
+  override var contentShouldExtendBorders: Bool { return true }
+  override var contentNode: ASDisplayNode { return node }
+
+  override init() {
+    node = ReadingListCardContentNode()
+    super.init()
+  }
+}
+
+class ReadingListCardContentNode: ASDisplayNode {
+  override init() {
+    super.init()
+  }
+}
