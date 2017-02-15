6eeef5908edf14f13319a4dd14584f2bc54169e4
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-02-15 16:12:48 +0200

    Create book table view cell

---
+++ Bookwitty/Components/Views/BookTableViewCell/BookTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  BookTableViewCell.swift
+//  Bookwitty
+//
+//  Created by Marwan  on 2/15/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    // Initialization code
+  }
+  
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+    
+    // Configure the view for the selected state
+  }
+}
