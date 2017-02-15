0c4f4faa30b4397ba0a2dfb252c061283161a37b
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-02-15 12:25:06 +0200

    Add the change password view controller

---
+++ Bookwitty/ViewControllers/Account/ChangePasswordViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ChangePasswordViewController.swift
+//  Bookwitty
+//
+//  Created by charles on 2/15/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class ChangePasswordViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+}
