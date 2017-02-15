95bbf12a7c5334dbe07dc2af61a68a1ee2baa235
Shafic Hariri <shafic.hariri@keeward.com>
2017-02-15 16:49:31 +0200

    Add UIImage scaling extension

--- Bookwitty/Utilities/Image.swift
+++ Bookwitty/Utilities/Image.swift
@@ -21,3 +21,30 @@ public extension UIImage {
     self.init(cgImage: cgImage)
   }
 }
+
+
+// MARK: - Image Scaling.
+extension UIImage {
+  func imageWithSize(size:CGSize) -> UIImage?
+  {
+    var scaledImageRect = CGRect.zero
+
+    let aspectWidth:CGFloat = size.width / self.size.width
+    let aspectHeight:CGFloat = size.height / self.size.height
+    let aspectRatio:CGFloat = min(aspectWidth, aspectHeight)
+
+    scaledImageRect.size.width = self.size.width * aspectRatio
+    scaledImageRect.size.height = self.size.height * aspectRatio
+    scaledImageRect.origin.x = 0
+    scaledImageRect.origin.y = 0
+
+    UIGraphicsBeginImageContextWithOptions(size, false, 0)
+
+    self.draw(in: scaledImageRect)
+
+    let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+    
+    return scaledImage
+  }
+}
