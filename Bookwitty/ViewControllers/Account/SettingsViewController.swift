bde4a4dcd6187ac6301f2e9b2450e5b0645aaec6
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-02-16 11:06:14 +0200

    Add the settings view controller

---
+++ Bookwitty/ViewControllers/Account/SettingsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsViewController.swift
+//  Bookwitty
+//
+//  Created by charles on 2/16/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+}
