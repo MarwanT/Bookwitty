068a3fef4de3924423112726e19a3c294c746612
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-02-16 11:25:29 +0200

    Add the view model

---
+++ Bookwitty/ViewModels/Account/SettingsViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsViewModel.swift
+//  Bookwitty
+//
+//  Created by charles on 2/16/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+final class SettingsViewModel {
+
+}
