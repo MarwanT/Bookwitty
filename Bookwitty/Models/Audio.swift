0b29f2a9a975c1287bf6f3ef99073c69209bf0be
Shafic Hariri <shafic.hariri@keeward.com>
2017-02-16 20:46:47 +0200

    Develop Audio model

--- Bookwitty/Models/Audio.swift
+++ Bookwitty/Models/Audio.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+import Spine
+
+class Audio: Resource {
+  var createdAt: String?
+  var updatedAt: String?
+  var userId: String?
+  var thumbnailImageUrl: String?
+  var coverImageUrl: String?
+  var type: String?
+  var title: String?
+  var shortDescription: String?
+  var caption: String?
+  //TODO: Check if media_link is a related object or a dictionary
+  var media: [String: Any]?
+
+  override class var resourceType: ResourceType {
+    return "audios"
+  }
+
+  override class var fields: [Field] {
+    return fieldsFromDictionary([
+      "createdAt": Attribute().serializeAs("created-at"),
+      "updatedAt": Attribute().serializeAs("updated-at"),
+      "userId": Attribute().serializeAs("user-id"),
+      "thumbnailImageUrl": Attribute().serializeAs("thumbnail-image-url"),
+      "coverImageUrl": Attribute().serializeAs("cover-image-url"),
+      "caption": Attribute().serializeAs("caption"),
+      "shortDescription": Attribute().serializeAs("short-description"),
+      "title": Attribute().serializeAs("title"),
+      "type": Attribute().serializeAs("type"),
+      "media": Attribute().serializeAs("media")
+      ])
+  }
+}
+
