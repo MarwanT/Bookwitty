a545c130f898c01b775c1a7be4700814d27edece
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-02-16 19:58:27 +0200

    Create root tab bar view model

---
+++ Bookwitty/ViewModels/Root/RootTabBarViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  RootTabBarViewModel.swift
+//  Bookwitty
+//
+//  Created by Marwan  on 2/16/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+final class RootTabBarViewModel {
+}
