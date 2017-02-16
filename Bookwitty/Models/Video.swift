3e762d24349d98568384346d12eb176cba445635
Shafic Hariri <shafic.hariri@keeward.com>
2017-02-16 20:39:16 +0200

    Develop Video model

--- Bookwitty/Models/Video.swift
+++ Bookwitty/Models/Video.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+import Spine
+
+class Video: Resource {
+  var createdAt: String?
+  var updatedAt: String?
+  var userId: String?
+  var thumbnailImageUrl: String?
+  var coverImageUrl: String?
+  var type: String?
+  var title: String?
+  var shortDescription: String?
+  var caption: String?
+
+  override class var resourceType: ResourceType {
+    return "videos"
+  }
+
+  override class var fields: [Field] {
+    return fieldsFromDictionary([
+      "createdAt": Attribute().serializeAs("created-at"),
+      "updatedAt": Attribute().serializeAs("updated-at"),
+      "userId": Attribute().serializeAs("user-id"),
+      "thumbnailImageUrl": Attribute().serializeAs("thumbnail-image-url"),
+      "coverImageUrl": Attribute().serializeAs("cover-image-url"),
+      "type": Attribute().serializeAs("type"),
+      "title": Attribute().serializeAs("title"),
+      "shortDescription": Attribute().serializeAs("short-description"),
+      "caption": Attribute().serializeAs("caption")
+      ])
+  }
+}
+
