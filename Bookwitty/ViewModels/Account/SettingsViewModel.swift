0af27bd3229bad963348f7cb75bc3bfa136197c3
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-02-16 13:08:55 +0200

    Add the specific settings values for general and singout

--- Bookwitty/ViewModels/Account/SettingsViewModel.swift
+++ Bookwitty/ViewModels/Account/SettingsViewModel.swift
@@ -32,4 +32,39 @@ final class SettingsViewModel {
     sectionTitles = ["", ""]
   }
 
+  //General
+  private func valuesForGeneral(atRow row: Int) -> (title: String, value: Any) {
+    switch row {
+    case 0: //email
+      return (emailNotificationsText, false)
+    case 1: //change password
+      return (changePasswordText, "")
+    case 2: //country/region
+      return (countryRegionText, "")
+    default:
+      return ("", "")
+    }
+  }
+
+  private func accessoryForGeneral(atRow row: Int) -> Accessory {
+    switch row {
+    case 0: //email
+      return .Switch
+    case 1: //change password
+      return .Disclosure
+    case 2: //country/region
+      return .Disclosure
+    default:
+      return .None
+    }
+  }
+
+  //Sign Out
+  private func valuesForSignOut(atRow row: Int) -> (title: String, value: String) {
+    return (signOutText, "")
+  }
+
+  private func accessoryForSignOut(atRow row: Int) -> Accessory {
+      return .None
+  }
 }
