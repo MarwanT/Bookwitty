170d2bf744fb8aefd1a86a08f9dec170910a010a
Shafic Hariri <shafic.hariri@keeward.com>
2017-02-16 19:31:00 +0200

    Develop PageAuthor model

--- Bookwitty/Models/PageAuthor.swift
+++ Bookwitty/Models/PageAuthor.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+import Spine
+
+class PageAuthor: Resource {
+
+  var name: String?
+  var imageUrl: String?
+  var biography: String?
+  var shortDescription: String?
+
+  override class var resourceType: ResourceType {
+    return "page-authors"
+  }
+
+  override class var fields: [Field] {
+    return fieldsFromDictionary([
+      "name": Attribute().serializeAs("name"),
+      "imageUrl": Attribute().serializeAs("image-url"),
+      "biography": Attribute().serializeAs("biography"),
+      "shortDescription": Attribute().serializeAs("short-description")
+      ])
+  }
+}
+
