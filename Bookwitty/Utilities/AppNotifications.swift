183b3fb28900c16753c769554d1505bef040e7b6
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-02-16 21:23:11 +0200

    Add ‘didSignIn’ notification name

---
+++ Bookwitty/Utilities/AppNotifications.swift
@@ -0,0 +1,16 @@
+//
+//  AppNotifications.swift
+//  Bookwitty
+//
+//  Created by Marwan  on 2/1/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+struct AppNotification {
+  struct Name {
+    public static let failToRefreshToken: Notification.Name = Notification.Name(rawValue: "AppNotification.Name.failToRefreshToken")
+    public static let didSignIn: Notification.Name = Notification.Name(rawValue: "AppNotification.Name.didSignIn")
+  }
+}
