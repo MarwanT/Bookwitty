4ec8eff2cb14387df5920290a5cf4bd8890f8f1f
Shafic Hariri <shafic.hariri@keeward.com>
2017-02-16 18:44:43 +0200

    Develop the Image model

--- Bookwitty/Models/Image.swift
+++ Bookwitty/Models/Image.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+import Spine
+
+class Image: Resource {
+  var createdAt: String?
+  var updatedAt: String?
+  var userId: String?
+  var thumbnailImageUrl: String?
+  var coverImageUrl: String?
+  var caption: String?
+  var shortDescription: String?
+  var title: String?
+  var type: String?
+  var media: [String: Any]?
+
+  override class var resourceType: ResourceType {
+    return "images"
+  }
+
+  override class var fields: [Field] {
+    return fieldsFromDictionary([
+      "createdAt": Attribute().serializeAs("created-at"),
+      "updatedAt": Attribute().serializeAs("updated-at"),
+      "userId": Attribute().serializeAs("user-id"),
+      "thumbnailImageUrl": Attribute().serializeAs("thumbnail-image-url"),
+      "coverImageUrl": Attribute().serializeAs("cover-image-url"),
+      "caption": Attribute().serializeAs("caption"),
+      "shortDescription": Attribute().serializeAs("short-description"),
+      "title": Attribute().serializeAs("title"),
+      "type": Attribute().serializeAs("type"),
+      "media": Attribute().serializeAs("media")
+      ])
+  }
+}
+
