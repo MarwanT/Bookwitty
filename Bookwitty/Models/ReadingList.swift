521661b9ae279cc0ddefbdf6fb03c3a76ff8b9a8
Shafic Hariri <shafic.hariri@keeward.com>
2017-02-16 20:01:48 +0200

    Develop the Reading List model

--- Bookwitty/Models/ReadingList.swift
+++ Bookwitty/Models/ReadingList.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+import Spine
+
+class ReadingList: Resource {
+  var name: String?
+  var createdAt: String?
+  var updatedAt: String?
+  var userId: String?
+  var thumbnailImageUrl: String?
+  var coverImageUrl: String?
+  var title: String?
+  var shortDescription: String?
+  var conclusion: String?
+  var body: String?
+  //TODO: Check the full response for > relationships > posts model
+  
+  override class var resourceType: ResourceType {
+    return "reading-lists"
+  }
+
+  override class var fields: [Field] {
+    return fieldsFromDictionary([
+      "name": Attribute().serializeAs("name"),
+      "createdAt": Attribute().serializeAs("created-at"),
+      "updatedAt": Attribute().serializeAs("updated-at"),
+      "userId": Attribute().serializeAs("user-id"),
+      "thumbnailImageUrl": Attribute().serializeAs("thumbnail-image-url"),
+      "coverImageUrl": Attribute().serializeAs("cover-image-url"),
+      "shortDescription": Attribute().serializeAs("short-description"),
+      "title": Attribute().serializeAs("title"),
+      "conclusion": Attribute().serializeAs("conclusion"),
+      "body": Attribute().serializeAs("body")
+      ])
+  }
+}
+
