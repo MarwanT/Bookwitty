5f040dd3480f16110249728d11e21d072a59c601
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-02-17 02:53:46 +0200

    Add navigate to registration notifications

--- Bookwitty/Utilities/AppNotifications.swift
+++ Bookwitty/Utilities/AppNotifications.swift
@@ -12,5 +12,7 @@ struct AppNotification {
   struct Name {
     public static let failToRefreshToken: Notification.Name = Notification.Name(rawValue: "AppNotification.Name.failToRefreshToken")
     public static let didSignIn: Notification.Name = Notification.Name(rawValue: "AppNotification.Name.didSignIn")
+    public static let rootShouldDisplayRegistration: Notification.Name = Notification.Name(rawValue: "AppNotification.Name.rootShouldDisplayRegistration")
+    public static let introductionShouldDisplayRegistration: Notification.Name = Notification.Name(rawValue: "AppNotification.Name.introductionShouldDisplayRegistration")
   }
 }
