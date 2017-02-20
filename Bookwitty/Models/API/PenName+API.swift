bb4b2999ed8c96e4f72b3c304c1b1478a3a7bada
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-02-20 22:14:21 +0200

    Add the update post body helper function

--- Bookwitty/Models/API/PenName+API.swift
+++ Bookwitty/Models/API/PenName+API.swift
@@ -12,3 +12,25 @@ import Moya
 struct PenNameAPI {
 
 }
+
+//MARK: - Moya Needed parameters
+extension PenNameAPI {
+  static func updatePostBody(identifier: String, name: String?, biography: String?, avatarUrl: String?, facebookUrl: String?, tumblrUrl: String?, googlePlusUrl: String?, twitterUrl: String?, instagramUrl: String?, pinterestUrl: String?, youtubeUrl: String?, linkedinUrl: String?, wordpressUrl: String?, websiteUrl: String?) -> [String : Any]? {
+    let penName = PenName()
+    penName.id = identifier
+    penName.name = name
+    penName.biography = biography
+    penName.avatarUrl = avatarUrl
+    penName.facebookUrl = facebookUrl
+    penName.tumblrUrl = tumblrUrl
+    penName.googlePlusUrl = googlePlusUrl
+    penName.twitterUrl = twitterUrl
+    penName.instagramUrl = instagramUrl
+    penName.pinterestUrl = pinterestUrl
+    penName.youtubeUrl = youtubeUrl
+    penName.linkedinUrl = linkedinUrl
+    penName.wordpressUrl = wordpressUrl
+    penName.websiteUrl = websiteUrl
+    return penName.serializeData(options: [.IncludeID, .OmitNullValues])
+  }
+}
