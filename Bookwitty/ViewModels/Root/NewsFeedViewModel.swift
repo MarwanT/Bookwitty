af51e4ea5f4843daf17d58fdb791c025090ac881
Shafic Hariri <shafic.hariri@keeward.com>
2017-02-20 22:28:33 +0200

    Request the news feed and set the data

--- Bookwitty/ViewModels/Root/NewsFeedViewModel.swift
+++ Bookwitty/ViewModels/Root/NewsFeedViewModel.swift
@@ -7,11 +7,21 @@
 //
 
 import Foundation
+import Moya
 import Spine
 
 final class NewsFeedViewModel {
+  var cancellableRequest:  Cancellable?
   let viewController = localizedString(key: "news", defaultValue: "News")
   var data: [Resource] = []
+  var selectedPenNameId: String = "6c08337a-108f-4335-b2d0-3a25b9fe6bed"
+
+  func loadNewsfeed(completionBlock: @escaping (_ success: Bool) -> ()) {
+    cancellableRequest = NewsfeedAPI.feed(forPenName: selectedPenNameId) { (success, resources, error) in
+      self.data = resources ?? []
+      completionBlock(success)
+    }
+  }
 
   func numberOfSections() -> Int {
     return data.count > 0 ? 1 : 0
