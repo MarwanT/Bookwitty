94ce62a3c969d2b1badaf39ea549dd3253db0013
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-02-20 21:38:03 +0200

    Add the pen name api file

---
+++ Bookwitty/Models/API/PenName+API.swift
@@ -0,0 +1,14 @@
+//
+//  PenName+API.swift
+//  Bookwitty
+//
+//  Created by charles on 2/20/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+struct PenNameAPI {
+
+}
