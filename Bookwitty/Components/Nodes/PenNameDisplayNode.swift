efd8f636d9f7c0bbab0d829392da716cdea6410c
Shafic Hariri <shafic.hariri@keeward.com>
2017-02-20 13:02:47 +0200

    PenName Display Node: Create and initialize class properties

--- Bookwitty/Components/Nodes/PenNameDisplayNode.swift
+++ Bookwitty/Components/Nodes/PenNameDisplayNode.swift
@@ -10,7 +10,40 @@ import Foundation
 import AsyncDisplayKit
 
 class PenNameDisplayNode: ASControlNode {
-  override init() {
+  private let internalMargin = ThemeManager.shared.currentTheme.cardInternalMargin()
+  private let downArrowImageSize: CGSize = CGSize(width: 45, height: 45)
+
+  private let penNameTextNode: ASTextNode
+  private let separatorNode: ASDisplayNode
+  private let downArrowImageNode: ASImageNode
+
+  private var nodeHeight: CGFloat
+
+  var delegate: PenNameDisplayNodeDelegate?
+  var shouldExpand: Bool = true {
+    didSet {
+    }
+  }
+  var penNameSummary: String? {
+    didSet {
+    }
+  }
+
+  private override init() {
+    penNameTextNode = ASTextNode()
+    separatorNode = ASDisplayNode()
+    downArrowImageNode = ASImageNode()
+    nodeHeight = 45.0 // Temporary value
     super.init()
+    automaticallyManagesSubnodes = true
+  }
+
+  convenience init(withCellHeight cellHeight: CGFloat) {
+    self.init()
+    nodeHeight = cellHeight
+    setupNodes()
+  }
+
+  func setupNodes() {
   }
 }
