ebf734f74957e7e3bd1ce46a228177b63fc7ae3b
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-02-21 19:36:32 +0200

    Add books table view model

---
+++ Bookwitty/ViewModels/Books/BooksTableViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  BooksTableViewModel.swift
+//  Bookwitty
+//
+//  Created by Marwan  on 2/21/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+final class BooksTableViewModel {
+  
+}
