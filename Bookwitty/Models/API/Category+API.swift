a0337992ae17d5f14485c4fc71179433b75b3e6d
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-02-21 12:58:21 +0200

    Integrate the category curated collection API

--- Bookwitty/Models/API/Category+API.swift
+++ Bookwitty/Models/API/Category+API.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+import Moya
+
+struct CategoryAPI {
+  static func categoryCuratedContent(categoryIdentifier: String, completion: @escaping (_ success: Bool, _ collection: CuratedCollection?, _ error: BookwittyAPIError?) -> Void) -> Cancellable? {
+    return signedAPIRequest(target: .categoryCuratedContent(categoryIdentifier: categoryIdentifier), completion: {
+      (data, statusCode, response, error) in
+      var success: Bool = false
+      var collection: CuratedCollection? = nil
+      var error: BookwittyAPIError? = nil
+      defer {
+        completion(success, collection, error)
+      }
+      
+      if let data = data {
+        collection = CuratedCollection.parseData(data: data)
+        success = collection != nil
+      } else {
+        error = BookwittyAPIError.failToParseData
+      }
+    })
+  }
+}
