768a006547d210d748dec2c3a0edc167aab384bf
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-02-22 09:23:41 +0200

    Add the static helper for the search api

--- Bookwitty/Models/API/Search+API.swift
+++ Bookwitty/Models/API/Search+API.swift
@@ -7,7 +7,27 @@
 //
 
 import UIKit
+import Moya
 
 struct SearchAPI {
+  static func search(filter: (query: String?, category: [String]?)?, page: (number: String?, size: String?)?, completion: @escaping (_ success: Bool, _ collection: [ModelResource]?, _ error: BookwittyAPIError?) -> Void) -> Cancellable? {
+    return signedAPIRequest(target: BookwittyAPI.Search(filter: filter, page: page)) {
+      (data, statusCode, response, error) in
+      var success: Bool = false
+      var collection: [ModelResource]? = nil
+      var error: BookwittyAPIError? = nil
+      defer {
+        completion(success, collection, error)
+      }
 
+      guard let data = data else {
+        error = BookwittyAPIError.failToParseData
+        return
+      }
+      // Parse Data
+      collection = Parser.parseDataArray(data: data)
+      success = true
+      error = nil
+    }
+  }
 }
