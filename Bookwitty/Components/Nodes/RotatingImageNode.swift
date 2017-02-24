497c7a77f5f40f93813ce72f98d83995747e8a00
Shafic Hariri <shafic.hariri@keeward.com>
2017-02-24 11:26:35 +0200

    Create the Direction enum

--- Bookwitty/Components/Nodes/RotatingImageNode.swift
+++ Bookwitty/Components/Nodes/RotatingImageNode.swift
@@ -10,11 +10,20 @@ import Foundation
 import AsyncDisplayKit
 
 class RotatingImageNode: ASImageNode {
+  enum Direction {
+    case up
+    case down
+    case left
+    case right
+  }
+
   private let imageSize: CGSize = CGSize(width: 45, height: 45)
   private var angle: CATransform3D {
     return CATransform3DMakeRotation(0.0, 0.0, 0.0, 1.0) // Rotation Transform 
   }
 
+  var currentDirection: Direction = .down
+  
   private override init() {
     super.init()
   }
