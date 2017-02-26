cd817e5aa41176cbb1818171e26277122d9f125e
Shafic Hariri <shafic.hariri@keeward.com>
2017-02-26 21:41:48 +0200

    Implement the getPenNames API

--- Bookwitty/Models/API/PenName+API.swift
+++ Bookwitty/Models/API/PenName+API.swift
@@ -10,6 +10,28 @@ import Foundation
 import Moya
 
 struct PenNameAPI {
+  public static func getPenNames(completionBlock: @escaping (_ success: Bool, _ penNames: [PenName]?, _ error: BookwittyAPIError?)->()) -> Cancellable? {
+    return signedAPIRequest(target: BookwittyAPI.penNames(), completion: { (data: Data?, statusCode: Int?, response: URLResponse?, error: BookwittyAPIError?) in
+      var penNames: [PenName]? = nil
+      var error: BookwittyAPIError? = nil
+      var success: Bool = false
+      defer {
+        completionBlock(success, penNames, error)
+      }
+
+      if let data = data {
+        penNames = PenName.parseDataArray(data: data)
+        success = penNames != nil
+
+        if let penNames = penNames {
+          UserManager.shared.penNames = penNames
+        }
+      } else {
+        error = BookwittyAPIError.failToParseData
+      }
+    })
+  }
+
   public static func updatePenName(identifier: String, name: String?, biography: String?, avatarUrl: String?, facebookUrl: String?, tumblrUrl: String?, googlePlusUrl: String?, twitterUrl: String?, instagramUrl: String?, pinterestUrl: String?, youtubeUrl: String?, linkedinUrl: String?, wordpressUrl: String?, websiteUrl: String?, completionBlock: @escaping (_ success: Bool, _ penName: PenName?, _ error: BookwittyAPIError?)->()) -> Cancellable? {
     return signedAPIRequest(target: BookwittyAPI.updatePenName(identifier: identifier, name: name, biography: biography, avatarUrl: avatarUrl, facebookUrl: facebookUrl, tumblrUrl: tumblrUrl, googlePlusUrl: googlePlusUrl, twitterUrl: twitterUrl, instagramUrl: instagramUrl, pinterestUrl: pinterestUrl, youtubeUrl: youtubeUrl, linkedinUrl: linkedinUrl, wordpressUrl: wordpressUrl, websiteUrl: websiteUrl), completion: {
       (data, statusCode, response, error) in
