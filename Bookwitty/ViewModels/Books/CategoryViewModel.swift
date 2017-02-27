4ce4f93ba18e5515637b73f5f9ffb1d806b5f660
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-02-27 17:03:41 +0200

    Create ‘Category View Model’

---
+++ Bookwitty/ViewModels/Books/CategoryViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  CategoryViewModel.swift
+//  Bookwitty
+//
+//  Created by Marwan  on 2/27/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+final class CategoryViewModel {
+  
+}
