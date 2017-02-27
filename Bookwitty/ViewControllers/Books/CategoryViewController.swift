e1e7f14ea8c416a177882486c83aa6696c8b5c5c
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-02-27 19:27:17 +0200

    Define main subviews in category view controller

--- Bookwitty/ViewControllers/Books/CategoryViewController.swift
+++ Bookwitty/ViewControllers/Books/CategoryViewController.swift
@@ -12,9 +12,18 @@ class CategoryViewController: UIViewController {
   @IBOutlet weak var scrollView: UIScrollView!
   @IBOutlet weak var stackView: UIStackView!
   
+  let banner = BannerView()
+  let featuredContentCollectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewLayout())
+  let bookwittySuggestsTableView = UITableView(frame: CGRect.zero, style: UITableViewStyle.plain)
+  let selectionTableView = UITableView(frame: CGRect.zero, style: UITableViewStyle.plain)
+  let viewAllBooksView = UIView.loadFromView(DisclosureView.self, owner: nil)
+  let viewAllSelectionsView = UIView.loadFromView(DisclosureView.self, owner: nil)
+  
   let viewModel = CategoryViewModel()
   
   override func viewDidLoad() {
     super.viewDidLoad()
   }
+  
+  
 }
