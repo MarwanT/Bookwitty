27a635fbab58b2b6ca5e33d26d61e8ee64c0a540
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-02-27 13:53:44 +0200

    Create Supplier information class

--- Bookwitty/Models/Book.swift
+++ Bookwitty/Models/Book.swift
@@ -107,6 +107,33 @@ class ProductDetails: NSObject {
   }
 }
 
+
+// MARK: - Supplier Information
+class SupplierInformation: NSObject {
+  var quantity: Int?
+  var listPrice: Price?
+  var price: Price?
+  var userPrice: Price?
+  
+  override init() {
+    super.init()
+  }
+  
+  init(for dictionary: [String : Any]) {
+    super.init()
+    setValues(dictionary: dictionary)
+  }
+  
+  private func setValues(dictionary: [String : Any]) {
+    let json = JSON(dictionary)
+    self.quantity = json["quantity"].intValue
+    self.listPrice = Price(for: json["list-price"].dictionaryObject)
+    self.price = Price(for: json["price"].dictionaryObject)
+    self.userPrice = Price(for: json["user-price"].dictionaryObject)
+  }
+}
+
+
 // MARK: - Price
 struct Price {
   var currency: String?
