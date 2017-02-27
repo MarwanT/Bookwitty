185f029b43d67732fbb7361e6bce228e70dba023
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-02-27 17:34:30 +0200

    Add method that loads a category curated content

--- Bookwitty/ViewModels/Books/CategoryViewModel.swift
+++ Bookwitty/ViewModels/Books/CategoryViewModel.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import Moya
 
 final class CategoryViewModel {
   fileprivate var curatedCollection: CuratedCollection? = nil
@@ -14,4 +15,40 @@ final class CategoryViewModel {
   fileprivate var categoryBooks: [Book]? = nil
   fileprivate var readingLists: [ReadingList]? = nil
   fileprivate var banner: Banner? = nil
+  
+  
+  // MARK: API Calls
+
+  private func loadCuratedContent(categoryIdentifier: String, completion: @escaping (_ success: Bool, _ identifiers: [String]? , _ error: BookwittyAPIError?) -> Void) -> Cancellable? {
+    return CategoryAPI.categoryCuratedContent(categoryIdentifier: categoryIdentifier, completion: {
+      (success, collection, error) in
+      var identifiers = [String]()
+      
+      guard success, let collection = collection else {
+        completion(false, nil, BookwittyAPIError.undefined)
+        return
+      }
+      
+      self.curatedCollection = collection
+      self.featuredContents = nil
+      self.categoryBooks = nil
+      self.readingLists = nil
+      self.banner = nil
+      
+      guard let sections = collection.sections  else {
+        completion(true, nil, nil)
+        return
+      }
+      
+      if let featuredContent = sections.featuredContent {
+        let featuredContentIdentifiers = featuredContent.flatMap({ $0.wittyId })
+        identifiers += featuredContentIdentifiers
+      }
+      if let readingListIdentifiers = sections.readingListIdentifiers {
+        identifiers += readingListIdentifiers
+      }
+      
+      completion(success, identifiers, error)
+    })
+  }
 }
