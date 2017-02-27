b9e978e1a9c24ab6b1cdb08a8f814884e80b6212
Shafic Hariri <shafic.hariri@keeward.com>
2017-02-27 14:03:39 +0200

    Remove commented line

--- Bookwitty/ViewControllers/Root/NewsFeedViewController.swift
+++ Bookwitty/ViewControllers/Root/NewsFeedViewController.swift
@@ -95,7 +95,6 @@ class NewsFeedViewController: ASViewController<ASCollectionNode> {
   }
 
   func reloadPenNamesNode() {
-    //collectionNode.reloadData()
     penNameSelectionNode.loadData(penNames: viewModel.penNames, withSelected: viewModel.defaultPenName)
   }
 }
