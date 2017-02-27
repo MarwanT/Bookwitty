0d4f3c5a6f2dd6189340d83e2d7b002f68c14567
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-02-27 19:37:18 +0200

    Initialize category featured content collection view

--- Bookwitty/ViewControllers/Books/CategoryViewController.swift
+++ Bookwitty/ViewControllers/Books/CategoryViewController.swift
@@ -19,10 +19,60 @@ class CategoryViewController: UIViewController {
   let viewAllBooksView = UIView.loadFromView(DisclosureView.self, owner: nil)
   let viewAllSelectionsView = UIView.loadFromView(DisclosureView.self, owner: nil)
   
+  fileprivate let leftMargin = ThemeManager.shared.currentTheme.generalExternalMargin()
+  
   let viewModel = CategoryViewModel()
   
   override func viewDidLoad() {
     super.viewDidLoad()
     title = viewModel.viewControllerTitle
+    
+    initializeSubviews()
+  }
+  
+  private func initializeSubviews() {
+    // Featured Content View
+    let itemSize = FeaturedContentCollectionViewCell.defaultSize
+    let interItemSpacing: CGFloat = 10
+    let contentInset = UIEdgeInsets(
+      top: 15, left: ThemeManager.shared.currentTheme.generalExternalMargin(),
+      bottom: 10, right: ThemeManager.shared.currentTheme.generalExternalMargin())
+    let flowLayout = UICollectionViewFlowLayout()
+    flowLayout.scrollDirection = UICollectionViewScrollDirection.horizontal
+    flowLayout.itemSize = itemSize
+    flowLayout.minimumInteritemSpacing = interItemSpacing
+    featuredContentCollectionView.collectionViewLayout = flowLayout
+    featuredContentCollectionView.register(FeaturedContentCollectionViewCell.nib, forCellWithReuseIdentifier: FeaturedContentCollectionViewCell.reuseIdentifier)
+    featuredContentCollectionView.dataSource = self
+    featuredContentCollectionView.delegate = self
+    featuredContentCollectionView.backgroundColor = UIColor.clear
+    featuredContentCollectionView.contentInset = contentInset
+    featuredContentCollectionView.showsHorizontalScrollIndicator = false
+    featuredContentCollectionView.constrainHeight("\(itemSize.height + contentInset.top + contentInset.bottom)")
+    featuredContentCollectionView.constrainWidth("\(self.view.frame.width)")
+  }
+}
+
+
+// MARK: - Featured Content Collection Delegates
+
+extension CategoryViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return viewModel.featuredContentNumberOfItems
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FeaturedContentCollectionViewCell.reuseIdentifier, for: indexPath) as? FeaturedContentCollectionViewCell else {
+      return UICollectionViewCell()
+    }
+    
+    let data = viewModel.featuredContentValues(for: indexPath)
+    cell.title = data.title
+    cell.imageURL = data.imageURL
+    return cell
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    // TODO: Handle featured content selection
   }
 }
