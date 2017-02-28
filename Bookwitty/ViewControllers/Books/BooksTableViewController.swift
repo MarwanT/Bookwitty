2eb0b8c8166b8932feda395c9566b77948ce0ea9
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-02-28 23:55:32 +0200

    Declare books table view model as variable instead of a constant

--- Bookwitty/ViewControllers/Books/BooksTableViewController.swift
+++ Bookwitty/ViewControllers/Books/BooksTableViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class BooksTableViewController: UITableViewController {
-  let viewModel = BooksTableViewModel()
+  var viewModel = BooksTableViewModel()
   
   override func viewDidLoad() {
     super.viewDidLoad()
