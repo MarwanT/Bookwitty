8d0cdc859df884221174450f2237876bac573eb0
Shafic Hariri <shafic.hariri@keeward.com>
2017-02-28 20:21:16 +0200

    Develop the Paginator logic

--- Bookwitty/Utilities/Paginator.swift
+++ Bookwitty/Utilities/Paginator.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+
+class Paginator {
+  private var ids: [String]
+  private var pageSize: Int
+  private var startIndex: Int
+
+  init(ids: [String] ,pageSize: Int = 10, startPage: Int = 0) {
+    self.ids = ids
+    self.pageSize = pageSize
+    self.startIndex = min((startPage * pageSize), ids.endIndex)
+  }
+
+  func nextPageIds() -> [String]? {
+    guard startIndex < ids.endIndex else {
+      return nil
+    }
+    let endIndex = min((startIndex + pageSize), ids.endIndex)
+    let nextRangeIds: ArraySlice<String> = ids[startIndex..<endIndex]
+
+    startIndex = endIndex
+
+    return Array(nextRangeIds)
+  }
+}
