f0173d957c919ef45c994f815bdefe7d4abfac2f
Shafic Hariri <shafic.hariri@keeward.com>
2017-02-28 16:58:50 +0200

    Initialize view model

--- Bookwitty/ViewControllers/Access/DiscoverViewController.swift
+++ Bookwitty/ViewControllers/Access/DiscoverViewController.swift
@@ -14,6 +14,8 @@ class DiscoverViewController: ASViewController<ASCollectionNode> {
   let collectionNode: ASCollectionNode
   let flowLayout: UICollectionViewFlowLayout
 
+  let viewModel = DiscoverViewModel()
+  
   required init?(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
