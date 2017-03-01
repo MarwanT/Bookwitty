008b1259b1b0600f9ea047e71244d0224f048140
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-03-01 17:10:58 +0200

    Add the segmented control view

---
+++ Bookwitty/Components/Views/Segmented/SegmentedControl.swift
@@ -0,0 +1,13 @@
+//
+//  SegmentedControl.swift
+//  Bookwitty
+//
+//  Created by charles on 3/1/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+final class SegmentedControl: UIView {
+
+}
