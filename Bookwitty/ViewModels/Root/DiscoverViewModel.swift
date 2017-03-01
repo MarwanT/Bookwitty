30b675629cff817fa0a75115545975e8b22208d2
Shafic Hariri <shafic.hariri@keeward.com>
2017-03-01 12:35:04 +0200

    Update Discover Data: dataIdentifiers, reset the data array and initialize the Paginator

--- Bookwitty/ViewModels/Root/DiscoverViewModel.swift
+++ Bookwitty/ViewModels/Root/DiscoverViewModel.swift
@@ -18,24 +18,25 @@ final class DiscoverViewModel {
 
   func loadDiscoverData(completionBlock: @escaping (_ success: Bool) -> ()) {
     cancellableRequest = DiscoverAPI.discover { (success, curatedCollection, error) in
-      defer {
-        completionBlock(success)
-      }
-
       guard let sections = curatedCollection?.sections else {
+        completionBlock(false)
         return
       }
-
-      if let booksIdentifiers = sections.booksIdentifiers {
-        self.dataIdentifiers += booksIdentifiers
-      }
-      if let readingListIdentifiers = sections.readingListIdentifiers {
-        self.dataIdentifiers += readingListIdentifiers
-      }
+      //Note: We may need Add booksIdentifiers and readingListIdentifiers later
       if let featuredContent = sections.featuredContent {
-        self.dataIdentifiers += featuredContent
+        self.dataIdentifiers = featuredContent
       }
+      //Reset data
+      self.data = []
+      self.paginator = Paginator(ids: self.dataIdentifiers)
+      self.loadNextPage(completionBlock: completionBlock)
     }
   }
 
+  func loadNextPage(completionBlock: @escaping (_ success: Bool) -> ()) {
+    //TODO: load Next Page
+    defer {
+      completionBlock(success)
+    }
+  }
 }
