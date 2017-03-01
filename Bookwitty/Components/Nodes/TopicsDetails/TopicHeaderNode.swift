521ce6ec38d162f0d22aa0c484d62fba6dfc5b39
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-03-01 12:16:50 +0200

    Add the header node

---
+++ Bookwitty/Components/Nodes/TopicsDetails/TopicHeaderNode.swift
@@ -0,0 +1,22 @@
+//
+//  TopicHeaderNode.swift
+//  Bookwitty
+//
+//  Created by charles on 2/28/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+import AsyncDisplayKit
+
+class TopicHeaderNode: ASDisplayNode {
+	
+  override init() {
+    super.init()
+    setupNode()
+  }
+
+  private func setupNode() {
+    
+  }
+}
