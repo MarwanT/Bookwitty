b27eb5bd7c4d86031747a19e16912f405e978296
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-03-01 15:08:31 +0200

    Create book details view model class

---
+++ Bookwitty/ViewModels/Books/BookDetailsViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  BookDetailsViewModel.swift
+//  Bookwitty
+//
+//  Created by Marwan  on 3/1/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+final class BookDetailsViewModel {
+  
+}
