1c0b8466e1c1b3918bbffc029be73c4128e2e61a
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-03-01 15:00:50 +0200

    Create Book Details VC

---
+++ Bookwitty/ViewControllers/Books/BookDetailsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BookDetailsViewController.swift
+//  Bookwitty
+//
+//  Created by Marwan  on 3/1/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+import AsyncDisplayKit
+
+class BookDetailsViewController: ASViewController<ASDisplayNode> {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+  }
+}
