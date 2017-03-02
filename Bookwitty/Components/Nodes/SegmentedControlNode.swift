70d8e0ab379b9c595640cd50325873cc7ec62689
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-03-02 13:12:18 +0200

    Add the segment control node

---
+++ Bookwitty/Components/Nodes/SegmentedControlNode.swift
@@ -0,0 +1,13 @@
+//
+//  SegmentedControlNode.swift
+//  Bookwitty
+//
+//  Created by charles on 3/2/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class SegmentedControlNode: ASDisplayNode {
+
+}
