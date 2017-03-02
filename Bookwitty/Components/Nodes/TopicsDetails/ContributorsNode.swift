bccb73110bb2e1ee7f3867588f9e395be9d36a4c
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-03-02 13:18:57 +0200

    Add the contributors node

---
+++ Bookwitty/Components/Nodes/TopicsDetails/ContributorsNode.swift
@@ -0,0 +1,14 @@
+//
+//  ContributorsNode.swift
+//  Bookwitty
+//
+//  Created by charles on 3/2/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+import AsyncDisplayKit
+
+class ContributorsNode: ASDisplayNode {
+
+}
