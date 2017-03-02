7c23f462c596d1fb1abeb4bba05c9099aa6174c7
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-03-02 17:12:23 +0200

    Add the layout specs that fit to layout the contributors
    limit the images nodes to a certain limit from the calculated size

--- Bookwitty/Components/Nodes/TopicsDetails/ContributorsNode.swift
+++ Bookwitty/Components/Nodes/TopicsDetails/ContributorsNode.swift
@@ -28,6 +28,41 @@ class ContributorsNode: ASDisplayNode {
   private func setupNode() {
     statsNode.maximumNumberOfLines = 1
   }
+
+  override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
+    var nodesArray: [ASLayoutElement] = []
+
+
+    let canFitImageCount: Int = Int(ceil((constrainedSize.max.width - (statsNode.calculatedSize.width + internalMargin + 1.0)) / (imageSize.width + 5.0)))
+
+
+    while imagesNodes.count > canFitImageCount {
+      _ = imagesNodes.popLast()
+    }
+
+    let imagesStackSpec = ASStackLayoutSpec(direction: .horizontal,
+                                                         spacing: 5,
+                                                         justifyContent: .end,
+                                                         alignItems: .center,
+                                                         children: imagesNodes)
+
+
+    nodesArray.append(imagesStackSpec)
+
+    nodesArray.append(spacer(flexGrow: 1.0))
+    nodesArray.append(spacer(width: internalMargin))
+
+    nodesArray.append(statsNode)
+
+    let horizontalSpec = ASStackLayoutSpec(direction: .horizontal,
+                                                         spacing: 0,
+                                                         justifyContent: .end,
+                                                         alignItems: .center,
+                                                         children: nodesArray)
+
+    let insetSpec = ASInsetLayoutSpec(insets: sideInset(), child: horizontalSpec)
+    return insetSpec
+  }
 }
 
 //Helpers
