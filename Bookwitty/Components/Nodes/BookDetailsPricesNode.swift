d147dbc1417d6af93aed5189015e9d48b200b692
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-03-03 21:14:27 +0200

    Add value setters functions

--- Bookwitty/Components/Nodes/BookDetailsPricesNode.swift
+++ Bookwitty/Components/Nodes/BookDetailsPricesNode.swift
@@ -32,6 +32,75 @@ class BookDetailsPricesNode: ASDisplayNode {
     listPriceTextNode.isLayerBacked = true
     savingTextNode.isLayerBacked = true
   }
+  
+  func set(supplierInformation: SupplierInformation?) {
+    guard let supplierInformation = supplierInformation else {
+      setNeedsLayout()
+      return
+    }
+    
+    // Set price
+    self.price = supplierInformation.preferredPrice?.formattedValue
+    
+    // Set user price if available
+    self.userPrice = supplierInformation.userPrice?.formattedValue
+    
+    // Set list prive and saving price if available
+    if let listPrice = supplierInformation.listPrice,
+      let price = supplierInformation.price,
+      let listPriceValue = listPrice.value,
+      let listPriceCurrency = listPrice.currency,
+      let priceValue = price.value,
+      let priceCurrency = price.currency,
+      listPriceCurrency == priceCurrency {
+      
+      let savedValue = listPriceValue - priceValue
+      if savedValue > 0 {
+        let savedPrice = Price(currency: priceCurrency, value: savedValue)
+        self.savingPrice = savedPrice.formattedValue
+        self.listPrice = listPrice.formattedValue
+      } else {
+        self.savingPrice = nil
+        self.listPrice = nil
+      }
+    } else {
+      self.savingPrice = nil
+      self.listPrice = nil
+    }
+    
+    setNeedsLayout()
+  }
+  
+  private var price: String? {
+    didSet {
+      priceTextNode.attributedText = AttributedStringBuilder(fontDynamicType: .headline)
+        .append(text: price ?? "", color: configuration.priceTextColor).attributedString
+      setNeedsLayout()
+    }
+  }
+  
+  private var userPrice: String? {
+    didSet {
+      userPriceTextNode.attributedText = AttributedStringBuilder(fontDynamicType: .caption1)
+        .append(text: "(\((userPrice ?? "")))", color: configuration.userPriceTextColor).attributedString
+      setNeedsLayout()
+    }
+  }
+  
+  private var listPrice: String? {
+    didSet {
+      listPriceTextNode.attributedText = AttributedStringBuilder(fontDynamicType: .caption1).append(text: (Strings.list_price() + ": "), color: configuration.defaultTextColor).append(text: listPrice ?? "", color: configuration.defaultTextColor, strikeThroughStyle: NSUnderlineStyle.styleSingle).attributedString
+      setNeedsLayout()
+    }
+  }
+  
+  private var savingPrice: String? {
+    didSet {
+      savingTextNode.attributedText = AttributedStringBuilder(fontDynamicType: .caption1)
+        .append(text: (Strings.you_save() + ": " + (savingPrice ?? "")), color: configuration.defaultTextColor).attributedString
+      setNeedsLayout()
+    }
+  }
 }
 
 extension BookDetailsPricesNode {
