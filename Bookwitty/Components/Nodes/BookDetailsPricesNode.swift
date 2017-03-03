56562cf59ac6a8a91191a13e79576de9fa9f720c
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-03-03 21:12:44 +0200

    Add book details prices node configuration

--- Bookwitty/Components/Nodes/BookDetailsPricesNode.swift
+++ Bookwitty/Components/Nodes/BookDetailsPricesNode.swift
@@ -14,11 +14,24 @@ class BookDetailsPricesNode: ASDisplayNode {
   fileprivate let listPriceTextNode: ASTextNode
   fileprivate let savingTextNode: ASTextNode
   
+  var configuration = Configuration()
+  
   override init() {
     priceTextNode = ASTextNode()
     userPriceTextNode = ASTextNode()
     listPriceTextNode = ASTextNode()
     savingTextNode = ASTextNode()
     super.init()
+  }
+}
+
+extension BookDetailsPricesNode {
+  struct Configuration {
+    let priceTextColor = ThemeManager.shared.currentTheme.defaultECommerceColor()
+    let userPriceTextColor = ThemeManager.shared.currentTheme.colorNumber15()
+    let defaultTextColor = ThemeManager.shared.currentTheme.defaultTextColor()
+    fileprivate var textEdgeInsets = UIEdgeInsetsMake(0, 5, 0, 5)
+    fileprivate var savingPricesEdgeInsets = UIEdgeInsetsMake(
+      ThemeManager.shared.currentTheme.generalExternalMargin() - 9, 0, 0, 0)
   }
 }
