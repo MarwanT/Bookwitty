eea96b58bdcd801a92d7127119902469809cfc83
Shafic Hariri <shafic.hariri@keeward.com>
2017-03-06 01:01:55 +0200

    Initialize the OnBoarding ItemHeader node

--- Bookwitty/Components/Nodes/OnBoardingItemHeaderNode.swift
+++ Bookwitty/Components/Nodes/OnBoardingItemHeaderNode.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+import AsyncDisplayKit
+
+class OnBoardingItemHeaderNode: ASDisplayNode {
+  fileprivate let internalMargin: CGFloat = ThemeManager.shared.currentTheme.cardInternalMargin()
+  static let nodeHeight: CGFloat = 45.0
+
+  private let titleTextNode: ASTextNode
+  private let rotatingImageNode: RotatingImageNode
+  private let separator: ASDisplayNode
+
+  override init() {
+    rotatingImageNode = RotatingImageNode(image: #imageLiteral(resourceName: "downArrow"), size: CGSize(width: 45.0, height: 45.0), direction: .right)
+    titleTextNode = ASTextNode()
+    separator = ASDisplayNode()
+
+    super.init()
+    automaticallyManagesSubnodes = true
+  }
+}
