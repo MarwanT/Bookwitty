4528ba21bf16bfc67b33ea2cc8e37172b03963ce
Shafic Hariri <shafic.hariri@keeward.com>
2017-03-06 01:15:03 +0200

    Initialize OnBoardingCellNode

--- Bookwitty/Components/Nodes/OnBoardingCellNode.swift
+++ Bookwitty/Components/Nodes/OnBoardingCellNode.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+import AsyncDisplayKit
+
+class OnBoardingCellNode: ASCellNode {
+  fileprivate let internalMargin: CGFloat = ThemeManager.shared.currentTheme.cardInternalMargin()
+  fileprivate let contentSpacing: CGFloat = ThemeManager.shared.currentTheme.contentSpacing()
+  fileprivate let collapsedCellHeight: CGFloat = 45.0
+
+
+  let headerNode: OnBoardingItemHeaderNode
+  let collectionNode: ASCollectionNode
+  let flowLayout: UICollectionViewFlowLayout
+
+  override init() {
+    headerNode = OnBoardingItemHeaderNode()
+    flowLayout = UICollectionViewFlowLayout()
+    flowLayout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    flowLayout.minimumInteritemSpacing  = 0
+    flowLayout.minimumLineSpacing       = 0
+    flowLayout.headerReferenceSize = CGSize(width: UIScreen.main.bounds.width, height: 45.0)
+
+    collectionNode = ASCollectionNode(collectionViewLayout: flowLayout)
+
+    super.init()
+
+    automaticallyManagesSubnodes = true
+  }
+}
