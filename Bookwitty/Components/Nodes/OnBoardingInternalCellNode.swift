de56c71a1d8d09083e2c7cd32da5f90a30258d71
Shafic Hariri <shafic.hariri@keeward.com>
2017-03-06 01:12:18 +0200

    Setup OnBoardingInternalCellNode’s sub-nodes

--- Bookwitty/Components/Nodes/OnBoardingInternalCellNode.swift
+++ Bookwitty/Components/Nodes/OnBoardingInternalCellNode.swift
@@ -29,5 +29,24 @@ class OnBoardingInternalCellNode: ASCellNode {
     imageNode =  ASNetworkImageNode()
     separator = ASDisplayNode()
     super.init()
+    initializeNode()
+  }
+
+  func initializeNode() {
+    automaticallyManagesSubnodes = true
+
+    shortDescriptionTextNode.maximumNumberOfLines = 3
+    titleTextNode.maximumNumberOfLines = 1
+
+    selectionButtonNode.backgroundColor = UIColor.bwRuby
+    selectionButtonNode.style.preferredSize = CGSize(width: 36.0, height: 36.0)
+    imageNode.style.preferredSize = CGSize(width: 45.0, height: 45.0)
+    imageNode.backgroundColor = ASDisplayNodeDefaultPlaceholderColor()
+
+    separator.style.height = ASDimensionMake(1.0)
+    separator.style.width = ASDimensionMake(UIScreen.main.bounds.width)
+    separator.backgroundColor = ThemeManager.shared.currentTheme.defaultSeparatorColor()
+
+    style.preferredSize = CGSize(width: UIScreen.main.bounds.width, height: OnBoardingInternalCellNode.cellHeight)
   }
 }
