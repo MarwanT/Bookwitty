325686ad560b3a6bc93bde189c170f64db3d735c
Shafic Hariri <shafic.hariri@keeward.com>
2017-03-06 00:47:04 +0200

    Initialize the OnBoardingController Node

--- Bookwitty/Components/Nodes/OnBoardingControllerNode.swift
+++ Bookwitty/Components/Nodes/OnBoardingControllerNode.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+import AsyncDisplayKit
+
+class OnBoardingControllerNode: ASDisplayNode {
+  fileprivate let internalMargin = ThemeManager.shared.currentTheme.cardInternalMargin()
+  fileprivate let contentSpacing = ThemeManager.shared.currentTheme.contentSpacing()
+  fileprivate let headerHeight: CGFloat = 45.0
+
+  let titleTextNode: ASTextNode
+  let separatorNode: ASDisplayNode
+  let collectionNode: ASCollectionNode
+  let flowLayout: UICollectionViewFlowLayout
+
+  override init() {
+    titleTextNode = ASTextNode()
+    flowLayout = UICollectionViewFlowLayout()
+    flowLayout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    flowLayout.minimumInteritemSpacing  = 0
+    flowLayout.minimumLineSpacing       = 0
+
+    collectionNode = ASCollectionNode(collectionViewLayout: flowLayout)
+    separatorNode = ASDisplayNode()
+    super.init()
+    automaticallyManagesSubnodes = true
+  }
+}
+
