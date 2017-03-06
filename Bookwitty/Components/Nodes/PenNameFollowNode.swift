885f6942a6cf9b9f53a48810f1ae38eb582c443f
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-03-06 20:41:22 +0200

    Add the ui elements

--- Bookwitty/Components/Nodes/PenNameFollowNode.swift
+++ Bookwitty/Components/Nodes/PenNameFollowNode.swift
@@ -14,8 +14,21 @@ class PenNameFollowNode: ASCellNode {
   fileprivate let imageSize: CGSize = CGSize(width: 45.0, height: 45.0)
   fileprivate let buttonSize: CGSize = CGSize(width: 36.0, height: 36.0)
 
+  private var imageNode: ASNetworkImageNode
+  private var nameNode: ASTextNode
+  private var biographyNode: ASTextNode
+  private var actionButton: ASButtonNode
+
   override init() {
+    imageNode = ASNetworkImageNode()
+    nameNode = ASTextNode()
+    biographyNode = ASTextNode()
+    actionButton = ASButtonNode()
     super.init()
+    addSubnode(imageNode)
+    addSubnode(nameNode)
+    addSubnode(biographyNode)
+    addSubnode(actionButton)
     setupNode()
   }
 
