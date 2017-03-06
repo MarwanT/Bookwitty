34fbc4c24ea5edb8e07e51cd9a5488a260d17590
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-03-06 16:40:48 +0200

    Add the pen name follow node

---
+++ Bookwitty/Components/Nodes/PenNameFollowNode.swift
@@ -0,0 +1,22 @@
+//
+//  PenNameFollowNode.swift
+//  Bookwitty
+//
+//  Created by charles on 3/6/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+import AsyncDisplayKit
+
+class PenNameFollowNode: ASCellNode {
+
+  override init() {
+    super.init()
+    setupNode()
+  }
+
+  private func setupNode() {
+
+  }
+}
