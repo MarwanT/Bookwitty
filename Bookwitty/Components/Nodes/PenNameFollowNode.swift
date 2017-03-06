2cb133a3862393d7a2f073057eec1c5b5ee1d0ba
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-03-06 20:41:45 +0200

    Initialize components

--- Bookwitty/Components/Nodes/PenNameFollowNode.swift
+++ Bookwitty/Components/Nodes/PenNameFollowNode.swift
@@ -33,6 +33,31 @@ class PenNameFollowNode: ASCellNode {
   }
 
   private func setupNode() {
+    imageNode.placeholderColor = ASDisplayNodeDefaultPlaceholderColor()
+    imageNode.imageModificationBlock = ASImageNodeRoundBorderModificationBlock(0.0, nil)
+
+    nameNode.maximumNumberOfLines = 1
+    biographyNode.maximumNumberOfLines = 3
+
+    let buttonFont = FontDynamicType.subheadline.font
+    let textColor = ThemeManager.shared.currentTheme.defaultButtonColor()
+    let selectedTextColor = ThemeManager.shared.currentTheme.colorNumber23()
+    let buttonBackgroundImage = UIImage(color: ThemeManager.shared.currentTheme.defaultBackgroundColor())
+    let selectedButtonBackgroundImage = UIImage(color: ThemeManager.shared.currentTheme.defaultButtonColor())
+    actionButton.titleNode.maximumNumberOfLines = 1
+    actionButton.setBackgroundImage(buttonBackgroundImage, for: .normal)
+    actionButton.setBackgroundImage(selectedButtonBackgroundImage, for: .selected)
+
+    actionButton.setTitle(Strings.follow(), with: buttonFont, with: textColor, for: .normal)
+    actionButton.setTitle(Strings.followed(), with: buttonFont, with: selectedTextColor, for: .selected)
+    actionButton.cornerRadius = 2
+    actionButton.borderColor = ThemeManager.shared.currentTheme.defaultButtonColor().cgColor
+    actionButton.borderWidth = 2
+    actionButton.clipsToBounds = true
+
+    imageNode.style.preferredSize = imageSize
+    actionButton.contentEdgeInsets = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
+    actionButton.style.height = ASDimensionMake(buttonSize.height)
 
   }
 }
