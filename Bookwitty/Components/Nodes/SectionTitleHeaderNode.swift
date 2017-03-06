dd280c1d85bf3b3b62f667bbcf2471a4f0f9d68d
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-03-06 12:57:25 +0200

    Declare Section title header node configuration structure

--- Bookwitty/Components/Nodes/SectionTitleHeaderNode.swift
+++ Bookwitty/Components/Nodes/SectionTitleHeaderNode.swift
@@ -20,3 +20,16 @@ class SectionTitleHeaderNode: ASDisplayNode {
     super.init()
   }
 }
+
+extension SectionTitleHeaderNode {
+  struct Configuration {
+    var verticalBarColor = ThemeManager.shared.currentTheme.colorNumber6()
+    var horizontalBarColor = ThemeManager.shared.currentTheme.colorNumber5()
+    fileprivate var defaultTextColor = ThemeManager.shared.currentTheme.defaultTextColor()
+    fileprivate var verticalBarWidth: CGFloat = 8
+    fileprivate var minimumHeight: CGFloat = 60
+    fileprivate var verticalBarEdgeInsets = UIEdgeInsets(top: 8, left: 0, bottom: 8, right: 0)
+    fileprivate var horizontalBarEdgeInsets = UIEdgeInsets(top: 0, left: 80 - 8, bottom: 0, right: 0)
+    fileprivate var titleEdgeInsets = UIEdgeInsets(top: 10, left: 25, bottom: 10, right: 0)
+  }
+}
