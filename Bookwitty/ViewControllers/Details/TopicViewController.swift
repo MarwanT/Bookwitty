2791feba8c86ae9fd6b6f6a504ecdd5bbc440e3a
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-03-06 15:44:27 +0200

    Add the collection delegate and datasource

--- Bookwitty/ViewControllers/Details/TopicViewController.swift
+++ Bookwitty/ViewControllers/Details/TopicViewController.swift
@@ -43,8 +43,63 @@ class TopicViewController: ASViewController<ASCollectionNode> {
     //TODO: Should be localized and mapped to an enum
     segmentedNode.initialize(with: ["Latest", "Related Books", "Followers"])
     collectionNode.registerSupplementaryNode(ofKind: UICollectionElementKindSectionHeader)
+    collectionNode.delegate = self
+    collectionNode.dataSource = self
+    
     flowLayout.minimumInteritemSpacing = 0
     flowLayout.minimumLineSpacing = 0
     flowLayout.sectionHeadersPinToVisibleBounds = true
+  }
+}
+
+extension TopicViewController: UICollectionViewDelegateFlowLayout {
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+    return section == 0 ? CGSize.zero : CGSize(width: collectionView.frame.size.width, height: segmentedNodeHeight)
+  }
+}
+
+extension TopicViewController: ASCollectionDataSource, ASCollectionDelegate {
+  func numberOfSections(in collectionNode: ASCollectionNode) -> Int {
+    return 2
+  }
+
+  func collectionNode(_ collectionNode: ASCollectionNode, numberOfItemsInSection section: Int) -> Int {
+    return section == 0 ? 1 : 20
+  }
+
+  func collectionNode(_ collectionNode: ASCollectionNode, nodeBlockForItemAt indexPath: IndexPath) -> ASCellNodeBlock {
+    if indexPath.section == 0 {
+      return {
+        return self.headerNode
+      }
+    }
+
+    return {
+      return BaseCardPostNode()
+    }
+  }
+
+  func collectionNode(_ collectionNode: ASCollectionNode, nodeForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> ASCellNode {
+    guard indexPath.section == 1 else {
+      return ASCellNode()
+    }
+
+    switch kind {
+    case UICollectionElementKindSectionHeader:
+      return self.segmentedNode
+    default:
+      return ASCellNode()
+    }
+  }
+
+  func collectionNode(_ collectionNode: ASCollectionNode, willDisplayItemWith node: ASCellNode) {
+
+  }
+
+  public func collectionNode(_ collectionNode: ASCollectionNode, constrainedSizeForItemAt indexPath: IndexPath) -> ASSizeRange {
+    return ASSizeRange(
+      min: CGSize(width: collectionNode.frame.width, height: 0),
+      max: CGSize(width: collectionNode.frame.width, height: .infinity)
+    )
   }
 }
