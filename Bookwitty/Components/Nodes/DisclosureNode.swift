6c3afc611ce4627e8c3140f465bb532a9382e1a2
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-03-06 16:27:28 +0200

    Layout Disclosure node subnodes

--- Bookwitty/Components/Nodes/DisclosureNode.swift
+++ Bookwitty/Components/Nodes/DisclosureNode.swift
@@ -37,6 +37,38 @@ class DisclosureNode: ASControlNode {
     setNeedsLayout()
   }
   
+  override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
+    style.width = ASDimensionMake(constrainedSize.max.width)
+    
+    let horizontalStack = ASStackLayoutSpec(
+      direction: .horizontal,
+      spacing: 0,
+      justifyContent: .start,
+      alignItems: .center,
+      children: [titleTextNode, spacer(flexGrow: 1.0), imageNode])
+    horizontalStack.style.width = ASDimensionMake(constrainedSize.max.width)
+    let insetSpec = ASInsetLayoutSpec(
+      insets: configuration.nodeEdgeInsets,
+      child: horizontalStack)
+    let centerSpec = ASCenterLayoutSpec(
+      horizontalPosition: .start,
+      verticalPosition: .center,
+      sizingOption: ASRelativeLayoutSpecSizingOption.minimumHeight,
+      child: insetSpec)
+    
+    return centerSpec
+  }
+  
+  // MARK: Helpers
+  private func spacer(flexGrow: CGFloat = 0.0, height: CGFloat = 0.0, width: CGFloat = 0.0) -> ASLayoutSpec {
+    return ASLayoutSpec().styled { (style) in
+      style.height = ASDimensionMake(height)
+      style.width = ASDimensionMake(width)
+      style.flexGrow = flexGrow
+      style.flexShrink = flexGrow
+    }
+  }
+  
   var text: String? {
     didSet {
       titleTextNode.attributedText = AttributedStringBuilder(fontDynamicType: configuration.style.fontType)
