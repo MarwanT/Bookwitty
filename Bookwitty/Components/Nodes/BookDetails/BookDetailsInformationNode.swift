c53e52f63a87d7e4357d6263b1de7b50c3a91830
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-03-07 22:03:21 +0200

    Load real data in book details information node

--- Bookwitty/Components/Nodes/BookDetails/BookDetailsInformationNode.swift
+++ Bookwitty/Components/Nodes/BookDetails/BookDetailsInformationNode.swift
@@ -115,17 +115,10 @@ class BookDetailsInformationNode: ASTableNode, ASTableDelegate, ASTableDataSourc
 // MARK: - Helpers
 extension BookDetailsInformationNode {
   fileprivate func  refactorProductDetailsForTableData() {
-    // TODO: apply logic
     tableViewData.removeAll()
-    tableViewData = [
-      ("Paper back", "224 pages"),
-      ("Publisher", "Bloomsberry Press (Aug 2013)"),
-      ("Language", "English"),
-      ("ISBN-10", "1408834960"),
-      ("ISBN-13", "978-1408834960"),
-      ("Dimensions", "13 x 1.5 x 19.7 cm"),
-      ("Shipping Weight", "159g")
-    ]
+    if let associatedValues = productDetails?.associatedKeyValues() {
+      tableViewData = associatedValues
+    }
   }
   
   fileprivate func reloadNode() {
