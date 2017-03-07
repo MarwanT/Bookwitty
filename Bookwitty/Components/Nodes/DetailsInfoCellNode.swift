4687735666d2b8f4661364bb46956ec3e4d63cfd
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-03-07 14:56:37 +0200

    Define details cell configuration structure

--- Bookwitty/Components/Nodes/DetailsInfoCellNode.swift
+++ Bookwitty/Components/Nodes/DetailsInfoCellNode.swift
@@ -12,9 +12,24 @@ class DetailsInfoCellNode: ASCellNode {
   fileprivate var keyTextNode: ASTextNode
   fileprivate var valueTextNode: ASTextNode
   
+  private var configuration = Configuration()
+  
   override init() {
     keyTextNode = ASTextNode()
     valueTextNode = ASTextNode()
     super.init()
+  }
+}
+
+extension DetailsInfoCellNode {
+  struct Configuration {
+    static let minimumHeight: CGFloat = 45
+    let defaultTextColor = ThemeManager.shared.currentTheme.defaultTextColor()
+    let stackEdgeInsets = UIEdgeInsets(
+      top: 10, left: 0, bottom: 10,
+      right: ThemeManager.shared.currentTheme.generalExternalMargin())
+    let keyNodeEdgeInsets = UIEdgeInsets(
+      top: 0, left: 0, bottom: 0,
+      right: (ThemeManager.shared.currentTheme.generalExternalMargin() * 2))
   }
 }
