f531aa509131ff3bbab9441f6e46db907956b5d3
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-03-07 15:00:27 +0200

    Declare ‘Book Details Information Node’ class

--- Bookwitty/Components/Nodes/BookDetails/BookDetailsInformationNode.swift
+++ Bookwitty/Components/Nodes/BookDetails/BookDetailsInformationNode.swift
@@ -6,4 +6,7 @@
 //  Copyright © 2017 Keeward. All rights reserved.
 //
 
-import Foundation
+import AsyncDisplayKit
+
+class BookDetailsInformationNode: ASTableNode, ASTableDelegate, ASTableDataSource {
+}
