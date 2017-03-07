9f03db179e90a44d9c7b862c70ff0b98a10e5419
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-03-07 14:57:50 +0200

    Layout details cell subnodes

--- Bookwitty/Components/Nodes/DetailsInfoCellNode.swift
+++ Bookwitty/Components/Nodes/DetailsInfoCellNode.swift
@@ -18,6 +18,42 @@ class DetailsInfoCellNode: ASCellNode {
     keyTextNode = ASTextNode()
     valueTextNode = ASTextNode()
     super.init()
+    automaticallyManagesSubnodes = true
+    style.minHeight = ASDimensionMake(Configuration.minimumHeight)
+  }
+  
+  override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
+    valueTextNode.style.flexShrink = 1.0
+    
+    let keyNodeInsets = ASInsetLayoutSpec(
+      insets: configuration.keyNodeEdgeInsets,
+      child: keyTextNode)
+    
+    let layoutElements: [ASLayoutElement] = [
+      keyNodeInsets,
+      spacer(flexGrow: 1.0),
+      valueTextNode
+    ]
+    
+    let horizontalStack = ASStackLayoutSpec(
+      direction: .horizontal,
+      spacing: 0,
+      justifyContent: .start, alignItems: .center, children: layoutElements)
+    horizontalStack.style.width = ASDimensionMake(constrainedSize.max.width)
+    let stackInsets = ASInsetLayoutSpec(
+      insets: configuration.stackEdgeInsets,
+      child: horizontalStack)
+    
+    return stackInsets
+  }
+  
+  private func spacer(flexGrow: CGFloat = 0.0, height: CGFloat = 0.0, width: CGFloat = 0.0) -> ASLayoutSpec {
+    return ASLayoutSpec().styled { (style) in
+      style.height = ASDimensionMake(height)
+      style.width = ASDimensionMake(width)
+      style.flexGrow = flexGrow
+      style.flexShrink = flexGrow
+    }
   }
 }
 
