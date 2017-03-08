19c4639ddffde3f6862c63282a06f410368efb72
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-03-08 11:06:52 +0200

    Add the subnodes

--- Bookwitty/Components/Nodes/BookNode.swift
+++ Bookwitty/Components/Nodes/BookNode.swift
@@ -13,8 +13,24 @@ class BookNode: ASCellNode {
   fileprivate let internalMargin = ThemeManager.shared.currentTheme.cardInternalMargin()
   fileprivate let imageSize: CGSize = CGSize(width: 100.0, height: 125.0)
 
+  private var imageNode: ASNetworkImageNode
+  private var titleNode: ASTextNode
+  private var authorNode: ASTextNode
+  private var formatNode: ASTextNode
+  private var priceNode: ASTextNode
+
   override init() {
+    imageNode = ASNetworkImageNode()
+    titleNode = ASTextNode()
+    authorNode = ASTextNode()
+    formatNode = ASTextNode()
+    priceNode = ASTextNode()
     super.init()
+    addSubnode(imageNode)
+    addSubnode(titleNode)
+    addSubnode(authorNode)
+    addSubnode(formatNode)
+    addSubnode(priceNode)
     setupNode()
   }
 
