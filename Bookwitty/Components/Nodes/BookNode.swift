46857936fc0800567c45c8f1fa9999d1ab140d3e
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-03-08 11:01:31 +0200

    Add the book node

---
+++ Bookwitty/Components/Nodes/BookNode.swift
@@ -0,0 +1,21 @@
+//
+//  BookNode.swift
+//  Bookwitty
+//
+//  Created by charles on 3/7/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+import AsyncDisplayKit
+
+class BookNode: ASCellNode {
+  override init() {
+    super.init()
+    setupNode()
+  }
+
+  private func setupNode() {
+    
+  }
+}
