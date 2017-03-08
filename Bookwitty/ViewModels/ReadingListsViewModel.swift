807581693ad3a05be081a0bc6dba14accc5ae8b9
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-03-08 13:59:25 +0200

    Add the reading lists view model

---
+++ Bookwitty/ViewModels/ReadingListsViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ReadingListsViewModel.swift
+//  Bookwitty
+//
+//  Created by charles on 3/8/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+final class ReadingListsViewModel {
+
+}
