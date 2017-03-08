6afc6277919ae9166e3b3f01ee9bd7684a026205
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-03-08 12:50:04 +0200

    Add the topic view model

---
+++ Bookwitty/ViewModels/Details/TopicViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TopicViewModel.swift
+//  Bookwitty
+//
+//  Created by charles on 3/8/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+final class TopicViewModel {
+
+}
