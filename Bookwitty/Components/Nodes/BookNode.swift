94c700b05bfe18ca81f8f8f0882ba89d41e1cb7e
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-03-08 11:04:35 +0200

    Add the needed constants

--- Bookwitty/Components/Nodes/BookNode.swift
+++ Bookwitty/Components/Nodes/BookNode.swift
@@ -10,6 +10,9 @@ import UIKit
 import AsyncDisplayKit
 
 class BookNode: ASCellNode {
+  fileprivate let internalMargin = ThemeManager.shared.currentTheme.cardInternalMargin()
+  fileprivate let imageSize: CGSize = CGSize(width: 100.0, height: 125.0)
+
   override init() {
     super.init()
     setupNode()
