380b9baacd5d7b710de0d2f52be46b79f44aac78
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-03-08 13:40:45 +0200

    Add the view model to the view controller

--- Bookwitty/ViewControllers/Details/TopicViewController.swift
+++ Bookwitty/ViewControllers/Details/TopicViewController.swift
@@ -26,6 +26,8 @@ class TopicViewController: ASViewController<ASCollectionNode> {
 
   fileprivate lazy var mode: Mode = .normal(categories: self.normal)
 
+  fileprivate let viewModel = TopicViewModel()
+
   required init?(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
