345c93539ca002ff69b83d8d30ee17cd6d55ef65
Shafic Hariri <shafic.hariri@keeward.com>
2017-03-09 17:14:34 +0200

    Initialize PostDetailsHeaderNode

--- Bookwitty/Components/Nodes/PostDetailsHeaderNode.swift
+++ Bookwitty/Components/Nodes/PostDetailsHeaderNode.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+import AsyncDisplayKit
+
+class PostDetailsHeaderNode: ASCellNode {
+  private let internalMargin = ThemeManager.shared.currentTheme.cardInternalMargin()
+  private let contentSpacing = ThemeManager.shared.currentTheme.contentSpacing()
+
+  fileprivate let imageNode: ASNetworkImageNode
+  fileprivate let textNode: ASTextNode
+  fileprivate let profileBarNode: ASDisplayNode
+  fileprivate let actionBarNode: CardActionBarNode
+  fileprivate let separator: ASDisplayNode
+  fileprivate let bottomSeparator: ASDisplayNode
+
+  override init() {
+    imageNode = ASNetworkImageNode()
+    textNode = ASTextNode()
+    profileBarNode = ASDisplayNode()
+    actionBarNode = CardActionBarNode()
+    separator = ASDisplayNode()
+    bottomSeparator = ASDisplayNode()
+    super.init()
+    automaticallyManagesSubnodes = true
+    initializeNode()
+  }
+
+  func initializeNode() {
+    //Separator Styling
+    //Post Iamge
+    //Post Title
+    //Profile Bar
+    //Action Bar
+  }
+}
