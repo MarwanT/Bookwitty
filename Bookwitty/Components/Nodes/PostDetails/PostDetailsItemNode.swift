8ffb1820010829e97cb7d950797f5701791e55e2
Shafic Hariri <shafic.hariri@keeward.com>
2017-03-09 19:42:29 +0200

    Initialize PostDetail Item sub-nodes

--- Bookwitty/Components/Nodes/PostDetails/PostDetailsItemNode.swift
+++ Bookwitty/Components/Nodes/PostDetails/PostDetailsItemNode.swift
@@ -27,6 +27,30 @@ class PostDetailItemNode: ASDisplayNode {
     captionNode = ASTextNode()
     bodyNode = ASTextNode()
     super.init()
+    initializeNode()
   }
-  
+
+  func initializeNode() {
+    //Image Setup
+    imageNode.style.preferredSize = CGSize(width: 75.0, height: 75.0)
+    //Body Setup
+    bodyNode.maximumNumberOfLines = 7
+    //HeadLine Setup
+    headLineNode.maximumNumberOfLines = 3
+    //subheadLine Setup
+    subheadLineNode.maximumNumberOfLines = 2
+    //caption Setup
+    captionNode.maximumNumberOfLines = 1
+
+    bodyNode.style.flexGrow = 1
+    headLineNode.style.flexGrow = 1
+    subheadLineNode.style.flexGrow = 1
+    captionNode.style.flexGrow = 1
+
+    bodyNode.style.flexShrink = 1
+    headLineNode.style.flexShrink = 1
+    subheadLineNode.style.flexShrink = 1
+    captionNode.style.flexShrink = 1
+  }
+
 }
