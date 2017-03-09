c87f5cff77d09b3594af366529fb712394c62678
Shafic Hariri <shafic.hariri@keeward.com>
2017-03-09 17:20:56 +0200

    Initialize PostDetailsNode: Add properties, outlets and elements initialization

--- Bookwitty/Components/Nodes/PostDetailsNode.swift
+++ Bookwitty/Components/Nodes/PostDetailsNode.swift
@@ -10,7 +10,38 @@ import Foundation
 import AsyncDisplayKit
 
 class PostDetailsNode: ASDisplayNode {
+  private let internalMargin = ThemeManager.shared.currentTheme.cardInternalMargin()
+  private let contentSpacing = ThemeManager.shared.currentTheme.contentSpacing()
+
+  fileprivate let headerNode: PostDetailsHeaderNode
+  fileprivate let descriptionNode: ASTextNode
+
+  var title: String? {
+    didSet {
+      headerNode.title = title
+    }
+  }
+  var coverImage: String? {
+    didSet {
+        headerNode.image = coverImage
+    }
+  }
+  var body: String? {
+    didSet {
+      let attributed = body.isEmptyOrNil() ? nil : AttributedStringBuilder(fontDynamicType: FontDynamicType.title3).append(text: body!).attributedString
+      descriptionNode.attributedText = attributed
+    }
+  }
+
   override init() {
+    headerNode = PostDetailsHeaderNode()
+    descriptionNode = ASTextNode()
     super.init()
+    automaticallyManagesSubnodes = true
+    automaticallyManagesContentSize = true
+    initializeNode()
+  }
+
+  func initializeNode() {
   }
 }
