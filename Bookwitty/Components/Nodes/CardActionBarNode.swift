5562b2d6ddb3b7ded5b57921104a8e54930cac73
Shafic Hariri <shafic.hariri@keeward.com>
2017-03-09 17:12:54 +0200

    Convert CardActionBarNode to ASCellNode instead ASDisplayNode

--- Bookwitty/Components/Nodes/CardActionBarNode.swift
+++ Bookwitty/Components/Nodes/CardActionBarNode.swift
@@ -13,7 +13,7 @@ protocol CardActionBarNodeDelegate {
   func cardActionBarNode(cardActionBar: CardActionBarNode, didRequestAction action: CardActionBarNode.Action, forSender sender: ASButtonNode, didFinishAction: ((_ success: Bool) -> ())?)
 }
 
-class CardActionBarNode: ASDisplayNode {
+class CardActionBarNode: ASCellNode {
   enum Action {
     case wit
     case unwit
