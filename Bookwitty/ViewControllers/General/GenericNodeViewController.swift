4958f6efad85dcbe17cf191be38b662f79c45d46
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-03-09 14:18:47 +0200

    Encapsulate generic node view controller base node in a scroll view if needed

--- Bookwitty/ViewControllers/General/GenericNodeViewController.swift
+++ Bookwitty/ViewControllers/General/GenericNodeViewController.swift
@@ -14,7 +14,28 @@ class GenericNodeViewController: ASViewController<ASDisplayNode> {
   }
   
   init(node: ASDisplayNode, title: String? = nil) {
-    super.init(node: node)
+    let baseNode = GenericNodeViewController.encapsulateWithScrollNodeIfNeeded(node: node)
+    super.init(node: baseNode)
     self.title = title
+  }
+  
+  private static func encapsulateWithScrollNodeIfNeeded(node: ASDisplayNode) -> ASDisplayNode {
+    guard !(node.view is UIScrollView) else {
+      return node
+    }
+    
+    let scrollNode = ASScrollNode()
+    scrollNode.automaticallyManagesSubnodes = true
+    scrollNode.automaticallyManagesContentSize = true
+    scrollNode.layoutSpecBlock = { displayNode, constrainedSize in
+      let stack = ASStackLayoutSpec(
+        direction: .vertical,
+        spacing: 0,
+        justifyContent: .start,
+        alignItems: .stretch,
+        children: [node])
+      return stack
+    }
+    return scrollNode
   }
 }
