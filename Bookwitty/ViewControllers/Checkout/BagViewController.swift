ba33d04dbe54f112175e017cb9bbda367e268a0e
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-03-11 00:07:14 +0200

    Create bag view controller

---
+++ Bookwitty/ViewControllers/Checkout/BagViewController.swift
@@ -0,0 +1,15 @@
+//
+//  BagViewController.swift
+//  Bookwitty
+//
+//  Created by Marwan  on 3/11/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import AsyncDisplayKit
+
+class BagViewController: ASViewController<ASDisplayNode> {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
