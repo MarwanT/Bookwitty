39a545a8d13f6528c1274948c1cf8a53e9b2e2a5
Shafic Hariri <shafic.hariri@keeward.com>
2017-03-10 16:07:57 +0200

    Initialize Node on PostDetails VC ViewDidLoad

--- Bookwitty/ViewControllers/Details/PostDetailsViewController.swift
+++ Bookwitty/ViewControllers/Details/PostDetailsViewController.swift
@@ -8,16 +8,44 @@
 
 import Foundation
 import AsyncDisplayKit
+import Spine
+import Moya
 
 class PostDetailsViewController: ASViewController<PostDetailsNode> {
+  let postDetailsNode: PostDetailsNode
+  var viewModel: PostDetailsViewModel
 
   required init?(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
 
   init(title: String? = nil, resource: Resource) {
+    viewModel = PostDetailsViewModel(resource: resource)
     postDetailsNode = PostDetailsNode()
     super.init(node: postDetailsNode)
+    self.title = title ?? viewModel.vcTitle
   }
 
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    postDetailsNode.title = viewModel.title
+    postDetailsNode.coverImage = viewModel.image
+    postDetailsNode.body = viewModel.body
+    let date = Date.formatDate(date: viewModel.date)
+    postDetailsNode.date = date
+    postDetailsNode.penName = viewModel.penName
+    loadContentPosts()
+  }
+
+  func loadContentPosts() {
+    guard viewModel.shouldLoadContentPosts() else {
+      return
+    }
+    //Start Loading
+    viewModel.loadContentPosts { (success) in
+      //Done Loading - Update UI
+      self.postDetailsNode.loadPostItemsNode()
+    }
+  }
 }
