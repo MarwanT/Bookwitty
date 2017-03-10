64975f39b8f2fc508c53848f7a88fc6ad36b87d8
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-03-10 18:58:58 +0200

    Add ‘should refresh data’ notification

--- Bookwitty/Utilities/AppNotification.swift
+++ Bookwitty/Utilities/AppNotification.swift
@@ -14,4 +14,5 @@ struct AppNotification {
   static let didSignIn: Notification.Name = Notification.Name(rawValue: "AppNotification.didSignIn")
   static let shouldDisplayRegistration: Notification.Name = Notification.Name(rawValue: "AppNotification.shouldDisplayRegistration")
   static let registrationSuccess: Notification.Name = Notification.Name(rawValue: "AppNotification.registrationSuccess")
+  static let shouldRefreshData: Notification.Name = Notification.Name(rawValue: "AppNotification.shouldRefreshData")
 }
