f057971d8edc823558034b1d8a18f1cf753eb663
Shafic Hariri <shafic.hariri@keeward.com>
2017-03-10 18:14:22 +0200

    Develop CardDetailsViewController using GenericNodeViewController

--- Bookwitty/ViewControllers/Details/CardDetailsViewController.swift
+++ Bookwitty/ViewControllers/Details/CardDetailsViewController.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+import AsyncDisplayKit
+import Spine
+
+class CardDetailsViewController: GenericNodeViewController {
+  var viewModel: CardDetailsViewModel
+
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  init(node: BaseCardPostNode, title: String? = nil, resource: ModelResource) {
+    viewModel = CardDetailsViewModel(resource: resource)
+    super.init(node: node, title: title)
+  }
+}
