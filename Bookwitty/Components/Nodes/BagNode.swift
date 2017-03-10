eb7db7f7b4e04215a3d6632928c9f1c0f4bb7f10
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-03-11 01:05:41 +0200

    Define bag node configuration structure

--- Bookwitty/Components/Nodes/BagNode.swift
+++ Bookwitty/Components/Nodes/BagNode.swift
@@ -13,9 +13,23 @@ class BagNode: ASDisplayNode {
   fileprivate let textNode: ASTextNode
   fileprivate let shopOnlineButton: ASButtonNode
   
+  var configuration = Configuration()
+  
   override init() {
     textNode = ASTextNode()
     shopOnlineButton = ASButtonNode()
     super.init()
+  }
+}
+
+// MARK: - Declaration
+extension BagNode {
+  struct Configuration {
+    fileprivate let shopOnlineButtonTitleColor = ThemeManager.shared.currentTheme.colorNumber23()
+    fileprivate let defaultTextColor = ThemeManager.shared.currentTheme.defaultTextColor()
+    fileprivate let backgroundColor = ThemeManager.shared.currentTheme.colorNumber1()
+    fileprivate let edgeInsets = UIEdgeInsets(
+      top: 0, left: ThemeManager.shared.currentTheme.generalExternalMargin(),
+      bottom: 0, right: ThemeManager.shared.currentTheme.generalExternalMargin())
   }
 }
