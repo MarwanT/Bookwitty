d2fad50fea27ad27a6cc53e1eebd74d9b91a3411
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-03-10 23:54:54 +0200

    Set tutorial vc colors

--- Bookwitty/ViewModels/Introduction/IntroductionViewModel.swift
+++ Bookwitty/ViewModels/Introduction/IntroductionViewModel.swift
@@ -12,29 +12,34 @@ final class IntroductionViewModel {
   let tutorialData: [TutorialPageData] = [
     (title: Strings.tutorial_first_page_title(), Strings.tutorial_first_page_title(),
      image: #imageLiteral(resourceName: "illustration1"),
-     color: ThemeManager.shared.currentTheme.colorNumber4()),
+     color: ThemeManager.shared.currentTheme.colorNumber19(),
+     pastelColor: ThemeManager.shared.currentTheme.colorNumber2()),
     (title: Strings.tutorial_second_page_title(),
      description: Strings.tutorial_second_page_description(),
      image: #imageLiteral(resourceName: "illustration2"),
-     color: ThemeManager.shared.currentTheme.colorNumber6()),
+     color: ThemeManager.shared.currentTheme.colorNumber10(),
+     pastelColor: ThemeManager.shared.currentTheme.colorNumber9()),
     (title: Strings.tutorial_third_page_title(),
      description: Strings.tutorial_second_page_description(),
      image: #imageLiteral(resourceName: "illustration3"),
-     color: ThemeManager.shared.currentTheme.colorNumber6()),
+     color: ThemeManager.shared.currentTheme.colorNumber4(),
+     pastelColor: ThemeManager.shared.currentTheme.colorNumber3()),
     (title: Strings.tutorial_forth_page_title(),
      description: Strings.tutorial_forth_page_description(),
      image: #imageLiteral(resourceName: "illustration4"),
-     color: ThemeManager.shared.currentTheme.colorNumber6()),
+     color: ThemeManager.shared.currentTheme.colorNumber12(),
+     pastelColor: ThemeManager.shared.currentTheme.colorNumber11()),
     (title: Strings.tutorial_fifth_page_title(),
      description: Strings.tutorial_fifth_page_description(),
      image: #imageLiteral(resourceName: "illustration5"),
-     color: ThemeManager.shared.currentTheme.colorNumber6())
+     color: ThemeManager.shared.currentTheme.colorNumber8(),
+     pastelColor: ThemeManager.shared.currentTheme.colorNumber7())
   ]
   
   let signInButtonTitle: String = Strings.sign_in()
   let registerButtonTitle: String = Strings.register()
   
-  func colorForIndex(index: Int) -> UIColor? {
-    return tutorialData[index].color
+  func colorsForIndex(index: Int) -> (color: UIColor?, pastelColor: UIColor?) {
+    return (tutorialData[index].color, tutorialData[index].pastelColor)
   }
 }
