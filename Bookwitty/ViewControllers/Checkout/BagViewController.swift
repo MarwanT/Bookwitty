2983c2ad90212603897016fed80e53acca94be79
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-03-11 01:10:12 +0200

    Add settings navigation bar button

--- Bookwitty/ViewControllers/Checkout/BagViewController.swift
+++ Bookwitty/ViewControllers/Checkout/BagViewController.swift
@@ -22,6 +22,23 @@ class BagViewController: ASViewController<ASDisplayNode> {
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    initializeNavigationItems()
+  }
+  
+  private func initializeNavigationItems() {
+    let leftNegativeSpacer = UIBarButtonItem(barButtonSystemItem:
+      UIBarButtonSystemItem.fixedSpace, target: nil, action: nil)
+    leftNegativeSpacer.width = -10
+    let settingsBarButton = UIBarButtonItem(image: #imageLiteral(resourceName: "person"), style:
+      UIBarButtonItemStyle.plain, target: self, action:
+      #selector(self.settingsButtonTap(_:)))
+    navigationItem.leftBarButtonItems = [leftNegativeSpacer, settingsBarButton]
+  }
+  
+  func settingsButtonTap(_ sender: UIBarButtonItem) {
+    let settingsVC = Storyboard.Account.instantiate(AccountViewController.self)
+    settingsVC.hidesBottomBarWhenPushed = true
+    self.navigationController?.pushViewController(settingsVC, animated: true)
   }
 }
 
