049b23501401eaccd20b108823e9a238f70d909b
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-03-10 21:57:06 +0200

    Add the author contributors and counts

--- Bookwitty/Models/Author.swift
+++ Bookwitty/Models/Author.swift
@@ -27,6 +27,14 @@ class Author: Resource {
   var vote: String?
   
   //TODO: add PageAuthor model we have a problem with the json-api conforming from the api siding
+  @objc
+  private var contributorsCollection: LinkedResourceCollection?
+  lazy var contributors: [PenName]? = {
+    return self.contributorsCollection?.resources as? [PenName]
+  }()
+
+  @objc
+  fileprivate var countsDictionary: [String : Any]?
 
   override class var resourceType: ResourceType {
     return "authors"
@@ -44,12 +52,45 @@ class Author: Resource {
       "imageUrl": Attribute().serializeAs("image-url"),
       "caption": Attribute().serializeAs("caption"),
       "biography": Attribute().serializeAs("biography"),
+      "countsDictionary": Attribute().serializeAs("counts"),
       "shortDescription": Attribute().serializeAs("short-description"),
       "profileImageUrl": Attribute().serializeAs("profile-image-url"),
       "title": Attribute().serializeAs("title"),
       "type": Attribute().serializeAs("type"),
+      "contributorsCollection" : ToManyRelationship(PenName.self).serializeAs("contributors"),
       "penName" : ToOneRelationship(PenName.self).serializeAs("pen-name")
       ])
+  }
+}
+
+//MARK: - Counts Helpers
+extension Author {
+  private struct CountsKey {
+    private init(){}
+    static let followers = "followers"
+    static let contributors = "contributors"
+    static let comments = "comments"
+    static let relatedLinks = "related-links"
+  }
+
+  var counts: (contributors: Int?, followers: Int?, posts: Int?) {
+    guard let counts = countsDictionary else {
+      return (nil, nil, nil)
+    }
+
+    let contributors = (counts[CountsKey.contributors] as? NSNumber)?.intValue
+    let followers = (counts[CountsKey.followers] as? NSNumber)?.intValue
+    let relatedLinks = counts[CountsKey.relatedLinks] as? [String : NSNumber]
+
+    var posts: Int? = nil
+
+    if let relatedLinks = relatedLinks {
+      let values = Array(relatedLinks.values)
+      posts = values.reduce(0) { (cumulative, current: NSNumber) -> Int in
+        return cumulative + current.intValue
+      }
+    }
+    return (contributors, followers, posts)
   }
 }
 
