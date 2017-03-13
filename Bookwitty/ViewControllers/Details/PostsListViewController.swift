80bb7a64284d3006f35079160f45932ce1cbcaef
Shafic Hariri <shafic.hariri@keeward.com>
2017-03-13 19:40:32 +0200

    Initialize PostsListViewController

--- Bookwitty/ViewControllers/Details/PostsListViewController.swift
+++ Bookwitty/ViewControllers/Details/PostsListViewController.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+import AsyncDisplayKit
+import Spine
+
+class PostsListViewController: ASViewController<ASCollectionNode> {
+  fileprivate let collectionNodeLoadingSection: Int = 1
+  fileprivate let collectionNodeItemsSection: Int = 0
+  fileprivate let collectionNode: ASCollectionNode
+  fileprivate let flowLayout: UICollectionViewFlowLayout
+  fileprivate let loaderNode: LoaderNode
+
+
+  var viewModel: PostsListViewModel
+
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  init(title: String? = nil, ids: [String], preloadedList: [Resource]) {
+    viewModel = PostsListViewModel(ids: ids, preloadedList: preloadedList)
+    flowLayout = UICollectionViewFlowLayout()
+    flowLayout.sectionInset = UIEdgeInsets.zero
+    flowLayout.minimumInteritemSpacing  = 0
+    flowLayout.minimumLineSpacing       = 0
+    collectionNode = ASCollectionNode(collectionViewLayout: flowLayout)
+    loaderNode = LoaderNode()
+    loaderNode.style.width = ASDimensionMake(UIScreen.main.bounds.width)
+    super.init(node: collectionNode)
+
+    collectionNode.style.flexGrow = 1
+    collectionNode.style.flexShrink = 1
+    collectionNode.style.width = ASDimensionMake(UIScreen.main.bounds.width)
+    self.title = title
+  }
+
+}
