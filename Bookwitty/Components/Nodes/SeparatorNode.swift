24cecfdb2fa17fdab26fc763d7c4759ff1431d7f
Shafic Hariri <shafic.hariri@keeward.com>
2017-03-14 16:35:23 +0200

    Develop styled SeparatorNode of ASDisplayNode type

--- Bookwitty/Components/Nodes/SeparatorNode.swift
+++ Bookwitty/Components/Nodes/SeparatorNode.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+import AsyncDisplayKit
+
+class SeparatorNode: ASDisplayNode {
+  private override init() {
+    super.init()
+  }
+
+  convenience init(color: UIColor = ThemeManager.shared.currentTheme.defaultSeparatorColor(), width: CGFloat? = nil, height: CGFloat = 1.0) {
+    self.init()
+    backgroundColor = color
+    style.flexGrow = 1
+    style.flexShrink = 1
+    style.height = ASDimensionMake(height)
+    if let width = width {
+      style.width = ASDimensionMake(width)
+    }
+  }
+}
