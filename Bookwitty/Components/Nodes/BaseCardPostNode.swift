4ed5c3fb1ed5a10665cf05f071557725d7208484
Shafic Hariri <shafic.hariri@keeward.com>
2017-03-15 15:04:03 +0200

    Set the dim, wit values and their numbers

--- Bookwitty/Components/Nodes/BaseCardPostNode.swift
+++ Bookwitty/Components/Nodes/BaseCardPostNode.swift
@@ -56,15 +56,13 @@ class BaseCardPostNode: ASCellNode {
       }
     }
   }
-  var wit: Bool = false {
-    didSet {
-      actionBarNode.setWitButton(witted: wit)
-    }
+
+  func setWitValue(witted: Bool, wits: Int) {
+    actionBarNode.setWitButton(witted: witted, wits: wits)
   }
-  var dimmed: Bool = false {
-    didSet {
-      actionBarNode.setDimValue(dimmed: dimmed)
-    }
+
+  func setDimValue(dimmed: Bool, dims: Int) {
+    actionBarNode.setDimValue(dimmed: dimmed, dims: dims)
   }
 
   override init() {
