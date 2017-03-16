d4879bf2a28f63fd35caa35ae04ed3117d9095f0
Shafic Hariri <shafic.hariri@keeward.com>
2017-03-16 18:36:02 +0200

    Develop the search API request function

--- Bookwitty/ViewModels/General/SearchViewModel.swift
+++ Bookwitty/ViewModels/General/SearchViewModel.swift
@@ -10,4 +10,22 @@ import Foundation
 import Moya
 
 class SearchViewModel {
+  var data: [ModelResource] = []
+  var cancellableRequest: Cancellable?
+
+  private func search(query: String, completion: @escaping (_ success: Bool, _ error: BookwittyAPIError?) -> Void) {
+    self.data.removeAll(keepingCapacity: false)
+
+    cancellableRequest = SearchAPI.search(filter: (query, nil), page: nil, completion: {
+      (success, resources, nextPage, error) in
+      defer {
+        completion(success, error)
+      }
+
+      guard success, let resources = resources else {
+        return
+      }
+      self.data += resources
+    })
+  }
 }
