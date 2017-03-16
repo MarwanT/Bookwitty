10835affc04c7c6422b22e49c660b9dc5f00af66
Shafic Hariri <shafic.hariri@keeward.com>
2017-03-16 19:11:06 +0200

    Get the search result details using the batch api

--- Bookwitty/ViewModels/General/SearchViewModel.swift
+++ Bookwitty/ViewModels/General/SearchViewModel.swift
@@ -13,19 +13,41 @@ class SearchViewModel {
   var data: [ModelResource] = []
   var cancellableRequest: Cancellable?
 
-  private func search(query: String, completion: @escaping (_ success: Bool, _ error: BookwittyAPIError?) -> Void) {
+  func search(query: String, completion: @escaping (_ success: Bool, _ error: BookwittyAPIError?) -> Void) {
     self.data.removeAll(keepingCapacity: false)
 
     cancellableRequest = SearchAPI.search(filter: (query, nil), page: nil, completion: {
       (success, resources, nextPage, error) in
-      defer {
-        completion(success, error)
-      }
-
       guard success, let resources = resources else {
+        completion(success, error)
         return
       }
-      self.data += resources
+
+      let resourceIds: [String] = resources.flatMap({ $0.id })
+      self.cancellableRequest = self.loadBatch(listOfIdentifiers: resourceIds, completion: { (success, resources, error) in
+        defer {
+          completion(success, error)
+        }
+        guard success, let resources = resources else {
+          return
+        }
+        self.data += resources
+      })
+    })
+  }
+
+  private func loadBatch(listOfIdentifiers: [String], completion: @escaping (_ success: Bool, _ resources: [ModelResource]?, _ error: BookwittyAPIError?) -> Void) -> Cancellable? {
+    return UserAPI.batch(identifiers: listOfIdentifiers, completion: {
+      (success, resource, error) in
+      var resources: [ModelResource]?
+      defer {
+        completion(success, resources, error)
+      }
+
+      guard success, let result = resource else {
+        return
+      }
+      resources = result
     })
   }
 }
