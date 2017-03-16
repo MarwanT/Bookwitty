00ac2400afd1f98b0a1ba4a969c5cc8eccd7da77
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-03-16 12:25:42 +0200

    Add the forgot password view controller swift file

---
+++ Bookwitty/ViewControllers/Account/ForgotPasswordViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ForgotPasswordViewController.swift
+//  Bookwitty
+//
+//  Created by charles on 3/16/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class ForgotPasswordViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+    initializeComponents()
+  }
+
+  private func initializeComponents() {
+
+  }
+}
