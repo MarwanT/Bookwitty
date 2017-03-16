6dff33b9ea2ffdf87018b5434e047662989bc2fe
Shafic Hariri <shafic.hariri@keeward.com>
2017-03-16 16:30:50 +0200

    Initialize Search View Controller with a CollectionNode

--- Bookwitty/ViewControllers/General/SearchViewController.swift
+++ Bookwitty/ViewControllers/General/SearchViewController.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+import AsyncDisplayKit
+
+class SearchViewController: ASViewController<ASCollectionNode> {
+  var flowLayout: UICollectionViewFlowLayout
+  var collectionNode: ASCollectionNode
+
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  init() {
+    flowLayout = UICollectionViewFlowLayout()
+    flowLayout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    flowLayout.minimumInteritemSpacing  = 0
+    flowLayout.minimumLineSpacing       = 0
+
+    collectionNode = ASCollectionNode(collectionViewLayout: flowLayout)
+    super.init(node: collectionNode)
+  }
+}
