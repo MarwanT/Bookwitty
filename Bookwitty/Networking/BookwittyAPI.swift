34191aeb13c3a1e7b9b178740fc574a20f524a5f
Shafic Hariri <shafic.hariri@keeward.com>
2017-03-17 19:11:23 +0200

    Develop the penName content API

--- Bookwitty/Networking/BookwittyAPI.swift
+++ Bookwitty/Networking/BookwittyAPI.swift
@@ -41,6 +41,7 @@ public enum BookwittyAPI {
   case posts(identifier: String, type: [String]?)
   case editions(identifier: String)
   case resetPassword(email: String)
+  case penNameContent(identifier: String)
 }
 
 // MARK: - Target Type
@@ -127,6 +128,8 @@ extension BookwittyAPI: TargetType {
       path = "/content/\(identifier)/editions"
     case .resetPassword:
       path = "/user/reset_password"
+    case .penNameContent(let identifier):
+      path = "/pen_names/\(identifier)/content"
     }
     
     return apiBasePath + apiVersion + path
@@ -136,7 +139,7 @@ extension BookwittyAPI: TargetType {
     switch self {
     case .oAuth, .refreshToken:
       return .post
-    case .allAddresses, .user, .bookStore, .categoryCuratedContent, .newsFeed, .Search, .penNames, .absolute, .discover, .onBoarding, .content, .followers, .posts, .editions:
+    case .allAddresses, .user, .bookStore, .categoryCuratedContent, .newsFeed, .Search, .penNames, .absolute, .discover, .onBoarding, .content, .followers, .posts, .editions, .penNameContent:
       return .get
     case .register, .batch, .updatePreference, .wit, .follow, .dim, .resetPassword:
       return .post
@@ -181,7 +184,7 @@ extension BookwittyAPI: TargetType {
       return GeneralAPI.postsParameters(type: type)
     case .resetPassword(let email):
       return UserAPI.resetPasswordBody(email: email)
-    case .allAddresses, .user, .bookStore, .categoryCuratedContent, .newsFeed, .penNames, .wit, .unwit, .absolute, .discover, .onBoarding, .follow, .unfollow, .content, .followers, .editions, .dim, .undim:
+    case .allAddresses, .user, .bookStore, .categoryCuratedContent, .newsFeed, .penNames, .wit, .unwit, .absolute, .discover, .onBoarding, .follow, .unfollow, .content, .followers, .editions, .dim, .undim, .penNameContent:
       return nil
     }
   }
