afcb20db8892944e16c88e7e690d1c7e32c9f580
Shafic Hariri <shafic.hariri@keeward.com>
2017-03-17 16:27:29 +0200

    Create static vc initializer

--- Bookwitty/ViewControllers/Details/ProfileDetailsViewController.swift
+++ Bookwitty/ViewControllers/Details/ProfileDetailsViewController.swift
@@ -12,12 +12,20 @@ import AsyncDisplayKit
 class ProfileDetailsViewController: ASViewController<ASCollectionNode> {
   let flowLayout: UICollectionViewFlowLayout
   let collectionNode: ASCollectionNode
-  
+
+  fileprivate var viewModel: ProfileDetailsViewModel!
+
+  class func create(with viewModel: ProfileDetailsViewModel) -> ProfileDetailsViewController {
+    let profileVC = ProfileDetailsViewController()
+    profileVC.viewModel = viewModel
+    return profileVC
+  }
+
   required init?(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
 
-  init() {
+  private init() {
     flowLayout = UICollectionViewFlowLayout()
     collectionNode = ASCollectionNode(collectionViewLayout: flowLayout)
     super.init(node: collectionNode)
