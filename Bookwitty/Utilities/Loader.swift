b6a22581b1cd597f80c21bf84e594b8bb555c969
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-03-17 19:56:55 +0200

    Add configuration method for swift loader

--- Bookwitty/Utilities/Loader.swift
+++ Bookwitty/Utilities/Loader.swift
@@ -6,3 +6,13 @@
 //  Copyright © 2017 Keeward. All rights reserved.
 //
 
+import SwiftLoader
+
+extension SwiftLoader {
+  static func configure() {
+    var config : SwiftLoader.Config = SwiftLoader.Config()
+    config.size = 135.0
+    config.coverBackgroundColor = UIColor(white: 0.1, alpha: 0.4)
+    SwiftLoader.setConfig(config)
+  }
+}
