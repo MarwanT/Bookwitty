88a0bd0f61f7cecd4bdca5aa1ad601bd1e177005
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-03-17 10:13:19 +0200

    Add the name for the category enum

--- Bookwitty/Utilities/Analytics/Analytics+Constants.swift
+++ Bookwitty/Utilities/Analytics/Analytics+Constants.swift
@@ -38,6 +38,50 @@ extension Analytics {
     case CategoriesList
     case Bag
 
+    var name: String {
+      switch self {
+      case .Account:
+        return "Account"
+      case .Quote:
+        return "Quote"
+      case .Image:
+        return "Image"
+      case .Audio:
+        return "Audio"
+      case .Video:
+        return "Video"
+      case .Link:
+        return "Link"
+      case .ReadingList:
+        return "Reading List"
+      case .Text:
+        return "Text"
+      case .Topic:
+        return "Topic"
+      case .Author:
+        return "Author"
+      case .TopicBook:
+        return "Topic Book"
+      case .BookProduct:
+        return "Book Product"
+      case .PenName:
+        return "Pen Name"
+      case .NewsFeed:
+        return "News Feed"
+      case .Discover:
+        return "Discover"
+      case .Search:
+        return "Search"
+      case .BookStorefront:
+        return "Book Storefront"
+      case .BookCategory:
+        return "Book Category"
+      case .CategoriesList:
+        return "Categories List"
+      case .Bag:
+        return "Bag"
+      }
+    }
   }
   
   enum Action {
