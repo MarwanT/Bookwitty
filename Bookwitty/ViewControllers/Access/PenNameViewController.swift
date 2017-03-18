b0921fc11c5fac9a3a435d0c434daab0f708d03a
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-03-18 02:01:40 +0200

    Hide note label when pen name vc is accessed from the settings

--- Bookwitty/ViewControllers/Access/PenNameViewController.swift
+++ Bookwitty/ViewControllers/Access/PenNameViewController.swift
@@ -23,6 +23,8 @@ class PenNameViewController: UIViewController {
   @IBOutlet weak var topViewToTopConstraint: NSLayoutConstraint!
   let topViewToTopSpace: CGFloat = 40
   let viewModel: PenNameViewModel = PenNameViewModel()
+  
+  var showNoteLabel: Bool = true
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -48,6 +50,8 @@ class PenNameViewController: UIViewController {
     penNameInputField.delegate = self
 
     setupBiographyKeyboardToolbar()
+    
+    noteLabel.isHidden = !showNoteLabel
 
     //Make Cicular View tappable
     let tap = UITapGestureRecognizer(target: self, action: #selector(self.didTapOnCircularView(_:)))
