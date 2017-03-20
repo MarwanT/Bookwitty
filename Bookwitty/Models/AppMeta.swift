a7bf8b3ca1b60b1d934747610a847143bed1b36a
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-03-20 21:47:32 +0200

    Declare AppMeta class

---
+++ Bookwitty/Models/AppMeta.swift
@@ -0,0 +1,14 @@
+//
+//  AppMeta.swift
+//  Bookwitty
+//
+//  Created by Marwan  on 3/20/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+class AppMeta {
+}
+
