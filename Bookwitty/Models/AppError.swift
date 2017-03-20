5cccd6564a953247bcece2ea690de5e7ba31d7d0
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-03-21 01:48:00 +0200

    Add methods for retrieving app error objects out of data objects

--- Bookwitty/Models/AppError.swift
+++ Bookwitty/Models/AppError.swift
@@ -7,9 +7,36 @@
 //
 
 import Foundation
+import SwiftyJSON
 
 class AppError {
   var status: Int?
   var detail: String?
   var meta: [String: Any]?
+}
+
+extension AppError {
+  static func appError(for jsonObject: JSON) -> AppError {
+    let appError = AppError()
+    appError.status = jsonObject["status"].int
+    appError.detail = jsonObject["detail"].string
+    appError.meta = jsonObject["meta"].dictionaryObject
+    return appError
+  }
+  
+  static func appErrors(for data: Data) -> [AppError]? {
+    guard let jsonDictionary = try? JSONSerialization.jsonObject(
+      with: data, options: JSONSerialization.ReadingOptions.allowFragments) as? [String:Any] else {
+        return nil
+    }
+    
+    var errors = [AppError]()
+    let json = JSON(jsonDictionary)
+    let errorsJSON = json["errors"].arrayValue
+    for jsonObject in errorsJSON {
+      let error = appError(for: jsonObject)
+      errors.append(error)
+    }
+    return errors
+  }
 }
