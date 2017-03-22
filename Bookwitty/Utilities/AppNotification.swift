0cdd42d7d13540f2c5a5f27f044c37f72dddf67b
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-03-22 02:32:26 +0200

    Add ’should display sign in’ notification

--- Bookwitty/Utilities/AppNotification.swift
+++ Bookwitty/Utilities/AppNotification.swift
@@ -13,6 +13,7 @@ struct AppNotification {
   static let signOut: Notification.Name = Notification.Name(rawValue: "SignOutNotification")
   static let didSignIn: Notification.Name = Notification.Name(rawValue: "AppNotification.didSignIn")
   static let shouldDisplayRegistration: Notification.Name = Notification.Name(rawValue: "AppNotification.shouldDisplayRegistration")
+  static let shouldDisplaySignIn: Notification.Name = Notification.Name(rawValue: "AppNotification.shouldDisplaySignIn")
   static let registrationSuccess: Notification.Name = Notification.Name(rawValue: "AppNotification.registrationSuccess")
   static let shouldRefreshData: Notification.Name = Notification.Name(rawValue: "AppNotification.shouldRefreshData")
   static let didFinishBoarding: Notification.Name = Notification.Name(rawValue: "AppNotification.didFinishBoarding")
