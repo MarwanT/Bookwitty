792a161486149e32006ecee68894f7c990cb7cca
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-03-22 20:18:04 +0200

    Add the bar button items utils extension

---
+++ Bookwitty/Utilities/UIBarButtonItem+Utils.swift
@@ -0,0 +1,13 @@
+//
+//  UIBarButtonItem+Utils.swift
+//  Bookwitty
+//
+//  Created by charles on 3/22/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+extension UIBarButtonItem {
+
+}
