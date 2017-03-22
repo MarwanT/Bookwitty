792694da114c75f99018b8220d8e4363fcd26192
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-03-22 02:17:10 +0200

    Declare join us node subnodes

--- Bookwitty/Components/JoinUsNode.swift
+++ Bookwitty/Components/JoinUsNode.swift
@@ -9,4 +9,16 @@
 import AsyncDisplayKit
 
 class JoinUsNode: ASDisplayNode {
+  fileprivate let titleTextNode: ASTextNode
+  fileprivate let descriptionTextNode: ASTextNode
+  fileprivate let getStartedButtonNode: ASButtonNode
+  fileprivate let alreadyHaveAnAccountTextNode: ASTextNode
+  
+  override init() {
+    titleTextNode = ASTextNode()
+    descriptionTextNode = ASTextNode()
+    getStartedButtonNode = ASButtonNode()
+    alreadyHaveAnAccountTextNode = ASTextNode()
+    super.init()
+  }
 }
