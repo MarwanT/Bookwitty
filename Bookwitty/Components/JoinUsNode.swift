ebee6a2c561b6e655498a6ce28f969c9605d3cf5
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-03-22 16:32:21 +0200

    Apply design for the join us node

--- Bookwitty/Components/JoinUsNode.swift
+++ Bookwitty/Components/JoinUsNode.swift
@@ -9,18 +9,26 @@
 import AsyncDisplayKit
 
 class JoinUsNode: ASDisplayNode {
+  fileprivate let imageColoredBackgroundNode: ASDisplayNode
+  fileprivate let imageWhiteBackgroundNode: ASDisplayNode
+  fileprivate let imageNode: ASImageNode
   fileprivate let titleTextNode: ASTextNode
   fileprivate let descriptionTextNode: ASTextNode
   fileprivate let getStartedButtonNode: ASButtonNode
   fileprivate let alreadyHaveAnAccountTextNode: ASTextNode
+  fileprivate let contentBackgroundNode: ASDisplayNode
   
   var configuration = Configuration()
   
   override init() {
+    imageColoredBackgroundNode = ASDisplayNode()
+    imageWhiteBackgroundNode = ASDisplayNode()
+    imageNode = ASImageNode()
     titleTextNode = ASTextNode()
     descriptionTextNode = ASTextNode()
     getStartedButtonNode = ASButtonNode()
     alreadyHaveAnAccountTextNode = ASTextNode()
+    contentBackgroundNode = ASDisplayNode()
     super.init()
     initializeComponent()
     applyTheme()
@@ -29,11 +37,20 @@ class JoinUsNode: ASDisplayNode {
   private func initializeComponent() {
     automaticallyManagesSubnodes = true
     backgroundColor = configuration.backgroundColor
+    imageColoredBackgroundNode.backgroundColor = configuration.backgroundColor
     
     titleText = Strings.join_us_to_personalize_feed_title()
     descriptionText = Strings.join_us_to_personalize_feed_description()
     getStartedButtonTitle = Strings.get_started()
-    alreadyHaveAnAccountAttributedString = AttributedStringBuilder(fontDynamicType: FontDynamicType.caption1).append(text: "\(Strings.already_have_an_account())\n", color: configuration.textColor).append(text: Strings.sign_in(), fontDynamicType: FontDynamicType.footnote, color: configuration.signInTextColor).applyParagraphStyling(alignment: NSTextAlignment.center).attributedString
+    alreadyHaveAnAccountAttributedString = AttributedStringBuilder(fontDynamicType: FontDynamicType.footnote).append(text: "\(Strings.already_have_an_account())\n", color: configuration.textColor).append(text: Strings.sign_in(), fontDynamicType: FontDynamicType.footnote, color: configuration.signInTextColor).applyParagraphStyling(alignment: NSTextAlignment.center).attributedString
+    
+    imageNode.image = #imageLiteral(resourceName: "illustrationOstrich")
+    imageNode.contentMode = UIViewContentMode.scaleAspectFit
+    imageWhiteBackgroundNode.backgroundColor = UIColor.white
+    
+    getStartedButtonNode.contentEdgeInsets = configuration.buttonContentEdgeInsets
+    
+    contentBackgroundNode.backgroundColor = UIColor.white
     
     getStartedButtonNode.addTarget(self, action:
       #selector(self.getStartedTouchUpInside(_:)), forControlEvents: ASControlNodeEvent.touchUpInside)
@@ -41,36 +58,73 @@ class JoinUsNode: ASDisplayNode {
       #selector(self.alreadyHaveAnAccountTouchUpInside(_:)), forControlEvents: ASControlNodeEvent.touchUpInside)
   }
   
+  var imageBottomWhiteHeightDimension: CGFloat {
+    return 55.5
+  }
+  
   override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
-    let stackSpec = ASStackLayoutSpec(
+    // TOP PART OF THE NODE
+    //----------------------
+    let imageBackgroundForegroundInset = ASInsetLayoutSpec(
+      insets: UIEdgeInsets(top: CGFloat.infinity, left: 0, bottom: 0, right: 0),
+      child: imageWhiteBackgroundNode)
+    imageWhiteBackgroundNode.style.height = ASDimensionMake("\(imageBottomWhiteHeightDimension)%")
+    let imageBackgroundOverlaySpec = ASOverlayLayoutSpec(
+      child: imageColoredBackgroundNode,
+      overlay: imageBackgroundForegroundInset)
+    
+    let imageSpec = ASBackgroundLayoutSpec(child: imageNode, background: imageBackgroundOverlaySpec)
+    imageSpec.style.maxHeight = ASDimensionMake(constrainedSize.max.height/2)
+    imageSpec.style.flexShrink = 1.0
+    
+    
+    // BOTTOM HALF OF THE NODE
+    //-------------------------
+    
+    let contentStackSpec = ASStackLayoutSpec(
       direction: .vertical, spacing: 0,
       justifyContent: .center,
-      alignItems: .stretch,
+      alignItems: .center,
       children: [
         titleTextNode,
-        ASLayoutSpec.spacer(height: 15),
+        ASLayoutSpec.spacer(height: 5),
         descriptionTextNode,
-        ASLayoutSpec.spacer(height: 50),
+        ASLayoutSpec.spacer(height: 30),
         getStartedButtonNode,
-        ASLayoutSpec.spacer(height: 50),
+        ASLayoutSpec.spacer(height: 30),
         alreadyHaveAnAccountTextNode
       ])
-    let externalInsetSpec = ASInsetLayoutSpec(
+    contentStackSpec.style.minHeight = ASDimensionMake(constrainedSize.max.height/2)
+    let externalContentInsetSpec = ASInsetLayoutSpec(
       insets: configuration.externalEdgeInsets,
-      child: stackSpec)
-    return externalInsetSpec
+      child: contentStackSpec)
+    externalContentInsetSpec.style.flexGrow = 1.0
+    
+    
+    // Build the base vertical Layout
+    //--------------------------------
+    let verticalStack = ASStackLayoutSpec(
+      direction: .vertical,
+      spacing: 0,
+      justifyContent: ASStackLayoutJustifyContent.start,
+      alignItems: .stretch,
+      children: [imageSpec, externalContentInsetSpec])
+    verticalStack.style.height = ASDimensionMake(constrainedSize.max.height)
+    let verticalStackBackgroundSpec = ASBackgroundLayoutSpec(
+      child: verticalStack, background: contentBackgroundNode)
+    return verticalStackBackgroundSpec
   }
   
   var titleText: String? {
     didSet {
-      titleTextNode.attributedText = AttributedStringBuilder(fontDynamicType: .title1)
+      titleTextNode.attributedText = AttributedStringBuilder(fontDynamicType: .headline)
         .append(text: titleText ?? "", color: configuration.textColor).applyParagraphStyling(alignment: NSTextAlignment.center).attributedString
       setNeedsLayout()
     }
   }
   var descriptionText: String? {
     didSet {
-      descriptionTextNode.attributedText = AttributedStringBuilder(fontDynamicType: .callout)
+      descriptionTextNode.attributedText = AttributedStringBuilder(fontDynamicType: .subheadline)
         .append(text: descriptionText ?? "", color: configuration.textColor).applyParagraphStyling(alignment: NSTextAlignment.center).attributedString
       setNeedsLayout()
     }
@@ -105,7 +159,7 @@ extension JoinUsNode {
 // MARK: - Themeable
 extension JoinUsNode: Themeable {
   func applyTheme() {
-    ThemeManager.shared.currentTheme.styleECommercePrimaryButton(button: getStartedButtonNode)
+    ThemeManager.shared.currentTheme.stylePrimaryButton(button: getStartedButtonNode)
   }
 }
 
@@ -122,5 +176,8 @@ extension JoinUsNode {
       left: ThemeManager.shared.currentTheme.generalExternalMargin(),
       bottom: ThemeManager.shared.currentTheme.generalExternalMargin(),
       right: ThemeManager.shared.currentTheme.generalExternalMargin())
+    let buttonContentEdgeInsets = UIEdgeInsets(
+      top: 0, left: ThemeManager.shared.currentTheme.generalExternalMargin(),
+      bottom: 0, right: ThemeManager.shared.currentTheme.generalExternalMargin())
   }
 }
