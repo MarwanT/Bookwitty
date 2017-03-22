eb99f68501b3262f20ee5f5905f83977c21559f1
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-03-22 02:17:41 +0200

    Add join us node configuration structure

--- Bookwitty/Components/JoinUsNode.swift
+++ Bookwitty/Components/JoinUsNode.swift
@@ -14,11 +14,28 @@ class JoinUsNode: ASDisplayNode {
   fileprivate let getStartedButtonNode: ASButtonNode
   fileprivate let alreadyHaveAnAccountTextNode: ASTextNode
   
+  var configuration = Configuration()
+  
   override init() {
     titleTextNode = ASTextNode()
     descriptionTextNode = ASTextNode()
     getStartedButtonNode = ASButtonNode()
     alreadyHaveAnAccountTextNode = ASTextNode()
     super.init()
+  }
+}
+
+extension JoinUsNode {
+  struct Configuration {
+    let textColor = ThemeManager.shared.currentTheme.defaultTextColor()
+    let buttonTextColor = ThemeManager.shared.currentTheme.colorNumber23()
+    let backgroundColor = ThemeManager.shared.currentTheme.colorNumber2()
+    let linkTextColor = ThemeManager.shared.currentTheme.colorNumber16()
+    let signInTextColor = ThemeManager.shared.currentTheme.colorNumber19()
+    let externalEdgeInsets = UIEdgeInsets(
+      top: ThemeManager.shared.currentTheme.generalExternalMargin(),
+      left: ThemeManager.shared.currentTheme.generalExternalMargin(),
+      bottom: ThemeManager.shared.currentTheme.generalExternalMargin(),
+      right: ThemeManager.shared.currentTheme.generalExternalMargin())
   }
 }
