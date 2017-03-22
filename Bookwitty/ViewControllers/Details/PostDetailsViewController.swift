1aba13da2ee953df169c84ad521724eff84defe9
Elie Soueidy <elie.soueidy@keeward.com>
2017-03-22 02:04:03 +0200

    Hide the e-commerce button if the book format corresponds to an electronic book

--- Bookwitty/ViewControllers/Details/PostDetailsViewController.swift
+++ Bookwitty/ViewControllers/Details/PostDetailsViewController.swift
@@ -418,7 +418,10 @@ extension PostDetailsViewController: PostDetailsItemNodeDataSource {
     switch (resource.registeredResourceType) {
     case Book.resourceType:
       let res = resource as? Book
-      let itemNode = PostDetailItemNode(smallImage: false, showsSubheadline: false, showsButton: true)
+      let showEcommerceButton: Bool = (res?.supplierInformation != nil) &&
+        !(res?.productDetails?.isElectronicFormat() ?? false)
+
+      let itemNode = PostDetailItemNode(smallImage: false, showsSubheadline: false, showsButton: showEcommerceButton)
       itemNode.imageUrl = res?.thumbnailImageUrl
       itemNode.body = res?.bookDescription
       itemNode.buttonTitle = Strings.buy_this_book()
