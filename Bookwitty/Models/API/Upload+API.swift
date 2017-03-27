78647511cf574c9e3fc409b18a89799ecc050c8b
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-03-27 16:12:00 +0300

    Add the upload api helper file

---
+++ Bookwitty/Models/API/Upload+API.swift
@@ -0,0 +1,13 @@
+//
+//  Upload+API.swift
+//  Bookwitty
+//
+//  Created by charles on 3/27/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+public struct UploadAPI {
+
+}
