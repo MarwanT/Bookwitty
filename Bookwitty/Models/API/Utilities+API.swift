c28f93910d177f5a2e6c46f3971a81f5f31cad63
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-03-29 14:17:03 +0300

    Add the utilities api file

---
+++ Bookwitty/Models/API/Utilities+API.swift
@@ -0,0 +1,13 @@
+//
+//  Utilities+API.swift
+//  Bookwitty
+//
+//  Created by charles on 3/29/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+struct UtilitiesAPI {
+
+}
