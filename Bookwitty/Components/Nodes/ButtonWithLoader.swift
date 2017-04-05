317a3065de62ef74a51ffef08005a4a4a0e3fa46
Shafic Hariri <shafic.hariri@keeward.com>
2017-04-05 11:10:46 +0300

    Initialize ButtonWithLoader Node with the initial elements

--- Bookwitty/Components/Nodes/ButtonWithLoader.swift
+++ Bookwitty/Components/Nodes/ButtonWithLoader.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+import AsyncDisplayKit
+
+class ButtonWithLoader: ASDisplayNode {
+  fileprivate let smallButtonHeight: CGFloat = 36.0
+  fileprivate let largeButtonHeight: CGFloat = 44.0
+
+  fileprivate let loaderNode: LoaderNode
+  fileprivate let button: ASButtonNode
+
+  override init() {
+    loaderNode = LoaderNode()
+    button = ASButtonNode()
+    super.init()
+    automaticallyManagesSubnodes = true
+    setupNode()
+  }
+
+  func setupNode() {
+    style.flexGrow = 1.0
+    style.flexShrink = 1.0
+
+    button.titleNode.maximumNumberOfLines = 1
+    button.style.height = ASDimensionMake(smallButtonHeight)
+    button.contentEdgeInsets = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+  }
+}
