a020579475520cecabfeb5dea6e2191cbbe056ed
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-04-05 18:44:06 +0300

    Add the multipart upload function in utilities api

--- Bookwitty/Models/API/Utilities+API.swift
+++ Bookwitty/Models/API/Utilities+API.swift
@@ -7,7 +7,26 @@
 //
 
 import Foundation
+import Moya
 
 struct UtilitiesAPI {
+  static func upload(url: URL, paramters: [String : String], multipart: (data: Data, name: String), completion: @escaping (_ success: Bool, _ error: BookwittyAPIError?) -> Void) -> Cancellable? {
+    let successStatusCode: Int = 204
+    let apiRequest = createAPIRequest(target: .uploadMultipart(url: url, parameters: paramters, multipart: multipart), completion: {
+      (data, statusCode, response, error) in
+      var success: Bool = false
+      var error: BookwittyAPIError? = nil
+      defer {
+        completion(success, error)
+      }
 
+      guard data != nil, let statusCode = statusCode else {
+        error = BookwittyAPIError.invalidStatusCode
+        return
+      }
+
+      success = statusCode == successStatusCode
+    })
+    return apiRequest()
+  }
 }
