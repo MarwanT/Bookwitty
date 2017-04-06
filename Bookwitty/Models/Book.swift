190451cf85193b1be8f5223aa1e4859a9d93c5e5
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-04-06 16:25:24 +0300

    Move ‘Supplier Information’ model to a separate swift file

--- Bookwitty/Models/Book.swift
+++ Bookwitty/Models/Book.swift
@@ -196,38 +196,3 @@ extension ProductDetails {
     return ProductDetails.electronicProductForms.contains(productForm)
   }
 }
-
-
-// MARK: - Supplier Information
-class SupplierInformation: NSObject {
-  var quantity: Int?
-  var listPrice: Price?
-  var price: Price?
-  var userPrice: Price?
-  var preferredPrice: Price? {
-    if userPrice?.formattedValue != nil {
-      return userPrice
-    } else if price?.formattedValue != nil {
-      return price
-    } else {
-      return listPrice
-    }
-  }
-  
-  override init() {
-    super.init()
-  }
-  
-  init(for dictionary: [String : Any]) {
-    super.init()
-    setValues(dictionary: dictionary)
-  }
-  
-  private func setValues(dictionary: [String : Any]) {
-    let json = JSON(dictionary)
-    self.quantity = json["quantity"].intValue
-    self.listPrice = Price(for: json["list-price"].dictionaryObject)
-    self.price = Price(for: json["price"].dictionaryObject)
-    self.userPrice = Price(for: json["user-price"].dictionaryObject)
-  }
-}
