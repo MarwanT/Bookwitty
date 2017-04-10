7d1c96388e57f8f2ae257950a6506a3bd23c096f
Shafic Hariri <shafic.hariri@keeward.com>
2017-04-10 12:23:55 +0300

    Add the BookCardPost CellNode  and BookCardPost ContentNode with the initial setup

--- Bookwitty/Components/Nodes/Cards/BookCardPostCellNode.swift
+++ Bookwitty/Components/Nodes/Cards/BookCardPostCellNode.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+import AsyncDisplayKit
+
+class BookCardPostCellNode: BaseCardPostNode {
+
+  let node: BookCardPostContentNode
+  var showsInfoNode: Bool = false
+  override var shouldShowInfoNode: Bool { return showsInfoNode }
+  override var contentShouldExtendBorders: Bool { return false }
+  override var contentNode: ASDisplayNode { return node }
+
+  override init() {
+    node = BookCardPostContentNode()
+    super.init()
+  }
+
+  convenience init(shouldShowInfoNode: Bool) {
+    self.init()
+    showsInfoNode = shouldShowInfoNode
+  }
+}
+
+class BookCardPostContentNode: ASDisplayNode {
+}
