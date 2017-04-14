3eddeac513d1f729cc75e4164cd2584f726810c5
Shafic Hariri <shafic.hariri@keeward.com>
2017-04-14 19:14:13 +0300

    Develop the ModelCommonActions protocol

--- Bookwitty/Models/ModelCommonActions.swift
+++ Bookwitty/Models/ModelCommonActions.swift
@@ -7,3 +7,12 @@
 //
 
 import Foundation
+
+protocol ModelCommonActions {
+  var wit: Bool { get set }
+  var dim: Bool { get set }
+  var voteValue: String? { get set }
+  var isFollowing: Bool { get set }
+  var counts: Counts? { get }
+}
+
