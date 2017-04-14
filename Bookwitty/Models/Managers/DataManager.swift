13dc2664ed093cfeea45eddda3c8743a5cf59f49
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-04-14 12:14:49 +0300

    Add the actions and the functions needed to update accordingly

--- Bookwitty/Models/Managers/DataManager.swift
+++ Bookwitty/Models/Managers/DataManager.swift
@@ -9,6 +9,16 @@
 import Foundation
 
 class DataManager {
+
+  enum Action {
+    case wit
+    case unwit
+    case dim
+    case undim
+    case follow
+    case unfollow
+  }
+
   static let shared = DataManager()
   private init() {}
 
@@ -29,9 +39,59 @@ class DataManager {
     guard let identifier = resource.id else {
       return
     }
+    
     pool.updateValue(resource, forKey: identifier)
+  }
+
+  func updateResource(with identifier: String, after action: Action) {
+    guard let resource = self.fetchResource(with: identifier) else {
+      return
+    }
+
+    switch action {
+    case .wit:
+      wit(resource)
+    case .unwit:
+      unwit(resource)
+    case .dim:
+      dim(resource)
+    case .undim:
+      undim(resource)
+    case .follow:
+      follow(resource)
+    case .unfollow:
+      unfollow(resource)
+    }
   }
 
   //MARK: - Remove Resources
   //TODO: Will be discussed and implemented later
+}
+
+
+//MARK: - Update After Action Implementations
+extension DataManager {
+  fileprivate func wit(_ resource: ModelResource) {
+    //TODO: implement the update
+  }
+
+  fileprivate func unwit(_ resource: ModelResource) {
+    //TODO: implement the update
+  }
+
+  fileprivate func dim(_ resource: ModelResource) {
+    //TODO: implement the update
+  }
+
+  fileprivate func undim(_ resource: ModelResource) {
+    //TODO: implement the update
+  }
+
+  fileprivate func follow(_ resource: ModelResource) {
+    //TODO: implement the update
+  }
+
+  fileprivate func unfollow(_ resource: ModelResource) {
+    //TODO: implement the update
+  }
 }
