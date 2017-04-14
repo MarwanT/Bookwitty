37feb3814004ca3c1b33cf86a0e69b3a08f9b138
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-04-14 11:31:16 +0300

    Add the Data Manager class

---
+++ Bookwitty/Models/Managers/DataManager.swift
@@ -0,0 +1,13 @@
+//
+//  DataManager.swift
+//  Bookwitty
+//
+//  Created by charles on 4/14/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+class DataManager {
+
+}
