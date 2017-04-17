a5ef7902338da3981dbda7cc17130aa2ca13e4e2
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-04-17 17:48:45 +0300

    Add the article card view model

---
+++ Bookwitty/ViewModels/Cards/ArticleCardViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ArticleCardViewModel.swift
+//  Bookwitty
+//
+//  Created by charles on 4/17/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+class ArticleCardViewModel {
+
+}
