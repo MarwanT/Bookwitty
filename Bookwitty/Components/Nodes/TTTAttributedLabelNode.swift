a991b0794d2b851635835d545142292e0daffd77
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-04-17 11:50:15 +0300

    Wrap TTTAttributedLabel in a display node

--- Bookwitty/Components/Nodes/TTTAttributedLabelNode.swift
+++ Bookwitty/Components/Nodes/TTTAttributedLabelNode.swift
@@ -6,4 +6,62 @@
 //  Copyright © 2017 Keeward. All rights reserved.
 //
 
-import Foundation
+import AsyncDisplayKit
+import TTTAttributedLabel
+
+class TTTAttributedLabelNode: ASDisplayNode {
+  var label: TTTAttributedLabel?
+  
+  convenience override init() {
+    self.init(viewBlock: { () -> UIView in
+      let tttLabel = TTTAttributedLabel(frame: CGRect.zero)
+      return tttLabel
+    })
+
+    DispatchQueue.main.async {
+      self.label = self.view as? TTTAttributedLabel
+      self.label?.linkAttributes = self.linkAttributes
+      self.label?.attributedText = self.attributedText
+      self.label?.delegate = self.delegate
+      self.label?.textAlignment = self.textAlignment
+    }
+    
+    initializeNode()
+  }
+  
+  func initializeNode() {
+    // If preferred size is not specified there is a chance of a crash
+    style.preferredSize = CGSize(width: 45, height: 45)
+  }
+  
+  
+  // MARK: - TTTAttributedLabel APIs
+  
+  open var linkAttributes: [AnyHashable : Any]! {
+    didSet {
+      label?.linkAttributes = linkAttributes
+    }
+  }
+  
+  @NSCopying open var attributedText: NSAttributedString! {
+    didSet {
+      label?.attributedText = attributedText
+    }
+  }
+  
+  var textAlignment: NSTextAlignment = .left {
+    didSet {
+      self.label?.textAlignment = self.textAlignment
+    }
+  }
+  
+  open func addLink(to url: URL!, with range: NSRange) -> TTTAttributedLabelLink! {
+    return label?.addLink(to: url, with: range)
+  }
+  
+  weak var delegate: TTTAttributedLabelDelegate! {
+    didSet {
+      label?.delegate = delegate
+    }
+  }
+}
