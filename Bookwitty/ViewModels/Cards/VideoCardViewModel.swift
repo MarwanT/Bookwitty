6df8fc15ef945a694d50bdb7bfe9ad008e032a23
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-04-17 21:02:27 +0300

    Add the needed variables and functions that get the video properties

--- Bookwitty/ViewModels/Cards/VideoCardViewModel.swift
+++ Bookwitty/ViewModels/Cards/VideoCardViewModel.swift
@@ -16,13 +16,35 @@ class VideoCardViewModel {
 
   var resource: ModelCommonProperties? {
     didSet {
+      getVideoInfo()
       notifyChange()
     }
   }
+
+  private var videoProperties: (mediaLink: String?, url: URL?, thumbnail: String?) = (nil, nil, nil)
 
   weak var delegate: VideoCardViewModelDelegate?
 
   private func notifyChange() {
     delegate?.resourceUpdated(viewModel: self)
   }
+
+  private func getVideoInfo() {
+    guard let video = resource as? Video else {
+      return
+    }
+
+    if let urlStr = video.media?.mediaLink,
+      let url = URL(string: urlStr) {
+      if videoProperties.mediaLink != urlStr {
+        videoProperties.mediaLink = urlStr
+        IFramely.shared.loadResponseFor(url: url, closure: { (response: Response?) in
+          self.videoProperties.url = response?.embedUrl
+          self.videoProperties.thumbnail = response?.thumbnails?.first?.url?.absoluteString ?? video.coverImageUrl
+          self.notifyChange()
+        })
+      }
+    }
+  }
+
 }
