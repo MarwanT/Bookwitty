d23c20ce3e3c1920ba179130e9d9b6d48d9e077e
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-04-17 20:40:55 +0300

    Add the video card view model

---
+++ Bookwitty/ViewModels/Cards/VideoCardViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  VideoCardViewModel.swift
+//  Bookwitty
+//
+//  Created by charles on 4/17/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+class VideoCardViewModel {
+
+}
