beda88f61fc14d78d00e1ac79c7375148a32b6ff
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-04-18 11:05:45 +0300

    Add the function to return the values for the card

--- Bookwitty/ViewModels/Cards/ProfileCardViewModel.swift
+++ Bookwitty/ViewModels/Cards/ProfileCardViewModel.swift
@@ -24,4 +24,18 @@ class ProfileCardViewModel {
   private func notifyChange() {
     delegate?.resourceUpdated(viewModel: self)
   }
+
+  func values() -> (name: String?, biography: String?, imageUrl: String?, following: Bool, followers: Int?) {
+    guard let resource = resource else {
+      return (nil, nil, nil, false, nil)
+    }
+
+    let name = resource.title
+    let biography = resource.shortDescription
+    let imageUrl = resource.coverImageUrl
+    let following = resource.following
+    let followers = resource.counts?.followers
+
+    return (name: name, biography: biography, imageUrl: imageUrl, following: following, followers: followers)
+  }
 }
