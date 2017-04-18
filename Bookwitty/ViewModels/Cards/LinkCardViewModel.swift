6303365b2205f4b318dea1c27e1ad8f77146fda0
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-04-18 17:04:31 +0300

    Add the link card view model

---
+++ Bookwitty/ViewModels/Cards/LinkCardViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  LinkCardViewModel.swift
+//  Bookwitty
+//
+//  Created by charles on 4/18/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+class LinkCardViewModel {
+
+}
