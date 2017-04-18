09a66738d2fb0fb7ca9398747adfc0ba4dcd5c0b
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-04-18 12:33:33 +0300

    Add the function to return the values for the card

--- Bookwitty/ViewModels/Cards/BookCardViewModel.swift
+++ Bookwitty/ViewModels/Cards/BookCardViewModel.swift
@@ -24,4 +24,40 @@ class BookCardViewModel {
   private func notifyChange() {
     delegate?.resourceUpdated(viewModel: self)
   }
+
+  func values() -> (infoNode: Bool, postInfo: CardPostInfoNodeData?, content: (title: String?, description: String?, image: (cover: String?, thumbnail: String?), info: (author: String?, price: String?, format: String?), comments: String?, statistics: (posts: Int?, relatedBooks: Int?, followers: Int?), following: Bool, wit: (is: Bool, count: Int), dim: (is: Bool, count: Int))) {
+    guard let resource = resource, let book = resource as? Book else {
+      return (false, nil, content: (nil, nil, image: (nil, nil), info: (nil, nil, nil), nil, statistics: (nil, nil, nil), false, wit: (false, 0), dim: (false, 0)))
+    }
+
+    let cardPostInfoData: CardPostInfoNodeData?
+    if let penName = resource.penName {
+      let name = penName.name ?? ""
+      let date = Date.formatDate(date: resource.createdAt)
+      let penNameprofileImage = penName.avatarUrl
+      cardPostInfoData = CardPostInfoNodeData(name, date, penNameprofileImage)
+    } else {
+      cardPostInfoData = nil
+    }
+
+    let infoNode: Bool = !(cardPostInfoData?.name.isEmpty ?? true)
+    let title = resource.title
+    let description = resource.shortDescription
+    let imageUrl = resource.thumbnailImageUrl
+    let comments: String? = nil
+    let posts = resource.counts?.posts
+    let relatedBooks: Int? = nil
+    let following = resource.following
+    let followers = resource.counts?.followers
+    let statistics = (posts, relatedBooks, followers)
+    let wit = (is: resource.isWitted, count: resource.counts?.wits ?? 0)
+    let dim = (is: resource.isDimmed, count: resource.counts?.dims ?? 0)
+
+    let author = book.productDetails?.author
+    let price = (book.productDetails?.isElectronicFormat() ?? false) ? nil : book.supplierInformation?.preferredPrice?.formattedValue
+    let format = book.productDetails?.productFormat
+    let info = (author, price, format)
+
+    return (infoNode, cardPostInfoData, content: (title, description, image: (nil, imageUrl), info: info, comments, statistics: statistics, following, wit: wit, dim: dim))
+  }
 }
