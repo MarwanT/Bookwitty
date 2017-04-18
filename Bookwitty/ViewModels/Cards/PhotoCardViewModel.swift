a845a590669c1644111ab6c55ef5e24bf23f1bc5
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-04-18 17:50:31 +0300

    Add the photo card view model

---
+++ Bookwitty/ViewModels/Cards/PhotoCardViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCardViewModel.swift
+//  Bookwitty
+//
+//  Created by charles on 4/18/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+class PhotoCardViewModel {
+
+}
