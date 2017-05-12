676293c34a0dd762e24b43b4aec156509dfe1481
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-05-12 17:37:52 +0300

    Add the create pen name api call and the post body

--- Bookwitty/Models/API/PenName+API.swift
+++ Bookwitty/Models/API/PenName+API.swift
@@ -32,6 +32,26 @@ struct PenNameAPI {
     })
   }
 
+  public static func createPenName(name: String, biography: String?, avatarId: String?, avatarUrl: String?, facebookUrl: String?, tumblrUrl: String?, googlePlusUrl: String?, twitterUrl: String?, instagramUrl: String?, pinterestUrl: String?, youtubeUrl: String?, linkedinUrl: String?, wordpressUrl: String?, websiteUrl: String?, completionBlock: @escaping (_ success: Bool, _ penName: PenName?, _ error: BookwittyAPIError?)->()) -> Cancellable? {
+    return signedAPIRequest(target: BookwittyAPI.createPenName(name: name, biography: biography, avatarId: avatarId, avatarUrl: avatarUrl, facebookUrl: facebookUrl, tumblrUrl: tumblrUrl, googlePlusUrl: googlePlusUrl, twitterUrl: twitterUrl, instagramUrl: instagramUrl, pinterestUrl: pinterestUrl, youtubeUrl: youtubeUrl, linkedinUrl: linkedinUrl, wordpressUrl: wordpressUrl, websiteUrl: websiteUrl), completion: {
+      (data, statusCode, response, error) in
+      var success: Bool = false
+      var penName: PenName? = nil
+      var error: BookwittyAPIError? = error
+      defer {
+        completionBlock(success, penName, error)
+      }
+
+      if let data = data {
+        penName = PenName.parseData(data: data)
+        UserManager.shared.replaceUpdated(penName: penName)
+        success = penName != nil
+      } else {
+        error = BookwittyAPIError.failToParseData
+      }
+    })
+  }
+
   public static func updatePenName(identifier: String, name: String?, biography: String?, avatarId: String?, avatarUrl: String?, facebookUrl: String?, tumblrUrl: String?, googlePlusUrl: String?, twitterUrl: String?, instagramUrl: String?, pinterestUrl: String?, youtubeUrl: String?, linkedinUrl: String?, wordpressUrl: String?, websiteUrl: String?, completionBlock: @escaping (_ success: Bool, _ penName: PenName?, _ error: BookwittyAPIError?)->()) -> Cancellable? {
     return signedAPIRequest(target: BookwittyAPI.updatePenName(identifier: identifier, name: name, biography: biography, avatarId: avatarId, avatarUrl: avatarUrl, facebookUrl: facebookUrl, tumblrUrl: tumblrUrl, googlePlusUrl: googlePlusUrl, twitterUrl: twitterUrl, instagramUrl: instagramUrl, pinterestUrl: pinterestUrl, youtubeUrl: youtubeUrl, linkedinUrl: linkedinUrl, wordpressUrl: wordpressUrl, websiteUrl: websiteUrl), completion: {
       (data, statusCode, response, error) in
@@ -223,6 +243,25 @@ struct PenNameAPI {
 
 //MARK: - Moya Needed parameters
 extension PenNameAPI {
+  static func createPostBody(name: String, biography: String?, avatarId: String?, avatarUrl: String?, facebookUrl: String?, tumblrUrl: String?, googlePlusUrl: String?, twitterUrl: String?, instagramUrl: String?, pinterestUrl: String?, youtubeUrl: String?, linkedinUrl: String?, wordpressUrl: String?, websiteUrl: String?) -> [String : Any]? {
+    let penName = PenName()
+    penName.name = name
+    penName.biography = biography
+    penName.avatarId = avatarId
+    penName.avatarUrl = avatarUrl
+    penName.facebookUrl = facebookUrl
+    penName.tumblrUrl = tumblrUrl
+    penName.googlePlusUrl = googlePlusUrl
+    penName.twitterUrl = twitterUrl
+    penName.instagramUrl = instagramUrl
+    penName.pinterestUrl = pinterestUrl
+    penName.youtubeUrl = youtubeUrl
+    penName.linkedinUrl = linkedinUrl
+    penName.wordpressUrl = wordpressUrl
+    penName.websiteUrl = websiteUrl
+    return penName.serializeData(options: [.OmitNullValues])
+  }
+
   static func updatePostBody(identifier: String, name: String?, biography: String?, avatarId: String?, avatarUrl: String?, facebookUrl: String?, tumblrUrl: String?, googlePlusUrl: String?, twitterUrl: String?, instagramUrl: String?, pinterestUrl: String?, youtubeUrl: String?, linkedinUrl: String?, wordpressUrl: String?, websiteUrl: String?) -> [String : Any]? {
     let penName = PenName()
     penName.id = identifier
