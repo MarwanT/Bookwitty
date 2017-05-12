10d90c43143953349373a51e4f47fa23d9184f3f
Shafic Hariri <shafic.hariri@keeward.com>
2017-05-12 15:05:58 +0300

    Clean up: Discover node

--- Bookwitty/Components/Nodes/DiscoverNode.swift
+++ Bookwitty/Components/Nodes/DiscoverNode.swift
@@ -12,7 +12,6 @@ import AsyncDisplayKit
 class DiscoverNode: ASDisplayNode {
 
   fileprivate let segmentedNodeHeight: CGFloat = 45.0
-  fileprivate let externalMargin = ThemeManager.shared.currentTheme.cardExternalMargin()
 
   fileprivate let collectionNode: ASCollectionNode
   fileprivate let flowLayout: UICollectionViewFlowLayout
@@ -20,7 +19,7 @@ class DiscoverNode: ASDisplayNode {
   
   override init() {
     flowLayout = UICollectionViewFlowLayout()
-    flowLayout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: externalMargin/2, right: 0)
+    flowLayout.sectionInset = UIEdgeInsets(top: 0.0, left: 0, bottom: 0.0, right: 0)
     flowLayout.minimumInteritemSpacing  = 0
     flowLayout.minimumLineSpacing       = 0
     collectionNode = ASCollectionNode(collectionViewLayout: flowLayout)
@@ -30,7 +29,7 @@ class DiscoverNode: ASDisplayNode {
     setupNode()
   }
 
-  func setupNode() {
+  private func setupNode() {
     //Setup elements dimensions
     segmentedNode.style.preferredSize = CGSize(width: collectionNode.style.maxWidth.value, height: segmentedNodeHeight)
     collectionNode.style.flexGrow = 1.0
