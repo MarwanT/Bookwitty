e36b0ccd02bb847a6a4eef298142f3da27dae443
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-05-12 13:37:22 +0300

    Register user agent value in user defaults
    this way all webviews in the app use this field to add it as a parameter for the requests made

--- Bookwitty/AppDelegate.swift
+++ Bookwitty/AppDelegate.swift
@@ -19,7 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
+    // Register User Agent for network requests
+    UserDefaults.standard.register(defaults: ["UserAgent" : userAgentValue])
     
     applyTheme()
     
