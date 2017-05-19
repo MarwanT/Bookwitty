77b5788bd42d27dd2e915dbcd18d9a5dede531a5
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-05-19 13:37:01 +0300

    Add web vc subviews to view hierarchy and configure them

--- Bookwitty/ViewControllers/General/WebViewController.swift
+++ Bookwitty/ViewControllers/General/WebViewController.swift
@@ -13,7 +13,41 @@ class WebViewController: UIViewController {
   let webView: UIWebView = UIWebView()
   let textField: UITextField = UITextField()
   
+  var configuration = Configuration()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    self.view.addSubview(webView)
+    setupWebView()
+    setupNavigationBar()
+  }
+  
+  private func setupWebView() {
+    // Setup layout
+    webView.alignTop("0", leading: "0", bottom: "0", trailing: "0", toView: view)
+  }
+  
+  private func setupNavigationBar() {
+    let doneBarButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.done, target: self, action: #selector(doneBarButtonTouchUpInside(_:)))
+    self.navigationItem.leftBarButtonItem = doneBarButton
+    
+    textField.isUserInteractionEnabled = false
+    textField.textAlignment = NSTextAlignment.center
+    textField.textColor = configuration.textFieldTextColor
+    textField.borderStyle = UITextBorderStyle.roundedRect
+    let rightBarButton = UIBarButtonItem(customView: textField)
+    self.navigationItem.rightBarButtonItem = rightBarButton
+  }
+  
+  // MARK: Actions
+  func doneBarButtonTouchUpInside(_ sender: Any?) {
+    self.dismiss(animated: true, completion: nil)
+  }
+}
+
+// MARK: - Configuration Struct Declaration
+extension WebViewController {
+  struct Configuration {
+    var textFieldTextColor: UIColor = ThemeManager.shared.currentTheme.defaultTextColor()
   }
 }
