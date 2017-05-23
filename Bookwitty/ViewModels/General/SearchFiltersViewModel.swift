7dbb71a2fab68ec91f073d900350361e0f956288
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-05-23 12:06:54 +0300

    Add the search filters view model

---
+++ Bookwitty/ViewModels/General/SearchFiltersViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SearchFiltersViewModel.swift
+//  Bookwitty
+//
+//  Created by charles on 5/23/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+class SearchFiltersViewModel {
+
+}
