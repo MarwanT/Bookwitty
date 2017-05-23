373eb4e2ed11982ae759f7031c52c1f2d5a71084
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-05-23 11:58:08 +0300

    Add the search filters view controller

---
+++ Bookwitty/ViewControllers/General/SearchFiltersViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchFiltersViewController.swift
+//  Bookwitty
+//
+//  Created by charles on 5/23/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class SearchFiltersViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+}
