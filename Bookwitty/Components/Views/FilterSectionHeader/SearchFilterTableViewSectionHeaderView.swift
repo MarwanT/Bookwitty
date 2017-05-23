dac04a1d191c24e4ca55996bde6ab6837891bd51
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-05-23 17:04:15 +0300

    Add the selected variable and apply the selected state to the image view

--- Bookwitty/Components/Views/FilterSectionHeader/SearchFilterTableViewSectionHeaderView.swift
+++ Bookwitty/Components/Views/FilterSectionHeader/SearchFilterTableViewSectionHeaderView.swift
@@ -13,6 +13,12 @@ class SearchFilterTableViewSectionHeaderView: UITableViewHeaderFooterView {
   @IBOutlet var subTitleLabel: UILabel!
   @IBOutlet var imageView: UIImageView!
 
+  var selected: Bool = false {
+    didSet {
+      applySelectedState()
+    }
+  }
+
   override func awakeFromNib() {
     super.awakeFromNib()
 
@@ -32,5 +38,9 @@ extension SearchFilterTableViewSectionHeaderView: Themeable {
 
     subTitleLabel.font = FontDynamicType.caption1.font
     subTitleLabel.textColor = ThemeManager.shared.currentTheme.defaultTextColor()
+  }
+
+  fileprivate func applySelectedState() {
+    imageView.image = selected ? #imageLiteral(resourceName: "tick") : nil
   }
 }
