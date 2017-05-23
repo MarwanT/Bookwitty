686f513c959003c1abf4cd206b8bd434319be71e
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-05-23 16:45:02 +0300

    Add the search filter section header view

---
+++ Bookwitty/Components/Views/FilterSectionHeader/SearchFilterTableViewSectionHeaderView.swift
@@ -0,0 +1,13 @@
+//
+//  SearchFilterTableViewSectionHeaderView.swift
+//  Bookwitty
+//
+//  Created by charles on 5/23/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class SearchFilterTableViewSectionHeaderView: UITableViewHeaderFooterView {
+  
+}
