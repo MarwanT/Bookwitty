4fe609a231226fec596324d97512a64ab9548b00
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-05-23 18:07:59 +0300

    Add the check mark table view cell

---
+++ Bookwitty/Components/Views/CheckmarkTableViewCell/CheckmarkTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  CheckmarkTableViewCell.swift
+//  Bookwitty
+//
+//  Created by charles on 5/23/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class CheckmarkTableViewCell: UITableViewCell {
+
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    initializeComponents()
+  }
+
+  fileprivate func initializeComponents() {
+
+  }
+
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+
+    // Configure the view for the selected state
+  }
+}
