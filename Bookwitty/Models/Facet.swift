86ee0b3512b961de1784ec62725a0116408924c7
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-05-24 16:15:04 +0300

    Add the facet model

---
+++ Bookwitty/Models/Facet.swift
@@ -0,0 +1,34 @@
+//
+//  Facet.swift
+//  Bookwitty
+//
+//  Created by charles on 5/24/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+class Facet {
+  var categories: [Category]?
+  var languages: [String]?
+  var types: [String]?
+
+  fileprivate var categoriesCodes: [String]?
+
+  init(categories: [String]?, languages: [String]?, types: [String]?) {
+    self.categoriesCodes = categories
+    self.languages = languages
+    self.types = types
+
+    if let codes = categoriesCodes {
+      self.categories = CategoryManager.shared.categories(from: codes)
+    }
+  }
+
+  convenience init(from dictionary: [String : Any]) {
+    let categories = (dictionary["categories"] as? [[String : Any]])?.map({ $0["term"] }) as? [String]
+    let languages = (dictionary["languages"] as? [[String : Any]])?.map({ $0["term"] }) as? [String]
+    let types = (dictionary["types"] as? [[String : Any]])?.map({ $0["term"] }) as? [String]
+    self.init(categories: categories, languages: languages, types: types)
+  }
+}
