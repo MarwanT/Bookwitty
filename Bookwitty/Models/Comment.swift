58e186c386b2f639fb09fecc0fa2b5d294504295
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-05-24 17:40:38 +0300

    Declare Comment class

---
+++ Bookwitty/Models/Comment.swift
@@ -0,0 +1,14 @@
+//
+//  Comment.swift
+//  Bookwitty
+//
+//  Created by Marwan  on 5/24/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+import Spine
+
+class Comment: Resource {
+  
+}
