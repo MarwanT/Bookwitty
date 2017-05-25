403876b9fe8b5dfc5e9db060ccad989335a6ff33
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-05-25 11:37:56 +0300

    Implement get comment replies API

--- Bookwitty/Models/API/CommentAPI.swift
+++ Bookwitty/Models/API/CommentAPI.swift
@@ -40,6 +40,37 @@ struct CommentAPI {
       }
     })
   }
+  
+  public static func commentReplies(identifier: String, completion: @escaping (_ success: Bool, _ comments: [Comment]?, _ error: BookwittyAPIError?) -> Void) -> Cancellable? {
+    let retieveCommentsSuccessStatusCode = 200
+    
+    return signedAPIRequest(target: .replies(commentIdentifier: identifier), completion: {
+      (data, statucCode, response, error) in
+      DispatchQueue.global(qos: .background).async {
+        var success: Bool = false
+        var commentsArray: [Comment]? = nil
+        var completionError: BookwittyAPIError? = error
+        defer {
+          DispatchQueue.main.async {
+            completion(success, commentsArray, error)
+          }
+        }
+        
+        guard let statucCode = statucCode, statucCode == retieveCommentsSuccessStatusCode else {
+          completionError = BookwittyAPIError.invalidStatusCode
+          return
+        }
+        
+        // Parse Data
+        if let data = data, let comments = Comment.parseDataArray(data: data)?.resources {
+          commentsArray = comments
+          success = true
+        } else {
+          completionError = BookwittyAPIError.failToParseData
+        }
+      }
+    })
+  }
 }
 
 extension CommentAPI {
