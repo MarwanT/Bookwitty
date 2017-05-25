04d19cb1601185b2dc8a40b393d9d6bb954dc04f
Shafic Hariri <shafic.hariri@keeward.com>
2017-05-25 18:24:14 +0300

    Fix: [Update penname] Handle error code

--- Bookwitty/Models/API/PenName+API.swift
+++ Bookwitty/Models/API/PenName+API.swift
@@ -67,6 +67,10 @@ struct PenNameAPI {
   }
 
   public static func updatePenName(identifier: String, name: String?, biography: String?, avatarId: String?, avatarUrl: String?, facebookUrl: String?, tumblrUrl: String?, googlePlusUrl: String?, twitterUrl: String?, instagramUrl: String?, pinterestUrl: String?, youtubeUrl: String?, linkedinUrl: String?, wordpressUrl: String?, websiteUrl: String?, completionBlock: @escaping (_ success: Bool, _ penName: PenName?, _ error: BookwittyAPIError?)->()) -> Cancellable? {
+
+    let successStatusCode = 200
+    let errorStatusCode = 422
+
     return signedAPIRequest(target: BookwittyAPI.updatePenName(identifier: identifier, name: name, biography: biography, avatarId: avatarId, avatarUrl: avatarUrl, facebookUrl: facebookUrl, tumblrUrl: tumblrUrl, googlePlusUrl: googlePlusUrl, twitterUrl: twitterUrl, instagramUrl: instagramUrl, pinterestUrl: pinterestUrl, youtubeUrl: youtubeUrl, linkedinUrl: linkedinUrl, wordpressUrl: wordpressUrl, websiteUrl: websiteUrl), completion: {
       (data, statusCode, response, error) in
       var success: Bool = false
@@ -76,6 +80,15 @@ struct PenNameAPI {
         completionBlock(success, penName, error)
       }
 
+      guard statusCode == successStatusCode else {
+        if statusCode == errorStatusCode {
+          error = BookwittyAPIError.penNameHasAlreadyBeenTaken
+        } else {
+          error = BookwittyAPIError.undefined
+        }
+        return
+      }
+      
       if let data = data {
         penName = PenName.parseData(data: data)
         UserManager.shared.replaceUpdated(penName: penName)
