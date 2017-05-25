f8d47514372b2fe6c2c32c32d0ca98ba89a8dce6
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-05-25 12:05:40 +0300

    Add the sections array and the helper funtion to fill the sections from the facet

--- Bookwitty/ViewModels/General/SearchFiltersViewModel.swift
+++ Bookwitty/ViewModels/General/SearchFiltersViewModel.swift
@@ -11,7 +11,27 @@ import Foundation
 class SearchFiltersViewModel {
   var facet: Facet?
 
+  var sections: [Facet.Options] = []
+
   func initialize(with facet: Facet) {
     self.facet = facet
+  }
+
+  fileprivate func fillSections() {
+    guard let facet = self.facet else {
+      return
+    }
+
+    if facet.categories != nil {
+      sections.append(Facet.Options.categories)
+    }
+
+    if facet.languages != nil {
+      sections.append(Facet.Options.languages)
+    }
+
+    if facet.types != nil {
+      sections.append(Facet.Options.types)
+    }
   }
 }
