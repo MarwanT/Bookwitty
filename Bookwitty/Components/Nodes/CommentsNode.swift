9dd7e5e0cbea80e38424574e53a5c8d7caf13530
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-06-01 13:41:06 +0300

    Populate the comments node collection node

--- Bookwitty/Components/Nodes/CommentsNode.swift
+++ Bookwitty/Components/Nodes/CommentsNode.swift
@@ -28,6 +28,9 @@ class CommentsNode: ASCellNode {
     
     super.init()
     
+    collectionNode.delegate = self
+    collectionNode.dataSource = self
+    
     loaderNode.style.width = ASDimensionMake(UIScreen.main.bounds.width)
     
     automaticallyManagesSubnodes = true
@@ -41,6 +44,61 @@ class CommentsNode: ASCellNode {
     collectionNode.style.preferredSize = constrainedSize.max
     let externalInsetsSpec = ASInsetLayoutSpec(insets: configuration.externalInsets, child: collectionNode)
     return externalInsetsSpec
+  }
+}
+
+// MARK: - Collection View Delegates
+extension CommentsNode: ASCollectionDelegate, ASCollectionDataSource {
+  func numberOfSections(in collectionNode: ASCollectionNode) -> Int {
+    return viewModel.numberOfSection
+  }
+  
+  func collectionNode(_ collectionNode: ASCollectionNode, numberOfItemsInSection section: Int) -> Int {
+    switch section {
+    case Section.activityIndicator.rawValue:
+      return shouldShowLoader ? 1 : 0
+    default:
+      return viewModel.numberOfItems(in: section)
+    }
+  }
+  
+  func collectionNode(_ collectionNode: ASCollectionNode, nodeBlockForItemAt indexPath: IndexPath) -> ASCellNodeBlock {
+    return {
+      switch indexPath.section {
+      case Section.header.rawValue:
+        let externalInsets = UIEdgeInsets(
+          top: ThemeManager.shared.currentTheme.generalExternalMargin(),
+          left: 0, bottom: 0, right: 0)
+        let headerNode = SectionTitleHeaderNode(externalInsets: externalInsets)
+        headerNode.setTitle(
+          title: "Comments", // TODO: Localize
+          verticalBarColor: ThemeManager.shared.currentTheme.colorNumber6(),
+          horizontalBarColor: ThemeManager.shared.currentTheme.colorNumber5())
+        return headerNode
+      case Section.write.rawValue:
+        return ASCellNode()
+      case Section.read.rawValue:
+        guard let comment = self.viewModel.comment(for: indexPath) else {
+          return ASCellNode()
+        }
+        let commentTreeNode = CommentTreeNode()
+        commentTreeNode.comment = comment
+        return commentTreeNode
+      case Section.activityIndicator.rawValue:
+        return self.loaderNode
+      default:
+        return ASCellNode()
+      }
+    }
+  }
+  
+  func collectionNode(_ collectionNode: ASCollectionNode, willDisplayItemWith node: ASCellNode) {
+    switch node {
+    case loaderNode:
+      loaderNode.updateLoaderVisibility(show: true)
+    default:
+      return
+    }
   }
 }
 
