8113676568b1826255e6e68b5244188e6b842abb
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-06-01 13:11:39 +0300

    Defile and initialize comment node subnodes

--- Bookwitty/Components/Nodes/CommentsNode.swift
+++ Bookwitty/Components/Nodes/CommentsNode.swift
@@ -9,4 +9,23 @@
 import AsyncDisplayKit
 
 class CommentsNode: ASCellNode {
+  let flowLayout: UICollectionViewFlowLayout
+  let collectionNode: ASCollectionNode
+  let loaderNode: LoaderNode
+  
+  override init() {
+    flowLayout = UICollectionViewFlowLayout()
+    flowLayout.sectionInset = UIEdgeInsets.zero
+    flowLayout.minimumInteritemSpacing  = 0
+    flowLayout.minimumLineSpacing       = 0
+    
+    collectionNode = ASCollectionNode(collectionViewLayout: flowLayout)
+    loaderNode = LoaderNode()
+    
+    super.init()
+    
+    loaderNode.style.width = ASDimensionMake(UIScreen.main.bounds.width)
+    
+    automaticallyManagesSubnodes = true
+  }
 }
