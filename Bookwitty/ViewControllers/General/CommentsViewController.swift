5d6bbfd0655e0820694253a13bc2633523f0f697
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-06-01 14:02:11 +0300

    Load comments when view controller loads

--- Bookwitty/ViewControllers/General/CommentsViewController.swift
+++ Bookwitty/ViewControllers/General/CommentsViewController.swift
@@ -19,4 +19,13 @@ class CommentsViewController: ASViewController<ASDisplayNode> {
   required init?(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    reloadData()
+  }
+  
+  func reloadData() {
+    commentsNode.reloadData()
+  }
 }
