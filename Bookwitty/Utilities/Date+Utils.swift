457d8598211dc9e70d379a42537b75bdb4310884
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-06-15 11:14:56 +0300

    Add extension to date formatter to get a relative date format given a date

--- Bookwitty/Utilities/Date+Utils.swift
+++ Bookwitty/Utilities/Date+Utils.swift
@@ -38,3 +38,80 @@ extension NSDate {
     return (self as Date).formatted(format: format)
   }
 }
+
+
+extension DateFormatter {
+  /**
+   Formats a date as the time since that date (e.g., “Last week, yesterday, etc.”).
+   
+   - Parameter from: The date to process.
+   - Parameter numericDates: Determines if we should return a numeric variant, e.g. "1 month ago" vs. "Last month".
+   
+   - Returns: A string with formatted `date`.
+   */
+  func timeSince(from: Date, numericDates: Bool = false) -> String {
+    let calendar = Calendar.current
+    let now = Date()
+    let earliest = (now as NSDate).earlierDate(from)
+    let latest = earliest == now ? from : now
+    let components = calendar.dateComponents([.year, .weekOfYear, .month, .day, .hour, .minute, .second], from: earliest, to: latest)
+    
+    var result = ""
+    
+    if components.year! >= 2 {
+      result = "\(components.year!) years ago"
+    } else if components.year! >= 1 {
+      if numericDates {
+        result = "1 year ago"
+      } else {
+        result = "Last year"
+      }
+    } else if components.month! >= 2 {
+      result = "\(components.month!) months ago"
+    } else if components.month! >= 1 {
+      if numericDates {
+        result = "1 month ago"
+      } else {
+        result = "Last month"
+      }
+    } else if components.weekOfYear! >= 2 {
+      result = "\(components.weekOfYear!) weeks ago"
+    } else if components.weekOfYear! >= 1 {
+      if numericDates {
+        result = "1 week ago"
+      } else {
+        result = "Last week"
+      }
+    } else if components.day! >= 2 {
+      result = "\(components.day!) days ago"
+    } else if components.day! >= 1 {
+      if numericDates {
+        result = "1 day ago"
+      } else {
+        result = "Yesterday"
+      }
+    } else if components.hour! >= 2 {
+      result = "\(components.hour!) hours ago"
+    } else if components.hour! >= 1 {
+      if numericDates {
+        result = "1 hour ago"
+      } else {
+        result = "An hour ago"
+      }
+    } else if components.minute! >= 2 {
+      result = "\(components.minute!) minutes ago"
+    } else if components.minute! >= 1 {
+      if numericDates {
+        result = "1 minute ago"
+      } else {
+        result = "A minute ago"
+      }
+    } else if components.second! >= 3 {
+      result = "\(components.second!) seconds ago"
+    } else {
+      result = "Just now"
+    }
+    
+    return result
+  }
+}
