488b9b20db2c4682a438afdc0b89493d40748a4d
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-06-22 10:59:10 +0300

    Add the pen name list view controller

---
+++ Bookwitty/ViewControllers/Lists/PenNameListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PenNameListViewController.swift
+//  Bookwitty
+//
+//  Created by Charles Abou Yakzan on 6/22/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+import AsyncDisplayKit
+
+class PenNameListViewController: ASViewController<ASCollectionNode> {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+}
