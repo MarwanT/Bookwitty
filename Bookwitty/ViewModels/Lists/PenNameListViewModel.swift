1ade267f9f5bed7b0473dad51aaab99e458fe0a4
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-06-22 15:01:31 +0300

    Add the view model file

---
+++ Bookwitty/ViewModels/Lists/PenNameListViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  PenNameListViewModel.swift
+//  Bookwitty
+//
+//  Created by Charles Abou Yakzan on 6/22/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+class PenNameListViewModel {
+
+}
