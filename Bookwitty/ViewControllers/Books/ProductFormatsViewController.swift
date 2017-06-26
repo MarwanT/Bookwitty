f4fd662bef3a38f477c2b1c1f2a53728b8d2bb93
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-06-26 13:30:08 +0300

    Create 'Product Formats VC'

---
+++ Bookwitty/ViewControllers/Books/ProductFormatsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProductFormatsViewController.swift
+//  Bookwitty
+//
+//  Created by Marwan  on 6/26/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class ProductFormatsViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+  }
+}
