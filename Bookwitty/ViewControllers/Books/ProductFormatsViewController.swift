7ad9713734049e9686ce0dd319d7d4ee041270fb
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-06-26 14:14:00 +0300

    Add product formats view model

--- Bookwitty/ViewControllers/Books/ProductFormatsViewController.swift
+++ Bookwitty/ViewControllers/Books/ProductFormatsViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ProductFormatsViewController: UIViewController {
+  fileprivate var viewModel = ProductFormatsViewModel()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
---
+++ Bookwitty/ViewModels/Books/ProductFormatsViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ProductFormatsViewModel.swift
+//  Bookwitty
+//
+//  Created by Marwan  on 6/26/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+final class ProductFormatsViewModel {
+  
+}
