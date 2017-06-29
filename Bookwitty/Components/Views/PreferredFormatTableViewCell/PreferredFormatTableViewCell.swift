f7870992e163de36b79431578d12707a13c67142
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-06-29 21:35:20 +0300

    Add empty declaration of 'Preferred format table view cell'

---
+++ Bookwitty/Components/Views/PreferredFormatTableViewCell/PreferredFormatTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PreferredFormatTableViewCell.swift
+//  Bookwitty
+//
+//  Created by Marwan  on 6/28/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class PreferredFormatTableViewCell: UITableViewCell {
+}
