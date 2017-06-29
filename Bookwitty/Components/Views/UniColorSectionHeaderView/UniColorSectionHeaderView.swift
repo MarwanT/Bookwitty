5c109d50947a497972f3630f8dee22c9bae9c2b0
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-06-29 23:29:13 +0300

    Declare 'Uni color header section view'

---
+++ Bookwitty/Components/Views/UniColorSectionHeaderView/UniColorSectionHeaderView.swift
@@ -0,0 +1,13 @@
+//
+//  UniColorSectionHeaderView.swift
+//  Bookwitty
+//
+//  Created by Marwan  on 6/29/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class UniColorSectionHeaderView: UITableViewHeaderFooterView {
+  
+}
