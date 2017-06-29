c13862b019431eaf55f78f7333742f1841920861
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-06-29 18:57:04 +0300

    Add the comment compact node

---
+++ Bookwitty/Components/Nodes/CommentCompactNode.swift
@@ -0,0 +1,14 @@
+//
+//  CommentCompactNode.swift
+//  Bookwitty
+//
+//  Created by Charles Abou Yakzan on 6/29/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+import AsyncDisplayKit
+
+class CommentCompactNode: ASCellNode {
+
+}
