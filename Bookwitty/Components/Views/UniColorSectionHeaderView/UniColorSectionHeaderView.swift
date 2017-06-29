0ce0a730b53d6c8cf17b7e1ff5af7f72d6eb2f82
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-06-30 00:29:03 +0300

    Add top and bottom separators

--- Bookwitty/Components/Views/UniColorSectionHeaderView/UniColorSectionHeaderView.swift
+++ Bookwitty/Components/Views/UniColorSectionHeaderView/UniColorSectionHeaderView.swift
@@ -7,17 +7,41 @@
 //
 
 import UIKit
+import FLKAutoLayout
 
 class UniColorSectionHeaderView: UITableViewHeaderFooterView {
   static let reuseIdentifier = "UniColorSectionHeaderView"
   
+  fileprivate var topSeparator: UIView!
+  fileprivate var bottomSeparator: UIView!
+  
   override init(reuseIdentifier: String?) {
     super.init(reuseIdentifier: reuseIdentifier)
+    initializeView()
     applyTheme()
   }
   
   required init?(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func initializeView() {
+    topSeparator = separatorViewInstance()
+    bottomSeparator = separatorViewInstance()
+    contentView.addSubview(topSeparator)
+    contentView.addSubview(bottomSeparator)
+    
+    topSeparator.alignTop("0", leading: "0", toView: contentView)
+    topSeparator.constrainWidth(toView: contentView, predicate: "0")
+    bottomSeparator.alignBottom("0", trailing: "0", toView: contentView)
+    bottomSeparator.constrainWidth(toView: contentView, predicate: "0")
+  }
+  
+  fileprivate func separatorViewInstance() -> UIView {
+    let separatorView = UIView(frame: CGRect.zero)
+    separatorView.backgroundColor = ThemeManager.shared.currentTheme.defaultSeparatorColor()
+    separatorView.constrainHeight("1")
+    return separatorView
   }
 }
 
