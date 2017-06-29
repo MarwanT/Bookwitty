f27dbf618c8694dc48175fe1de93617da85ffe49
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-06-30 00:28:45 +0300

    Configure header view styling

--- Bookwitty/Components/Views/UniColorSectionHeaderView/UniColorSectionHeaderView.swift
+++ Bookwitty/Components/Views/UniColorSectionHeaderView/UniColorSectionHeaderView.swift
@@ -11,4 +11,20 @@ import UIKit
 class UniColorSectionHeaderView: UITableViewHeaderFooterView {
   static let reuseIdentifier = "UniColorSectionHeaderView"
   
+  override init(reuseIdentifier: String?) {
+    super.init(reuseIdentifier: reuseIdentifier)
+    applyTheme()
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
+
+extension UniColorSectionHeaderView: Themeable {
+  func applyTheme() {
+    textLabel?.font = FontDynamicType.subheadline.font
+    textLabel?.textColor = ThemeManager.shared.currentTheme.defaultTextColor()
+    contentView.backgroundColor = ThemeManager.shared.currentTheme.colorNumber2()
+  }
 }
