041bd9a02f8c85635cb17bb24830020006b346c2
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-07-01 02:08:27 +0300

    Declare format edition table view cell

---
+++ Bookwitty/Components/Views/FormatEditionTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  FormatEditionTableViewCell.swift
+//  Bookwitty
+//
+//  Created by Marwan  on 7/1/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+class FormatEditionTableViewCell: UITableViewCell {
+}
