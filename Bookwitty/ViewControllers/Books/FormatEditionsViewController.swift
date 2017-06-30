267d8b05eab1bbb3f1b03466cff2aa15ae6e1714
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-06-30 20:01:50 +0300

    Add Format Editions VC

---
+++ Bookwitty/ViewControllers/Books/FormatEditionsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FormatEditionsViewController.swift
+//  Bookwitty
+//
+//  Created by Marwan  on 6/30/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class FormatEditionsViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+}
