8ccae55525d4edebb49e982ae2b2ce77c2f1e6a6
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-06-30 14:11:40 +0300

    Add the vote resource model

---
+++ Bookwitty/Models/Vote.swift
@@ -0,0 +1,14 @@
+//
+//  Vote.swift
+//  Bookwitty
+//
+//  Created by Charles Abou Yakzan on 6/30/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+import Spine
+
+class Vote: Resource {
+
+}
