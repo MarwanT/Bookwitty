653640146405c114aa7b606bd0fec2ff9d3ab989
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-06-30 20:26:26 +0300

    Set table view data source and delegate

--- Bookwitty/ViewControllers/Books/FormatEditionsViewController.swift
+++ Bookwitty/ViewControllers/Books/FormatEditionsViewController.swift
@@ -15,6 +15,27 @@ class FormatEditionsViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    initializeTableView()
+  }
+  
+  private func initializeTableView() {
+    tableView.delegate = self
+    tableView.dataSource = self
+  }
+}
+
+// MARK: - FormatEditionsViewController
+extension FormatEditionsViewController: UITableViewDelegate, UITableViewDataSource {
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 0
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 0
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    return UITableViewCell()
   }
 }
