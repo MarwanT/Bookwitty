581f31aff03d7718a62a00d9b85cb1a77cfb8a38
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-06-30 20:05:28 +0300

    Link table view to outlet

--- Bookwitty/ViewControllers/Books/FormatEditionsViewController.swift
+++ Bookwitty/ViewControllers/Books/FormatEditionsViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class FormatEditionsViewController: UIViewController {
+  @IBOutlet weak var tableView: UITableView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
