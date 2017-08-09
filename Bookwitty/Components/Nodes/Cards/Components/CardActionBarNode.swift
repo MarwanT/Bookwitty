301f99be3c3cbeff801a4b0fba77cf57e2be225a
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-08-09 13:31:05 +0300

    Add the target for the more button

--- Bookwitty/Components/Nodes/Cards/Components/CardActionBarNode.swift
+++ Bookwitty/Components/Nodes/Cards/Components/CardActionBarNode.swift
@@ -105,6 +105,7 @@ class CardActionBarNode: ASCellNode {
     setupFollowButtonStyling()
     setupReplyNodeStyling()
 
+    moreButton.addTarget(self, action: #selector(moreButtonTouchUpInside(_:)), forControlEvents: .touchUpInside)
     shareButton.addTarget(self, action: #selector(shareButtonTouchUpInside(_:)), forControlEvents: .touchUpInside)
     commentButton.addTarget(self, action: #selector(commentButtonTouchUpInside(_:)), forControlEvents: .touchUpInside)
     witButton.addTarget(self, action: #selector(witButtonTouchUpInside(_:)), forControlEvents: .touchUpInside)
@@ -247,6 +248,11 @@ class CardActionBarNode: ASCellNode {
     delegate?.cardActionBarNode(cardActionBar: self, didRequestAction: CardActionBarNode.Action.share, forSender: sender, didFinishAction: nil)
   }
   
+  func moreButtonTouchUpInside(_ sender: ASButtonNode?) {
+    guard let sender = sender else { return }
+    
+  }
+
   func replyButtonTouchUpInside(_ sender: ASButtonNode?) {
     guard let sender = sender else { return }
     delegate?.cardActionBarNode(cardActionBar: self, didRequestAction: CardActionBarNode.Action.reply, forSender: sender, didFinishAction: nil)
