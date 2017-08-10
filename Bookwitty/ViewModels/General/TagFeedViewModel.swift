6cf486b10141a99c5bc0f91b5ac8f2e7b4ed2b21
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-08-10 13:05:22 +0300

    Add the tag feed view model

---
+++ Bookwitty/ViewModels/General/TagFeedViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TagFeedViewModel.swift
+//  Bookwitty
+//
+//  Created by Charles Abou Yakzan on 2017/08/10.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+class TagFeedViewModel {
+
+}
