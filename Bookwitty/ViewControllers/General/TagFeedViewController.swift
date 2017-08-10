67856eb9d6f0e2c9433a3c4ffbf0639690fb65c0
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-08-10 13:09:31 +0300

    Add the view model to the controller

--- Bookwitty/ViewControllers/General/TagFeedViewController.swift
+++ Bookwitty/ViewControllers/General/TagFeedViewController.swift
@@ -14,6 +14,8 @@ class TagFeedViewController: ASViewController<ASCollectionNode> {
   let collectionNode: ASCollectionNode
   let flowLayout: UICollectionViewFlowLayout
 
+  fileprivate let viewModel = TagFeedViewModel()
+
   required init?(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
