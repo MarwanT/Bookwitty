6272302de9b42ffde5ca0c8bf5ab2c142c28d6a2
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-08-10 16:08:03 +0300

    Add the data array and the related resource helper functions

--- Bookwitty/ViewModels/General/TagFeedViewModel.swift
+++ Bookwitty/ViewModels/General/TagFeedViewModel.swift
@@ -9,5 +9,18 @@
 import Foundation
 
 class TagFeedViewModel {
+  var data: [String] = []
 
+  func resourceFor(id: String?) -> ModelResource? {
+    guard let id = id else {
+      return nil
+    }
+    return DataManager.shared.fetchResource(with: id)
+  }
+
+  func resourceForIndex(index: Int) -> ModelResource? {
+    guard index >= 0, data.count > index else { return nil }
+    let resource = resourceFor(id: data[index])
+    return resource
+  }
 }
