7c53648fbf8fc0fa373293b0e71e42243bd5080d
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-08-11 12:26:00 +0300

    Add the tag model

---
+++ Bookwitty/Models/Tag.swift
@@ -0,0 +1,14 @@
+//
+//  Tag.swift
+//  Bookwitty
+//
+//  Created by Charles Abou Yakzan on 2017/08/11.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+import Spine
+
+class Tag: Resource {
+
+}
