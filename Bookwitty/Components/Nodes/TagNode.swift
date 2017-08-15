a50bf3c3507fc010b44b9145e0ec05f6ec9847cf
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-08-15 13:08:37 +0300

    Add the tag node

---
+++ Bookwitty/Components/Nodes/TagNode.swift
@@ -0,0 +1,14 @@
+//
+//  TagNode.swift
+//  Bookwitty
+//
+//  Created by Charles Abou Yakzan on 2017/08/15.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+import AsyncDisplayKit
+
+class TagNode: ASCellNode {
+
+}
