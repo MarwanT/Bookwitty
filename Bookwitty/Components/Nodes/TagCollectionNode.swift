e2fab7fd9325e30ef6cde532da7434f1a0881e97
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-08-15 13:50:20 +0300

    Add the tag collection node

---
+++ Bookwitty/Components/Nodes/TagCollectionNode.swift
@@ -0,0 +1,14 @@
+//
+//  TagCollectionNode.swift
+//  Bookwitty
+//
+//  Created by Charles Abou Yakzan on 2017/08/15.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+import AsyncDisplayKit
+
+class TagCollectionNode: ASCellNode {
+
+}
