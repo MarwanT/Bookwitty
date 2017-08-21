9423157a00b38accec5413133372b3ca2bd7821a
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-08-21 19:30:36 +0300

    Add the auth platforms file

---
+++ Bookwitty/Utilities/Auth/AuthPlatforms+Constants.swift
@@ -0,0 +1,9 @@
+//
+//  AuthPlatforms+Constants.swift
+//  Bookwitty
+//
+//  Created by Charles Abou Yakzan on 2017/08/21.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
