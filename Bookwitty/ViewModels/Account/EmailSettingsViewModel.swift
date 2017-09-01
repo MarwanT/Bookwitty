cb2b031ea6e31760f1b5a26f90e1be4ce8a657ec
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-09-01 18:39:17 +0300

    Add the email settings view model

---
+++ Bookwitty/ViewModels/Account/EmailSettingsViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  EmailSettingsViewModel.swift
+//  Bookwitty
+//
+//  Created by Charles Abou Yakzan on 2017/09/01.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+final class EmailSettingsViewModel {
+
+}
