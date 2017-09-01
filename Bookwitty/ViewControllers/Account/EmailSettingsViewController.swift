2289d7198a8d51e391258706ad57d79e09491f7e
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-09-01 18:29:05 +0300

    Add the email settings view controller

---
+++ Bookwitty/ViewControllers/Account/EmailSettingsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  EmailSettingsViewController.swift
+//  Bookwitty
+//
+//  Created by Charles Abou Yakzan on 2017/09/01.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class EmailSettingsViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+}
