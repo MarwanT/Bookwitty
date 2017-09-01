56f72b948021a841f7507e55573d16d12275f1a4
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-09-01 19:25:18 +0300

    Add the table view functions in the view controller

--- Bookwitty/ViewControllers/Account/EmailSettingsViewController.swift
+++ Bookwitty/ViewControllers/Account/EmailSettingsViewController.swift
@@ -36,3 +36,75 @@ extension EmailSettingsViewController: Themeable {
     view.backgroundColor = ThemeManager.shared.currentTheme.colorNumber2()
   }
 }
+
+extension EmailSettingsViewController: UITableViewDataSource, UITableViewDelegate {
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return viewModel.numberOfSections()
+  }
+
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return viewModel.numberOfRowsIn(section: section)
+  }
+
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 45.0
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    return tableView.dequeueReusableCell(withIdentifier: DisclosureTableViewCell.identifier, for: indexPath)
+  }
+
+  func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+    let values = viewModel.values(forRowAt: indexPath)
+
+    guard let currentCell = cell as? DisclosureTableViewCell else {
+      return
+    }
+
+    currentCell.label.font = FontDynamicType.caption1.font
+    currentCell.label.text = values.title
+    currentCell.detailsLabel.text = values.value as? String
+
+    let accessory = viewModel.accessory(forRowAt: indexPath)
+
+    var hideDiclosure = true
+    switch accessory {
+    case .Disclosure:
+      hideDiclosure = false
+    case .Switch:
+      let switchView = UISwitch()
+      switchView.isOn = (values.value as? Bool ?? false)
+      currentCell.accessoryView = switchView
+    case .None:
+      break
+    }
+
+    currentCell.disclosureImageView.isHidden = hideDiclosure
+  }
+
+  func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+    let sectionView = tableView.dequeueReusableHeaderFooterView(withIdentifier: TableViewSectionHeaderView.reuseIdentifier) as? TableViewSectionHeaderView
+    sectionView?.label.text = viewModel.titleFor(section: section)
+    /** Discussion
+     * Setting the background color on UITableViewHeaderFooterView has been deprecated, BUT contentView.backgroundColor was not working on the IPOD or IPHONE-5/s
+     * so we kept both until 'contentView.backgroundColor' work 100% on all supported devices
+     */
+    sectionView?.contentView.backgroundColor = ThemeManager.shared.currentTheme.colorNumber2()
+    if let imagebg = ThemeManager.shared.currentTheme.colorNumber2().image(size: CGSize(width: sectionView?.frame.width ?? 0.0, height: sectionView?.frame.height ?? 0.0)) {
+      sectionView?.backgroundView = UIImageView(image: imagebg)
+    }
+
+    sectionView?.separators?.first?.isHidden = section == 0
+
+    return sectionView
+  }
+
+  func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+    return 15.0
+  }
+
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
+    
+  }
+}
