a917a6c9a7d008aff1b836944778020af332d5c8
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-09-15 12:52:28 +0300

    Remove the comment text edit border

--- Bookwitty/Components/Nodes/Cards/BaseCardPostNode.swift
+++ Bookwitty/Components/Nodes/Cards/BaseCardPostNode.swift
@@ -218,6 +218,7 @@ class BaseCardPostNode: ASCellNode, NodeTapProtocol {
 
     writeCommentNode.configuration.imageSize = CGSize(width: 30.0, height: 30.0)
     writeCommentNode.configuration.textContainerInset = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+    writeCommentNode.configuration.textNodeBorderWidth = 0.0
     writeCommentNode.style.preferredSize = CGSize(width: 35.0, height: 35.0)
     writeCommentNode.configuration.textNodeMinimumHeight = 30
     writeCommentNode.configuration.externalInsets = UIEdgeInsets.zero
