e5320535976bfdd50562836490e741beaa2d61b3
ibrahim <ibrahim.kteish@keeward.com>
2017-09-21 12:20:40 +0300

    Add content editor view controller

---
+++ Bookwitty/ViewControllers/Content/ContentEditorViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ContentEditorViewController.swift
+//  Bookwitty
+//
+//  Created by ibrahim on 9/21/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class ContentEditorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
