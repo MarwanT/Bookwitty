7173e1f71db59e3ae8faa22dffa36fade26e2f13
ibrahim <ibrahim.kteish@keeward.com>
2017-09-21 15:58:10 +0300

    Add the navigation items to content editor view controller navigation bar

--- Bookwitty/ViewControllers/Content/ContentEditorViewController.swift
+++ Bookwitty/ViewControllers/Content/ContentEditorViewController.swift
@@ -9,10 +9,49 @@
 import UIKit
 
 class ContentEditorViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+    loadNavigationBarButtons()
+  }
+  
+  private func loadNavigationBarButtons() {
+    navigationItem.backBarButtonItem = UIBarButtonItem.back
+    
+    let close = UIBarButtonItem(title: Strings.close(),
+                                style: UIBarButtonItemStyle.plain,
+                                target: self,
+                                action: nil)
+    
+    let drafts = UIBarButtonItem(title: Strings.drafts(),
+                                 style: UIBarButtonItemStyle.plain,
+                                 target: self,
+                                 action: nil)
+    
+    let undo = UIBarButtonItem(image: #imageLiteral(resourceName: "undo"),
+                               style: UIBarButtonItemStyle.plain,
+                               target: self,
+                               action: nil)
+    
+    let redo = UIBarButtonItem(image: #imageLiteral(resourceName: "redo"),
+                               style: UIBarButtonItemStyle.plain,
+                               target: self,
+                               action: nil)
+    let plus = UIBarButtonItem(image: #imageLiteral(resourceName: "plus"),
+                               style: UIBarButtonItemStyle.plain,
+                               target: self,
+                               action: nil)
+    
+    let next = UIBarButtonItem(title: Strings.next(),
+                                style: UIBarButtonItemStyle.plain,
+                                target: self,
+                                action: nil)
+   
+    let leftBarButtonItems = [close,drafts,undo]
+    let rightBarButtonItems = [next,plus,redo]
+    
+    navigationItem.leftBarButtonItems = leftBarButtonItems
+    navigationItem.rightBarButtonItems = rightBarButtonItems
+  }
 }
