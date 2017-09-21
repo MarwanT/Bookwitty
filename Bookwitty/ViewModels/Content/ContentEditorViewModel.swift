40920a95bdc19f6cd5d98463d8214f76e8e0e11c
ibrahim <ibrahim.kteish@keeward.com>
2017-09-21 12:24:51 +0300

    Add content editor view model files

---
+++ Bookwitty/ViewModels/Content/ContentEditorViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ContentViewModel.swift
+//  Bookwitty
+//
+//  Created by ibrahim on 9/21/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class ContentEditorViewModel  {
+
+}
