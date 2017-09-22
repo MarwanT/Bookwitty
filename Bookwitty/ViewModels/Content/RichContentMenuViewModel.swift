bd6580d5acebff90ebf482044069afcfc589776e
ibrahim <ibrahim.kteish@keeward.com>
2017-09-22 17:53:09 +0300

    Add Rich content menu view model

---
+++ Bookwitty/ViewModels/Content/RichContentMenuViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  RichContentMenuViewModel.swift
+//  Bookwitty
+//
+//  Created by ibrahim on 9/22/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+final class RichContentMenuViewModel {
+  
+}
