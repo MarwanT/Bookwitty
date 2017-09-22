abfbb5ac96e00c42f5a692d054f8b22c7e004e13
ibrahim <ibrahim.kteish@keeward.com>
2017-09-22 18:37:40 +0300

    Add item enum to rich content menu view controller

--- Bookwitty/ViewControllers/Content/RichContentMenuViewController.swift
+++ Bookwitty/ViewControllers/Content/RichContentMenuViewController.swift
@@ -10,26 +10,19 @@ import UIKit
 
 class RichContentMenuViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  enum Item {
+    case imageCamera
+    case imageLibrary
+    case link
+    case book
+    case video
+    case audio
+    case quote
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    // Do any additional setup after loading the view.
+  }
 }
