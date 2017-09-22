f81f86e7f03da564621982da76abec17af6b5e96
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-09-22 10:11:04 +0300

    Add the rich link preview controller

---
+++ Bookwitty/ViewControllers/Content/RichLinkPreviewViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RichLinkPreviewViewController.swift
+//  Bookwitty
+//
+//  Created by Charles Abou Yakzan on 2017/09/22.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class RichLinkPreviewViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+}
