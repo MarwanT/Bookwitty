7b3f38f91706198a4920a0caacd9640171ddf97d
ibrahim <ibrahim.kteish@keeward.com>
2017-09-24 19:49:22 +0300

    Add/Configure RichMenuCellTableViewCell xib and Swift file

---
+++ Bookwitty/Components/Views/Content/Editor/RichMenuCellTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  RichMenuCellTableViewCell.swift
+//  Bookwitty
+//
+//  Created by ibrahim on 9/24/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class RichMenuCellTableViewCell: UITableViewCell {
+
+  static let identifier = "RichMenuCellTableViewCellReuseIdentifier"
+  @IBOutlet weak var menuImageView: UIImageView!
+  @IBOutlet weak var menuLabel: UILabel!
+}
