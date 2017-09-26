252c692c1a97eb856873f58dc07daff2e4e253b1
ibrahim <ibrahim.kteish@keeward.com>
2017-09-26 12:43:12 +0300

    Add navigation items with their actions and text appearance for enabled state

--- Bookwitty/ViewControllers/Content/RichBookViewController.swift
+++ Bookwitty/ViewControllers/Content/RichBookViewController.swift
@@ -10,12 +10,52 @@ import UIKit
 import AsyncDisplayKit
 
 final class RichBookViewController: ASViewController<ASCollectionNode> {
-
+  
   var searchBar: UISearchBar?
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    loadNavigationBarButtons()
+    searchBar?.becomeFirstResponder()
+  }
+  
+  private func loadNavigationBarButtons() {
+    navigationItem.backBarButtonItem = UIBarButtonItem.back
+    
+    let cancel = UIBarButtonItem(title: Strings.cancel(),
+                                style: UIBarButtonItemStyle.plain,
+                                target: self,
+                                action: #selector(self.cancel(_:)))
+    cancel.tintColor = ThemeManager.shared.currentTheme.defaultButtonColor()
+    
+    let add = UIBarButtonItem(title: Strings.add(),
+                                 style: UIBarButtonItemStyle.plain,
+                                 target: self,
+                                 action: #selector(self.add(_:)))
+    setTextAppearanceState(of: add)
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-      searchBar?.becomeFirstResponder()
-    }
+    navigationItem.leftBarButtonItem = cancel
+    navigationItem.rightBarButtonItem = add
+  }
+  
+  @objc private func cancel(_ sender: UIBarButtonItem) {
+    
+  }
+  
+  @objc private func add(_ sender: UIBarButtonItem) {
+    
+  }
+  
+  @objc private func setTextAppearanceState(of barButtonItem: UIBarButtonItem) -> Void {
+    
+    var attributes = barButtonItem.titleTextAttributes(for: .normal) ?? [:]
+    //Normal
+    let defaultTextColor = ThemeManager.shared.currentTheme.defaultButtonColor()
+    attributes[NSForegroundColorAttributeName] = defaultTextColor
+    barButtonItem.setTitleTextAttributes(attributes, for: .normal)
+    //Disabled
+    let grayedTextColor = ThemeManager.shared.currentTheme.defaultGrayedTextColor()
+    attributes[NSForegroundColorAttributeName] = grayedTextColor
+    barButtonItem.setTitleTextAttributes(attributes, for: .disabled)
+  }
 }
