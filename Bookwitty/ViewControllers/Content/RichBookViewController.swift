6447591f5ca549d7a9b4c5169750dfa1dc41bbb1
ibrahim <ibrahim.kteish@keeward.com>
2017-09-26 11:05:45 +0300

    Add rich book view controller

---
+++ Bookwitty/ViewControllers/Content/RichBookViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RichBookViewController.swift
+//  Bookwitty
+//
+//  Created by ibrahim on 9/26/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+final class RichBookViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
