6760922b552b95657ece52f5e64b9582afec3190
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-09-26 13:03:27 +0300

    Add the quote editor controller

---
+++ Bookwitty/ViewControllers/Content/QuoteEditorViewController.swift
@@ -0,0 +1,18 @@
+//
+//  QuoteEditorViewController.swift
+//  Bookwitty
+//
+//  Created by Charles Abou Yakzan on 2017/09/26.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class QuoteEditorViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+}
