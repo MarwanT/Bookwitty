c9e268441735ac7163bacf262a91545f15d2c8b9
ibrahim <ibrahim.kteish@keeward.com>
2017-09-26 10:55:18 +0300

    add rich book view model

---
+++ Bookwitty/ViewModels/Content/RichBookViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  RichBookViewModel.swift
+//  Bookwitty
+//
+//  Created by ibrahim on 9/26/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+final class RichBookViewModel {
+  
+}
