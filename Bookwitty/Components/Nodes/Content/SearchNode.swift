4e831b0e1f1e841df0b11693e05f3c7ea8c21b27
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-09-27 17:00:13 +0300

    Add the search node

---
+++ Bookwitty/Components/Nodes/Content/SearchNode.swift
@@ -0,0 +1,14 @@
+//
+//  SearchNode.swift
+//  Bookwitty
+//
+//  Created by Charles Abou Yakzan on 2017/09/27.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+import AsyncDisplayKit
+
+class SearchNode: ASCellNode {
+
+}
