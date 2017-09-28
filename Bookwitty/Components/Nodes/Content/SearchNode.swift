d4ed268e33201b3ef6c71e3e7bd77d3ed80f1cb0
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-09-28 10:52:08 +0300

    Add the ui search bar to the search node

--- Bookwitty/Components/Nodes/Content/SearchNode.swift
+++ Bookwitty/Components/Nodes/Content/SearchNode.swift
@@ -10,5 +10,25 @@ import UIKit
 import AsyncDisplayKit
 
 class SearchNode: ASCellNode {
+  static let cellHeight: CGFloat = 44.0
 
+  var searchBarDelegate: UISearchBarDelegate?
+  private var searchBar: UISearchBar?
+
+  convenience override init() {
+    self.init(viewBlock: { () -> UIView in
+      let searchBar = UISearchBar()
+      return searchBar
+    })
+    style.preferredSize = CGSize(width: UIScreen.main.bounds.width, height: SearchNode.cellHeight)
+  }
+
+  override func didLoad() {
+    self.searchBar = self.view as? UISearchBar
+    self.searchBar?.delegate = searchBarDelegate
+  }
+
+  override func calculateSizeThatFits(_ constrainedSize: CGSize) -> CGSize {
+    return CGSize(width: constrainedSize.width, height: SearchNode.cellHeight)
+  }
 }
