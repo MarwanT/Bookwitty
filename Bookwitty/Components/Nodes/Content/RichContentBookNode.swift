6a27eb74ee27af4e025b1de81ff0273610935572
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-09-28 13:08:53 +0300

    Add the node elements and the layout constants

--- Bookwitty/Components/Nodes/Content/RichContentBookNode.swift
+++ Bookwitty/Components/Nodes/Content/RichContentBookNode.swift
@@ -10,5 +10,21 @@ import UIKit
 import AsyncDisplayKit
 
 class RichContentBookNode: ASCellNode {
+  fileprivate let internalMargin = ThemeManager.shared.currentTheme.cardInternalMargin()
+  fileprivate let imageSize: CGSize = CGSize(width: 60.0, height: 90.0)
 
+  private var imageNode: ASNetworkImageNode
+  private var titleNode: ASTextNode
+  private var authorNode: ASTextNode
+  private var addButton: ASButtonNode
+  private var separatorNode: ASDisplayNode
+
+  override init() {
+    imageNode = ASNetworkImageNode()
+    titleNode = ASTextNode()
+    authorNode = ASTextNode()
+    addButton = ASButtonNode()
+    separatorNode = ASDisplayNode()
+    super.init()
+  }
 }
