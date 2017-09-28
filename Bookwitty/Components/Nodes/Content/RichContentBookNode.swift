bb9dc526f3832ac5ea898d23936e156cf31ecb70
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-09-28 13:06:43 +0300

    Add the rich content book node

---
+++ Bookwitty/Components/Nodes/Content/RichContentBookNode.swift
@@ -0,0 +1,14 @@
+//
+//  RichContentBookNode.swift
+//  Bookwitty
+//
+//  Created by Charles Abou Yakzan on 2017/09/28.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+import AsyncDisplayKit
+
+class RichContentBookNode: ASCellNode {
+
+}
