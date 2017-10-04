d1c6651c5b0b4b159a2030203670c0d3c11df560
ibrahim <ibrahim.kteish@keeward.com>
2017-10-04 20:37:59 +0300

    Add Item enum to publish menu view controller

--- Bookwitty/ViewControllers/Content/PublishMenuViewController.swift
+++ Bookwitty/ViewControllers/Content/PublishMenuViewController.swift
@@ -11,6 +11,55 @@ import UIKit
 class PublishMenuViewController: UIViewController {
 
   @IBOutlet weak var tableViewHeightConstraint: NSLayoutConstraint!
+  
+  enum Item: Int {
+    case penName = 0
+    case linkTopics
+    case addTags
+    case postPreview
+    case publishYourPost
+    case saveAsDraft
+    case goBack
+    
+    var localizedString: String {
+      switch self {
+      case .penName:
+        return Strings.pen_name()
+      case .linkTopics:
+        return Strings.link_topics()
+      case .addTags:
+        return Strings.add_tags()
+      case .postPreview:
+        return Strings.post_preview()
+      case .publishYourPost:
+        return Strings.publish_your_post()
+      case .saveAsDraft:
+        return Strings.save_as_draft()
+      case .goBack:
+        return Strings.go_back()
+      }
+    }
+    
+    var image: UIImage? {
+      switch self {
+      case .penName:
+        return #imageLiteral(resourceName: "person")
+      case .linkTopics:
+        return nil
+      case .addTags:
+        return #imageLiteral(resourceName: "tag")
+      case .postPreview:
+        return #imageLiteral(resourceName: "gallery")
+      case .publishYourPost:
+        return nil
+      case .saveAsDraft:
+        return nil
+      case .goBack:
+        return nil
+      }
+    }
+  }
+  
     override func viewDidLoad() {
         super.viewDidLoad()
 
