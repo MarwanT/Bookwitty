6092b3ee39a20c77550d4f3affa8050cb3cb86eb
ibrahim <ibrahim.kteish@keeward.com>
2017-10-04 18:01:54 +0300

    Add publish table view cell (xib and swift) files

---
+++ Bookwitty/Components/Views/Content/Editor/PublishTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PulishTableViewCell.swift
+//  Bookwitty
+//
+//  Created by ibrahim on 10/4/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+final class PublishTableViewCell: UITableViewCell {
+
+}
