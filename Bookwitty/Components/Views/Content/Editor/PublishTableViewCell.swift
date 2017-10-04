b0a127b4971b93f81c6e9839ac7e0487fcee9fd9
ibrahim <ibrahim.kteish@keeward.com>
2017-10-04 18:03:22 +0300

    Link ib outlets of publish table view cell

--- Bookwitty/Components/Views/Content/Editor/PublishTableViewCell.swift
+++ Bookwitty/Components/Views/Content/Editor/PublishTableViewCell.swift
@@ -9,5 +9,10 @@
 import UIKit
 
 final class PublishTableViewCell: UITableViewCell {
-
+  @IBOutlet weak var cellImageView: UIImageView!
+  @IBOutlet weak var profileImageView: UIImageView!
+  @IBOutlet weak var userNameLabel: UILabel!
+  @IBOutlet weak var cellLabel: UILabel!
+  @IBOutlet weak var disclosureIndicatorImageView: UIImageView!
+  
 }
