cbed3774205495ae6beca6d6db398ae37bfab922
ibrahim <ibrahim.kteish@keeward.com>
2017-10-04 22:04:17 +0300

    Remove unnecessary function.

--- Bookwitty/ViewControllers/Content/PublishMenuViewController.swift
+++ Bookwitty/ViewControllers/Content/PublishMenuViewController.swift
@@ -69,9 +69,4 @@ class PublishMenuViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 }
