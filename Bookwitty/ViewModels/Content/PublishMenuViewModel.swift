66eca4351ed95dbe6c997a23852920f5eae7a465
ibrahim <ibrahim.kteish@keeward.com>
2017-10-04 11:32:40 +0300

    Add publish menu view model

---
+++ Bookwitty/ViewModels/Content/PublishMenuViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  PublishMenuViewModel.swift
+//  Bookwitty
+//
+//  Created by ibrahim on 10/4/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class PublishMenuViewModel {
+
+}
