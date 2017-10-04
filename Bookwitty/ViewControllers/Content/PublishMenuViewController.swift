edddc48a4383ed502aedab6d49324f163bcb84d6
ibrahim <ibrahim.kteish@keeward.com>
2017-10-04 11:28:02 +0300

    Add publish menu view controller swift file.

---
+++ Bookwitty/ViewControllers/Content/PublishMenuViewController.swift
@@ -0,0 +1,23 @@
+//
+//  PublishMenuViewController.swift
+//  Bookwitty
+//
+//  Created by ibrahim on 10/4/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class PublishMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
