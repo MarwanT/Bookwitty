ab6e6d8959ef6a98dcb366729d7985aa4b03482e
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-10-05 12:52:15 +0300

    Add the view model to the controller

--- Bookwitty/ViewControllers/Account/SelectPenNameViewController.swift
+++ Bookwitty/ViewControllers/Account/SelectPenNameViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class SelectPenNameViewController: UIViewController {
 
+  fileprivate let viewModel = SelectPenNameViewModel()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
 
