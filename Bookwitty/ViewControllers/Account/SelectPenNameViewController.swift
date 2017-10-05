4898394a99a677df922678ef0021635f625765b9
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-10-05 16:13:40 +0300

    Add the table view datasource and delegate

--- Bookwitty/ViewControllers/Account/SelectPenNameViewController.swift
+++ Bookwitty/ViewControllers/Account/SelectPenNameViewController.swift
@@ -25,3 +25,29 @@ class SelectPenNameViewController: UIViewController {
     tableView.tableFooterView = UIView.defaultSeparator(useAutoLayout: false)
   }
 }
+
+//MARK: - UITableViewDataSource, UITableViewDelegate implementation
+extension SelectPenNameViewController: UITableViewDataSource, UITableViewDelegate {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return viewModel.numberOfRows()
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    return tableView.dequeueReusableCell(withIdentifier: AccountPenNameTableViewCell.reuseIdentifier, for: indexPath)
+  }
+
+  func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+    let values = viewModel.values(for: indexPath.row)
+    guard let currentCell = cell as? AccountPenNameTableViewCell else {
+      return
+    }
+
+    currentCell.label.text = values.title
+    currentCell.profileImageView.sd_setImage(with: URL(string: values.imageUrl ?? ""), placeholderImage: ThemeManager.shared.currentTheme.penNamePlaceholder)
+    currentCell.disclosureIndicatorImageView.isHidden = true
+  }
+
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
+  }
+}
