e3a41d8daaf5544624df0b84b709a400a35bec06
ibrahim <ibrahim.kteish@keeward.com>
2017-10-05 12:01:19 +0300

    Add chips table view (xib and swift)

---
+++ Bookwitty/Components/Views/Content/Editor/ChipsTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ChipsTableViewCell.swift
+//  Bookwitty
+//
+//  Created by ibrahim on 10/5/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class ChipsTableViewCell: UITableViewCell {
+}
