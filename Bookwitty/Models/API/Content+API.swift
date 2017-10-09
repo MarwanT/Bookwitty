cf4abeb412d46f350e6a06f0e1415aea3bd903c7
ibrahim <ibrahim.kteish@keeward.com>
2017-10-09 18:29:20 +0300

    Add unlink content request to content api

--- Bookwitty/Models/API/Content+API.swift
+++ Bookwitty/Models/API/Content+API.swift
@@ -104,6 +104,22 @@ struct ContentAPI {
       success = statusCode == successStatusCode
     })
   }
+  
+  func unlinkContent(for contentIdentifier: String, with topicIdentifier: String, completion: @escaping (_ success: Bool, _ error: BookwittyAPIError?) -> Void) -> Cancellable? {
+    let successStatusCode = 204
+    return signedAPIRequest(target: .unlinkContent(contentIdentifier: contentIdentifier, topicIdentifier: topicIdentifier), completion: { (data, statusCode, response, error) in
+      var success: Bool = false
+      var error: BookwittyAPIError? = nil
+      defer {
+        completion(success, error)
+      }
+      guard data != nil, let statusCode = statusCode else {
+        error = BookwittyAPIError.invalidStatusCode
+        return
+      }
+      success = statusCode == successStatusCode
+    })
+  }
 }
 
 extension ContentAPI {
