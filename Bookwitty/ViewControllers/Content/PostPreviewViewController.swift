f0397d10f4c5a451d10a81fd325500b6c48433b7
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-10-09 10:27:42 +0300

    Add the basic collection node functionality

--- Bookwitty/ViewControllers/Content/PostPreviewViewController.swift
+++ Bookwitty/ViewControllers/Content/PostPreviewViewController.swift
@@ -44,3 +44,53 @@ class PostPreviewViewController: ASViewController<ASCollectionNode> {
     
   }
 }
+
+extension PostPreviewViewController: ASCollectionDataSource, ASCollectionDelegate {
+  func numberOfSections(in collectionNode: ASCollectionNode) -> Int {
+    return Sections.count
+  }
+
+  func collectionNode(_ collectionNode: ASCollectionNode, numberOfItemsInSection section: Int) -> Int {
+    guard let section = Sections(rawValue: section) else {
+      return 0
+    }
+
+    switch section {
+    case .penName:
+      return 0
+    case .cover:
+      return 0
+    case .title:
+      return 0
+    case .description:
+      return 0
+    case .characters:
+      return 0
+    case .newCover:
+      return 0
+    case .newTitle:
+      return 0
+    }
+  }
+
+  func collectionNode(_ collectionNode: ASCollectionNode, nodeBlockForItemAt indexPath: IndexPath) -> ASCellNodeBlock {
+    return {
+      return ASCellNode()
+    }
+  }
+
+  func collectionNode(_ collectionNode: ASCollectionNode, willDisplayItemWith node: ASCellNode) {
+
+  }
+
+  public func collectionNode(_ collectionNode: ASCollectionNode, constrainedSizeForItemAt indexPath: IndexPath) -> ASSizeRange {
+    return ASSizeRange(
+      min: CGSize(width: collectionNode.frame.width, height: 0),
+      max: CGSize(width: collectionNode.frame.width, height: .infinity)
+    )
+  }
+
+  func collectionNode(_ collectionNode: ASCollectionNode, didSelectItemAt indexPath: IndexPath) {
+
+  }
+}
