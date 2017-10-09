b0ebe051f72793162b0be8612ae54e77ba416a14
ibrahim <ibrahim.kteish@keeward.com>
2017-10-09 17:48:29 +0300

    Add remove tag request to tag api

--- Bookwitty/Models/API/Tag+API.swift
+++ Bookwitty/Models/API/Tag+API.swift
@@ -25,6 +25,22 @@ public struct TagAPI {
         success = statusCode == successStatusCode
       })
   }
+  
+  func removeTag(for contentIdentifier: String, with tagIdentifier: String, completion: @escaping (_ success: Bool, _ error: BookwittyAPIError?) -> Void) -> Cancellable? {
+    let successStatusCode = 204
+    return signedAPIRequest(target: .linkTag(contentIdentifier: contentIdentifier, tagIdentifier: tagIdentifier), completion: { (data, statusCode, response, error) in
+      var success: Bool = false
+      var error: BookwittyAPIError? = nil
+      defer {
+        completion(success, error)
+      }
+      guard data != nil, let statusCode = statusCode else {
+        error = BookwittyAPIError.invalidStatusCode
+        return
+      }
+      success = statusCode == successStatusCode
+    })
+  }
 }
 
 extension TagAPI {
