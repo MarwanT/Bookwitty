4dff8e8f7128dc1c91fff40cb98fd80d9944bc26
ibrahim <ibrahim.kteish@keeward.com>
2017-10-09 19:18:44 +0300

    Add remove content API request

--- Bookwitty/Models/API/Publish+API.swift
+++ Bookwitty/Models/API/Publish+API.swift
@@ -30,6 +30,23 @@ public struct PublishAPI {
       success = statusCode == successStatusCode
     })
   }
+
+  static func removeContent(contentIdentifier: String, completion: @escaping (_ success: Bool, _ error: BookwittyAPIError?) -> Void) -> Cancellable? {
+    let successStatusCode: Int = 204
+    return signedAPIRequest(target: BookwittyAPI.removeContent(contentIdentifier:contentIdentifier), completion: { (data, statusCode, response, error) in
+      var success: Bool = false
+      var error: BookwittyAPIError? = nil
+      defer {
+        completion(success, error)
+      }
+      guard data != nil, let statusCode = statusCode else {
+        error = BookwittyAPIError.invalidStatusCode
+        return
+      }
+      success = statusCode == successStatusCode
+    })
+  }
+
 }
 
 extension PublishAPI {
