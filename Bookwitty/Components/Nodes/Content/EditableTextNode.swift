082e7fe49cd37178ce85f6ea75c2a325f1a95d7c
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-10-09 19:02:08 +0300

    Add the children nodes

--- Bookwitty/Components/Nodes/Content/EditableTextNode.swift
+++ Bookwitty/Components/Nodes/Content/EditableTextNode.swift
@@ -9,5 +9,26 @@
 import AsyncDisplayKit
 
 class EditableTextNode: ASCellNode {
+  let textNode: ASEditableTextNode
+  let clearButtonNode: ASButtonNode
 
+  override init() {
+    textNode = ASEditableTextNode()
+    clearButtonNode = ASButtonNode()
+    super.init()
+    setupNode()
+  }
+
+  fileprivate func setupNode() {
+    automaticallyManagesSubnodes = true
+    textNode.style.height = ASDimension(unit: .points, value: 80.0)
+    textNode.maximumLinesToDisplay = 2
+
+    textNode.style.flexGrow = 1.0
+    textNode.style.flexShrink = 1.0
+
+    clearButtonNode.style.preferredSize = CGSize(width: 25.0, height: 25.0)
+    clearButtonNode.imageNode.imageModificationBlock = ASImageNodeRoundBorderModificationBlock(0.0, nil)
+    clearButtonNode.setImage(#imageLiteral(resourceName: "x"), for: .normal)
+  }
 }
