a95c758f694b3fd0fb869c4085bde8c5b5a1a7fd
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-10-10 16:06:19 +0300

    Add the cover photo node

---
+++ Bookwitty/Components/Nodes/Content/CoverPhotoNode.swift
@@ -0,0 +1,13 @@
+//
+//  CoverPhotoNode.swift
+//  Bookwitty
+//
+//  Created by Charles Abou Yakzan on 2017/10/10.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import AsyncDisplayKit
+
+class CoverPhotoNode: ASCellNode {
+
+}
