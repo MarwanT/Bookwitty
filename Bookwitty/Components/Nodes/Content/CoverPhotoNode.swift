5e3cc7fa9300358219246eaeb8e2b7ce44a51117
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-10-10 19:07:40 +0300

    Add the sub nodes and initialize them

--- Bookwitty/Components/Nodes/Content/CoverPhotoNode.swift
+++ Bookwitty/Components/Nodes/Content/CoverPhotoNode.swift
@@ -10,4 +10,27 @@ import AsyncDisplayKit
 
 class CoverPhotoNode: ASCellNode {
 
+  let imageNode: ASNetworkImageNode
+  let photoButton: ASButtonNode
+  let deleteButton: ASButtonNode
+
+  override init() {
+    imageNode = ASNetworkImageNode()
+    photoButton = ASButtonNode()
+    deleteButton = ASButtonNode()
+    super.init()
+    setupNode()
+  }
+
+  private func setupNode() {
+    automaticallyManagesSubnodes = true
+    imageNode.backgroundColor = UIColor.clear
+    imageNode.animatedImageRunLoopMode = RunLoopMode.defaultRunLoopMode.rawValue
+
+    photoButton.style.preferredSize = CGSize(width: 25.0, height: 25.0)
+    photoButton.clipsToBounds = true
+    deleteButton.style.preferredSize = CGSize(width: 25.0, height: 25.0)
+    deleteButton.clipsToBounds = true
+  }
+
 }
