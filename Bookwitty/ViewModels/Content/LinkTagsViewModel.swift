13c5876fa47164cd57051e121dc183169cbd2f1a
ibrahim <ibrahim.kteish@keeward.com>
2017-10-10 14:30:58 +0300

    Add link tags view model swift file.

---
+++ Bookwitty/ViewModels/Content/LinkTagsViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  LinkTagsViewModel.swift
+//  Bookwitty
+//
+//  Created by ibrahim on 10/10/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+final class LinkTagsViewModel {
+  
+}
