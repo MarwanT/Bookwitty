90b297be1de3dac1871ee1aeda98610072d79d5e
ibrahim <ibrahim.kteish@keeward.com>
2017-10-10 14:27:55 +0300

    Add tags view controller swift file.

---
+++ Bookwitty/ViewControllers/Content/LinkTagsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LinkTagsViewController.swift
+//  Bookwitty
+//
+//  Created by ibrahim on 10/10/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class LinkTagsViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
