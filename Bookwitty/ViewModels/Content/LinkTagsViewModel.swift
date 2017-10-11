d5d1295d5b0d224a506c0dd07fecc5f030c57f80
ibrahim <ibrahim.kteish@keeward.com>
2017-10-11 19:01:42 +0300

    Add selected tags property.

--- Bookwitty/ViewModels/Content/LinkTagsViewModel.swift
+++ Bookwitty/ViewModels/Content/LinkTagsViewModel.swift
@@ -11,6 +11,7 @@ import Foundation
 final class LinkTagsViewModel {
   var canLink: Bool = true
   var tags: [Tag] = []
+  var selectedTags: [Tag] = []
   let filter: Filter = Filter()
   
   init() {
