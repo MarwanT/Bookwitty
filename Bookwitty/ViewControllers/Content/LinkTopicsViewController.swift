87cba0741d2f0cd02f74a88c81e985d9b17a8d38
ibrahim <ibrahim.kteish@keeward.com>
2017-10-12 10:29:18 +0300

    Add link topics view controller.

---
+++ Bookwitty/ViewControllers/Content/LinkTopicsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LinkTopicsViewController.swift
+//  Bookwitty
+//
+//  Created by ibrahim on 10/12/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class LinkTopicsViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
