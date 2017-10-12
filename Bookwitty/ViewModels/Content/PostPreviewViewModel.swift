f29fdc2bbf538513bb65a1d1f823e80fd15f096d
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-10-12 17:02:46 +0300

    Add the post preview view model

---
+++ Bookwitty/ViewModels/Content/PostPreviewViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  PostPreviewViewModel.swift
+//  Bookwitty
+//
+//  Created by Charles Abou Yakzan on 2017/10/12.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+final class PostPreviewViewModel {
+
+}
