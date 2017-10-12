7364548d0e8f12939c762920650055945f232794
ibrahim <ibrahim.kteish@keeward.com>
2017-10-12 11:14:39 +0300

    Add keyboard notification handling for link topics view controller

--- Bookwitty/ViewControllers/Content/LinkTopicsViewController.swift
+++ Bookwitty/ViewControllers/Content/LinkTopicsViewController.swift
@@ -13,6 +13,7 @@ class LinkTopicsViewController: UIViewController {
   let viewModel = LinkTopicsViewModel()
   @IBOutlet weak var tagsView: WSTagsField!
   @IBOutlet weak var tableView: UITableView!
+  @IBOutlet weak var tableViewBottomConstraintToSuperview: NSLayoutConstraint!
   override func viewDidLoad() {
     super.viewDidLoad()
     self.initializeComponents()
@@ -29,6 +30,35 @@ class LinkTopicsViewController: UIViewController {
   
   @objc private func doneButtonTouchUpInside(_ sender:UIBarButtonItem) {
     self.dismiss(animated: true, completion: nil)
+  }
+  
+  // MARK: - Keyboard Handling
+  private func addKeyboardNotifications() {
+    NotificationCenter.default.addObserver(self,
+                                           selector: #selector(keyboardWillShow(_:)),
+                                           name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+    
+    NotificationCenter.default.addObserver(self,
+                                           selector: #selector(keyboardWillHide(_:)),
+                                           name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+  }
+  
+  func keyboardWillShow(_ notification: NSNotification) {
+    if let value = notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue {
+      let frame = value.cgRectValue
+      self.tableViewBottomConstraintToSuperview.constant = -frame.height
+    }
+    
+    UIView.animate(withDuration: 0.44) {
+      self.view.layoutSubviews()
+    }
+  }
+  
+  func keyboardWillHide(_ notification: NSNotification) {
+    self.tableViewBottomConstraintToSuperview.constant = 0
+    UIView.animate(withDuration: 0.44) {
+      self.view.layoutSubviews()
+    }
   }
 }
 
