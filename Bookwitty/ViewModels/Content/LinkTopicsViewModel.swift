5f4dc3fb3b93caca0cde737bc592e802f4b7dc43
ibrahim <ibrahim.kteish@keeward.com>
2017-10-12 10:30:17 +0300

    Add link topics view model

---
+++ Bookwitty/ViewModels/Content/LinkTopicsViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  LinkTopicsViewModel.swift
+//  Bookwitty
+//
+//  Created by ibrahim on 10/12/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+final class LinkTopicsViewModel {
+  
+}
