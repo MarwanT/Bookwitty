ab786fca2e3daa1085ee61a5f139d99309f32fa6
ibrahim <ibrahim.kteish@keeward.com>
2017-10-12 19:57:55 +0300

    Add delegate property of tyoe topic view controller delegate to the topic view controller

--- Bookwitty/ViewControllers/Details/TopicViewController.swift
+++ Bookwitty/ViewControllers/Details/TopicViewController.swift
@@ -61,7 +61,7 @@ class TopicViewController: ASViewController<ASCollectionNode> {
   fileprivate var loadingStatus: LoadingStatus = .none
 
   var navigationItemMode: NavigationItemMode = .view
-
+  weak var delegate: TopicViewControllerDelegate?
   required init?(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
