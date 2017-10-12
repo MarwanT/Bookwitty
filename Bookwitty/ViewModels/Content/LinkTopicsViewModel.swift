81002552af31846f0f454e5707ff0c57328b5748
ibrahim <ibrahim.kteish@keeward.com>
2017-10-12 11:00:53 +0300

    Implement link topics view model

--- Bookwitty/ViewModels/Content/LinkTopicsViewModel.swift
+++ Bookwitty/ViewModels/Content/LinkTopicsViewModel.swift
@@ -9,5 +9,29 @@
 import Foundation
 
 final class LinkTopicsViewModel {
+  var canLink: Bool = true
+  var topics: [Topic] = []
+  var selectedTopics: [Topic] = []
+  let filter: Filter = Filter()
   
+  init() {
+    self.filter.types = [Topic.resourceType]
+  }
+  
+  func append(_ topic: Topic) {
+    if !selectedTopics.contains(topic) {
+      selectedTopics.append(topic)
+    }
+  }
+}
+// Mark: - TableView helper
+extension LinkTopicsViewModel {
+  func numberOfItemsInSection(section: Int) -> Int {
+    return topics.count
+  }
+  func values(forRowAt indexPath: IndexPath) -> String? {
+    guard topics.count > indexPath.row else { return nil }
+    let resourceId = topics[indexPath.row]
+    return resourceId.title
+  }
 }
