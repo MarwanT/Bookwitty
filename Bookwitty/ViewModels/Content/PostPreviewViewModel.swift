ef8d8778cdc848125b309be682d535c51f56eb4f
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-10-13 15:55:02 +0300

    Add the upload image function to the view model

--- Bookwitty/ViewModels/Content/PostPreviewViewModel.swift
+++ Bookwitty/ViewModels/Content/PostPreviewViewModel.swift
@@ -14,4 +14,26 @@ final class PostPreviewViewModel {
   func initialize(with post: CandidatePost) {
     self.candidatePost = post
   }
+
+  func upload(image: UIImage?, completion: @escaping (_ success: Bool, _ imageId: String?) -> Void) {
+    guard let image = image, let data = image.dataForPNGRepresentation() else {
+      completion(false, nil)
+      return
+    }
+
+    let fileName = UUID().uuidString
+    _ = UploadAPI.uploadPolicy(file: (fileName, size: data.count), fileType: UploadAPI.FileType.image, assetType: UploadAPI.AssetType.inline) {
+      (success, policy, error) in
+      guard success, let policy = policy, let url = URL(string: policy.uploadUrl ?? "") else {
+        completion(false, nil)
+        return
+      }
+
+      let parameters: [String : String] = (policy.form as? [String : String]) ?? [:]
+      _ = UtilitiesAPI.upload(url: url, paramters: parameters, multipart: (data: data, name: "file"), completion: {
+        (success, error) in
+        completion(success, policy.link)
+      })
+    }
+  }
 }
