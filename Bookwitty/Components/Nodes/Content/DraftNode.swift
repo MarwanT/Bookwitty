10f0241da9976fb732d26a90b84fec4c0845a2d2
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-10-16 18:46:18 +0300

    Add the draft node

---
+++ Bookwitty/Components/Nodes/Content/DraftNode.swift
@@ -0,0 +1,13 @@
+//
+//  DraftNode.swift
+//  Bookwitty
+//
+//  Created by Charles Abou Yakzan on 2017/10/16.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import AsyncDisplayKit
+
+class DraftNode: ASCellNode {
+  
+}
