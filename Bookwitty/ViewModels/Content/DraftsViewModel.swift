156583c4f8efc7f7a1bdb9a1f544544a90fdfcd0
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-10-16 12:15:01 +0300

    Add the drafts view model

---
+++ Bookwitty/ViewModels/Content/DraftsViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  DraftsViewModel.swift
+//  Bookwitty
+//
+//  Created by Charles Abou Yakzan on 2017/10/16.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+final class DraftsViewModel {
+
+}
