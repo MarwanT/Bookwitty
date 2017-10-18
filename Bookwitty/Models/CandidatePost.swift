cf80f0ae2cedcc5145f9743b21f16a6f76d313a7
ibrahim <ibrahim.kteish@keeward.com>
2017-10-18 09:42:47 +0300

    Update candidate post protocol.

--- Bookwitty/Models/CandidatePost.swift
+++ Bookwitty/Models/CandidatePost.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 protocol CandidatePost {
+  var id: String? { get }
   var title: String? { get set }
   var body: String? { get set }
   var shortDescription: String? { get set }
   var imageUrl: String? { get set }
+  var tags: [Tag]? { get set }
+  var titleBodyHashValue: Int { get }
+  var penName: PenName? { get set }
+
+}
+
+extension CandidatePost {
+  var titleBodyHashValue: Int {
+    return (title?.hashValue ?? 0)
+      + (body?.hashValue ?? 0)
+  }
 }
 
 extension Text: CandidatePost {
