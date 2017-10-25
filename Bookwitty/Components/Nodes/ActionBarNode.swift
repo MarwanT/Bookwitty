8756c453792ccaa0fcb7308a116f84f3e6a40352
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-10-25 12:14:30 +0300

    Add the configuration for the node

--- Bookwitty/Components/Nodes/ActionBarNode.swift
+++ Bookwitty/Components/Nodes/ActionBarNode.swift
@@ -13,6 +13,12 @@ class ActionBarNode: ASCellNode {
   let editButton = ASButtonNode()
   let moreButton = ASButtonNode()
 
+  var configuration = Configuration() {
+    didSet {
+      setNeedsLayout()
+    }
+  }
+
   override init() {
     super.init()
     initializeComponents()
@@ -20,5 +26,22 @@ class ActionBarNode: ASCellNode {
 
   fileprivate func initializeComponents() {
     automaticallyManagesSubnodes = true
+  }
+}
+
+extension ActionBarNode {
+  struct Configuration {
+    var internalSpacing = ThemeManager.shared.currentTheme.contentSpacing() / 4.0
+    var insets: UIEdgeInsets {
+      return UIEdgeInsets(top: internalSpacing - 1.0, left: 2 * internalSpacing, bottom: internalSpacing - 1.0, right: 2 * internalSpacing)
+    }
+
+    var edgeInsets: UIEdgeInsets {
+      return UIEdgeInsets(top: 0.0, left: 2 * internalSpacing, bottom: 0.0, right: 2 * internalSpacing)
+    }
+
+    var height: CGFloat = 50.0
+    var buttonSize: CGSize = CGSize(width: 36.0, height: 36.0)
+    var iconSize: CGSize = CGSize(width: 40.0, height: 40.0)
   }
 }
