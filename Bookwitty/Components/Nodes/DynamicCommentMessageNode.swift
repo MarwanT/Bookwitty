efa29248919bc79f7b35a2b904d2ff90996ca189
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-11-08 16:38:07 +0200

    Add Dynamic Comment Message Node

---
+++ Bookwitty/Components/Nodes/DynamicCommentMessageNode.swift
@@ -0,0 +1,14 @@
+//
+//  DynamicCommentMessageNode.swift
+//  Bookwitty
+//
+//  Created by Marwan  on 11/8/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import AsyncDisplayKit
+import Foundation
+
+class DynamicCommentMessageNode: ASCellNode {
+  
+}
