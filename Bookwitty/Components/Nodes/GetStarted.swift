ce9643455e7d4f853dc950d3df2e831657a94561
Charles Abou Yakzan <charles.abouyakzan@keeward.com>
2017-11-22 21:42:06 +0200

    Add the configuration

--- Bookwitty/Components/Nodes/GetStarted.swift
+++ Bookwitty/Components/Nodes/GetStarted.swift
@@ -10,6 +10,12 @@ import AsyncDisplayKit
 
 class GetStarted: ASDisplayNode {
 
+  var configuration = Configuration() {
+    didSet {
+      setNeedsLayout()
+    }
+  }
+  
   override init() {
     super.init()
     initializeComponents()
@@ -25,5 +31,12 @@ class GetStarted: ASDisplayNode {
 extension GetStarted: Themeable {
   func applyTheme() {
     //TODO: Empty Implementation
+  }
+}
+
+//MARK: - Configuration Declaration
+extension GetStarted {
+  struct Configuration {
+    let backgroundColor = ThemeManager.shared.currentTheme.defaultBackgroundColor()
   }
 }
