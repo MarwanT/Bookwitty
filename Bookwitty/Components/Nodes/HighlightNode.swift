d82e6d0d65875a44f060797f1a5a82858aa19346
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-11-23 14:42:25 +0200

    Declare and Define HighlightNode
    It's main functionality that it animates the fade out of its color

--- Bookwitty/Components/Nodes/HighlightNode.swift
+++ Bookwitty/Components/Nodes/HighlightNode.swift
@@ -6,4 +6,36 @@
 //  Copyright © 2017 Keeward. All rights reserved.
 //
 
-import Foundation
+import AsyncDisplayKit
+
+class HighlightNode: ASDisplayNode {
+  var highlightColor: UIColor = ThemeManager.shared.currentTheme.colorNumber2() {
+    didSet {
+      backgroundColor = highlightColor
+    }
+  }
+  var coolDownColor: UIColor = UIColor.white
+  var animationDuration: TimeInterval = 0.60
+  
+  fileprivate var didFinishCoolDown: (() -> Void)?
+  
+  override init() {
+    super.init()
+    backgroundColor = highlightColor
+  }
+  
+  func startCoolDown(completion: (() -> Void)?) {
+    didFinishCoolDown = completion
+    transitionLayout(withAnimation: true, shouldMeasureAsync: false, measurementCompletion: nil)
+  }
+  
+  override func animateLayoutTransition(_ context: ASContextTransitioning) {
+    UIView.animate(withDuration: animationDuration, animations: {
+      self.backgroundColor = self.coolDownColor
+    }) { (success) in
+      context.completeTransition(true)
+      self.removeFromSupernode()
+      self.didFinishCoolDown?()
+    }
+  }
+}
