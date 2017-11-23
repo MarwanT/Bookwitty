089919346ef3ce91df3a2912dca01a2f907f6244
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-11-23 19:22:49 +0200

    Declare configuration property

--- Bookwitty/Components/Nodes/CommentsCountCellNode.swift
+++ Bookwitty/Components/Nodes/CommentsCountCellNode.swift
@@ -9,6 +9,16 @@
 import AsyncDisplayKit
 
 class CommentsCountCellNode: ASCellNode {
+  var configuration = Configuration() {
+    didSet {
+      refreshNode()
+    }
+  }
+  
+  // MARK: HELPERS
+  //==============
+  private func refreshNode() {
+  }
 }
 
 // MARK: - Configuration
