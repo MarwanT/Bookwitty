f2e9e4955a1c6a1c5886749ccfd75fee71fcbe88
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-11-28 13:12:20 +0200

    Add property to Comments Manager for fetching an excerpt string of the related resource

--- Bookwitty/Models/Managers/CommentsManager.swift
+++ Bookwitty/Models/Managers/CommentsManager.swift
@@ -31,6 +31,10 @@ class CommentsManager {
     self.nextPageURL = nextPageURL
   }
   
+  var resourceExcerpt: String? {
+    return resource?.title ?? resource?.shortDescription
+  }
+  
   var postIdentifier: String? {
     return resource?.id
   }
