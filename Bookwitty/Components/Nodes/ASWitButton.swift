e4c8af24b5f0dd276f5fa0113f8ec290d0be4bad
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-11-29 12:05:03 +0200

    Set the wit button theme for its different display modes

--- Bookwitty/Components/Nodes/ASWitButton.swift
+++ Bookwitty/Components/Nodes/ASWitButton.swift
@@ -13,6 +13,50 @@ class ASWitButton: ASButtonNode {
   fileprivate(set) var displayMode: DisplayMode = .lightWeight
 }
 
+// MARK: - THEMEABLE
+                                     //****\\
+extension ASWitButton {
+  func applyTheme() {
+    let theme = ThemeManager.shared.currentTheme
+    switch displayMode {
+    case .lightWeight:
+      let buttonFont = configuration.font
+      let textColor = theme.defaultGrayedTextColor()
+      let selectedTextColor = theme.defaultButtonColor()
+      
+      style.preferredSize.height = configuration.height
+      style.preferredSize.width = 50.0
+      
+      setTitle(Strings.wit_it(), with: buttonFont, with: textColor, for: .normal)
+      setTitle(Strings.witted(), with: buttonFont, with: selectedTextColor, for: .selected)
+      contentHorizontalAlignment = ASHorizontalAlignment.right
+      
+      cornerRadius = 0
+      borderWidth = 0
+    case .heavyWeight:
+      let buttonFont = configuration.font
+      let buttonBackgroundImage = theme.defaultBackgroundColor().image()
+      let selectedButtonBackgroundImage = theme.defaultButtonColor().image()
+      let textColor = theme.defaultButtonColor()
+      let selectedTextColor = theme.colorNumber23()
+      
+      style.preferredSize.height = configuration.height
+      style.preferredSize.width = 75.0
+      
+      setBackgroundImage(buttonBackgroundImage, for: .normal)
+      setBackgroundImage(selectedButtonBackgroundImage, for: .selected)
+      
+      setTitle(Strings.wit_it(), with: buttonFont, with: textColor, for: .normal)
+      setTitle(Strings.witted(), with: buttonFont, with: selectedTextColor, for: .selected)
+      
+      cornerRadius = 2.0
+      borderColor = theme.defaultButtonColor().cgColor
+      borderWidth = 2
+      clipsToBounds = true
+    }
+  }
+}
+
 // MARK: - DISPLAY MODE
                                    //****\\
 extension ASWitButton {
