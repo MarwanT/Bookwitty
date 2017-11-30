d5c2e4f4943765ed7d8832e99a5655a7640dce83
ibrahim <ibrahim.kteish@keeward.com>
2017-11-30 18:34:01 +0200

    Fix the target of the tap gesture. Remove unnecessary  assignment.

--- Bookwitty/ViewControllers/Content/RichContentMenuViewController.swift
+++ Bookwitty/ViewControllers/Content/RichContentMenuViewController.swift
@@ -81,11 +81,12 @@ class RichContentMenuViewController: UIViewController {
   }
   
   private func addTapGesture() {
-    let tapGesture = UITapGestureRecognizer(target: self, action: #selector(PublishMenuViewController.backgroundTapped(_:)))
+    let tapGesture = UITapGestureRecognizer(target: self, action: #selector(RichContentMenuViewController
+      .backgroundTapped(_:)))
+    self.view.isUserInteractionEnabled = true
     tapGesture.delegate = self
     tapGesture.numberOfTapsRequired = 1
     tapGesture.numberOfTouchesRequired = 1
-    tapGesture.cancelsTouchesInView = false
     self.view.addGestureRecognizer(tapGesture)
   }
   
