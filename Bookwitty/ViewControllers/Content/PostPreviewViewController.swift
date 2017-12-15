1b5267a3aabe62c8646202abb2c21a9436a95e89
ibrahim <ibrahim.kteish@keeward.com>
2017-12-15 15:59:22 +0200

    Only assign the short description if it is not empty or nil.

--- Bookwitty/ViewControllers/Content/PostPreviewViewController.swift
+++ Bookwitty/ViewControllers/Content/PostPreviewViewController.swift
@@ -350,7 +350,11 @@ extension PostPreviewViewController: EditableTextNodeDelegate {
 
 extension PostPreviewViewController: LimitedEditableTextNodeDelegate {
   func limitedEditableTextNodeDidFinishEditing(textNode: LimitedEditableTextNode) {
-    viewModel.candidatePost.shortDescription = textNode.contentText
+    
+    let shortDescription = textNode.contentText
+    if !shortDescription.isEmptyOrNil() {
+      viewModel.candidatePost.shortDescription = shortDescription
+    }
   }
 }
 
