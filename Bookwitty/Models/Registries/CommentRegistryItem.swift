ba7a621e29c1d75d0f16b9885a9ca49e240f592f
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-12-19 13:43:35 +0200

    Add Comment Registry Class

---
+++ Bookwitty/Models/Registries/CommentRegistryItem.swift
@@ -0,0 +1,12 @@
+//
+//  CommentRegistryItem.swift
+//  Bookwitty
+//
+//  Created by Marwan  on 12/19/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+class CommentRegistryItem {
+}
