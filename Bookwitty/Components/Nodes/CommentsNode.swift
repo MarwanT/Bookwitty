6799f0ef33d286313d98ee4e8edb791631a14c55
Marwan Toutounji <marwan.toutounji@keeward.com>
2018-01-05 13:30:27 +0200

    Fix typo

--- Bookwitty/Components/Nodes/CommentsNode.swift
+++ Bookwitty/Components/Nodes/CommentsNode.swift
@@ -691,7 +691,7 @@ extension CommentsNode {
   }
 }
 
-// MARK: - Comment instences related methods
+// MARK: - Comment instances related methods
 extension CommentsNode {
   func publishComment(content: String?, parentCommentIdentifier: String?, completion: @escaping (_ success: Bool, _ error: CommentsManager.Error?) -> Void) {
     viewModel.publishComment(content: content, parentCommentIdentifier: parentCommentIdentifier) {
