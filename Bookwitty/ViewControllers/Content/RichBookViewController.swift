b34b54544252c62984e1565f1eb913c551db75a8
Marwan Toutounji <marwan.toutounji@keeward.com>
2018-01-25 13:48:45 +0200

    Remove rich book VC as observer

--- Bookwitty/ViewControllers/Content/RichBookViewController.swift
+++ Bookwitty/ViewControllers/Content/RichBookViewController.swift
@@ -100,6 +100,10 @@ final class RichBookViewController: ASViewController<ASDisplayNode> {
     fatalError("init(coder:) has not been implemented")
   }
   
+  deinit {
+    NotificationCenter.default.removeObserver(self)
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     observeLanguageChanges()
