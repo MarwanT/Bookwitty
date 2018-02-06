142dd1669e64675e99b6db6ca039b3ec2b6e8988
Marwan Toutounji <marwan.toutounji@keeward.com>
2018-02-06 14:37:07 +0200

    Implement the image resizing process in the Image Resizing Operation class

--- Bookwitty/Utilities/ImageResizingOperation.swift
+++ Bookwitty/Utilities/ImageResizingOperation.swift
@@ -8,7 +8,24 @@
 import UIKit
 
 class ImageResizingOperation: Operation {
+  fileprivate(set) var image: UIImage?
+  let resizingCriteria: ResizingCriteria
   
+  init(image: UIImage, resizingCriteria: ResizingCriteria) {
+    self.image = image
+    self.resizingCriteria = resizingCriteria
+  }
+  
+  override func main() {
+    guard !isCancelled else {
+      return
+    }
+    
+    switch resizingCriteria {
+    case .maximumDataCount(let dataCount):
+      image = image?.resize(maximumDataCount: dataCount)
+    }
+  }
 }
 
 extension ImageResizingOperation {
