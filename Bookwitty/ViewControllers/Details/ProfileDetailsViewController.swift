1718b9ae1948805b6b2923d635a3f413bcd2e46c
Shafic Hariri <shafic.hariri@keeward.com>
2018-02-21 17:38:08 +0200

    PenName Details: Add the Pop VC action to the reported resource result

--- Bookwitty/ViewControllers/Details/ProfileDetailsViewController.swift
+++ Bookwitty/ViewControllers/Details/ProfileDetailsViewController.swift
@@ -214,12 +214,15 @@ extension ProfileDetailsViewController: PenNameFollowNodeDelegate {
     
     let penNameIdentifier: String
     let penNameResource: PenName
+    let shouldPopController: Bool
+
     if penNameHeaderNode === node {
       guard let identifier = viewModel.penName.id else {
         return
       }
       penNameIdentifier = identifier
       penNameResource = viewModel.penName
+      shouldPopController = true
     } else {
       guard let indexPath = collectionNode.indexPath(for: node),
         let resource = viewModel.resourceForIndex(indexPath: indexPath, segment: activeSegment),
@@ -229,12 +232,15 @@ extension ProfileDetailsViewController: PenNameFollowNodeDelegate {
       }
       penNameIdentifier = identifier
       penNameResource = penName
+      shouldPopController = false
     }
 
     let actions: [MoreAction] = MoreAction.actions(for: penNameResource as? ModelCommonProperties)
     self.showMoreActionSheet(identifier: penNameIdentifier, actions: actions, completion: {
       (success: Bool, action: MoreAction) in
-
+      if case MoreAction.report(.penName) = action, shouldPopController {
+        self.navigationController?.popViewController(animated: true)
+      }
     })
   }
 }
