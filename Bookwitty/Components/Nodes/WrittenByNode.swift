51dbc3d9ab1ee11a00ea1cb15a1810f4d986b42d
Shafic Hariri <shafic.hariri@keeward.com>
2018-02-26 09:15:06 +0200

    Setup the following button (Without its delegate)

--- Bookwitty/Components/Nodes/WrittenByNode.swift
+++ Bookwitty/Components/Nodes/WrittenByNode.swift
@@ -10,6 +10,7 @@ import Foundation
 import AsyncDisplayKit
 
 class WrittenByNode: ASCellNode {
+  private let buttonSize: CGSize = CGSize(width: 36.0, height: 36.0)
   let titleNode: ASTextNode
   let titleSeparatorNode: ASDisplayNode
   let headerNode: CardPostInfoNode
@@ -44,7 +45,21 @@ class WrittenByNode: ASCellNode {
   }
 
   private func setup() {
-    //TODO: Setup the node
+    let buttonFont = FontDynamicType.subheadline.font
+    let textColor = ThemeManager.shared.currentTheme.defaultButtonColor()
+    let selectedTextColor = ThemeManager.shared.currentTheme.colorNumber23()
+
+    followButton.setupSelectionButton(defaultBackgroundColor: ThemeManager.shared.currentTheme.defaultBackgroundColor(),
+                                      selectedBackgroundColor: ThemeManager.shared.currentTheme.defaultButtonColor(),
+                                      borderStroke: true,
+                                      borderColor: ThemeManager.shared.currentTheme.defaultButtonColor(),
+                                      borderWidth: 2.0,
+                                      cornerRadius: 2.0)
+
+    followButton.setTitle(title: Strings.follow(), with: buttonFont, with: textColor, for: .normal)
+    followButton.setTitle(title: Strings.following(), with: buttonFont, with: selectedTextColor, for: .selected)
+    followButton.state = self.following ? .selected : .normal
+    followButton.style.height = ASDimensionMake(buttonSize.height)
   }
 
   override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
