5a3a8a95329c7aada6381d3a48008f690c084e6d
Shafic Hariri <shafic.hariri@keeward.com>
2018-02-28 16:13:53 +0200

    Initialize StatefulNode with the expected properties

--- Bookwitty/Components/Nodes/StatefulNode.swift
+++ Bookwitty/Components/Nodes/StatefulNode.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+import AsyncDisplayKit
+
+class StatefulNode: ASCellNode {
+  let captionNode: ASTextNode
+  let actionNode: ASTextNode
+  let illustrationNode: ASImageNode
+  let misfortuneNode: MisfortuneNode
+
+  override init() {
+    captionNode = ASTextNode()
+    actionNode = ASTextNode()
+    illustrationNode = ASImageNode()
+    misfortuneNode = MisfortuneNode(mode: MisfortuneNode.Mode.empty)
+    super.init()
+  }
+}
