5ba61ed6996c18033e60c6fec4f054a07fad7626
Marwan Toutounji <marwan.toutounji@keeward.com>
2018-03-01 19:34:34 +0200

    Remove link tag api implementation

--- Bookwitty/Models/API/Tag+API.swift
+++ Bookwitty/Models/API/Tag+API.swift
@@ -10,22 +10,6 @@ import Moya
 
 public struct TagAPI {
   
-  static func linkTag(for contentIdentifier: String, tagIdentifier: String, tagTitle: String, completion: @escaping (_ success: Bool, _ error: BookwittyAPIError?) -> Void) -> Cancellable? {
-    let successStatusCode = 201
-    return signedAPIRequest(target: .linkTag(contentIdentifier: contentIdentifier, tagIdentifier: tagIdentifier, tagTitle: tagTitle), completion: { (data, statusCode, response, error) in
-        var success: Bool = false
-        var error: BookwittyAPIError? = nil
-        defer {
-          completion(success, error)
-        }
-        guard data != nil, let statusCode = statusCode else {
-          error = BookwittyAPIError.invalidStatusCode
-          return
-        }
-        success = statusCode == successStatusCode
-      })
-  }
-  
   static func removeTag(for contentIdentifier: String, with tagIdentifier: String, completion: @escaping (_ success: Bool, _ error: BookwittyAPIError?) -> Void) -> Cancellable? {
     let successStatusCode = 204
     return signedAPIRequest(target: .removeTag(contentIdentifier: contentIdentifier, tagIdentifier: tagIdentifier), completion: { (data, statusCode, response, error) in
