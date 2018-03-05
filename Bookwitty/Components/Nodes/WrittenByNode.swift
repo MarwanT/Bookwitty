1f41d120ae6729ac4760a511b31c63955496e55b
Shafic Hariri <shafic.hariri@keeward.com>
2018-03-05 13:04:46 +0200

    Fix: biography text to work with the characters limit and more font

--- Bookwitty/Components/Nodes/WrittenByNode.swift
+++ Bookwitty/Components/Nodes/WrittenByNode.swift
@@ -28,8 +28,10 @@ class WrittenByNode: ASCellNode {
   var biography: String? {
     didSet {
       if let biography = biography {
-        biographyNode.attributedText = AttributedStringBuilder(fontDynamicType: .body3)
-          .append(text: biography, color: ThemeManager.shared.currentTheme.defaultTextColor()).attributedString
+        biographyNode.setString(text: biography,
+                                fontDynamicType: .body3,
+                                moreFontDynamicType: .title5,
+                                color: ThemeManager.shared.currentTheme.defaultTextColor())
         setNeedsLayout()
       }
     }
