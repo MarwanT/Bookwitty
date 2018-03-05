dd5d4d71fe58953b5c5d364cc1ccdc059302f85e
Marwan Toutounji <marwan.toutounji@keeward.com>
2018-03-05 14:02:45 +0200

    Add a configuration struct to Chips Table View Cell

--- Bookwitty/Components/Views/Content/Editor/ChipsTableViewCell.swift
+++ Bookwitty/Components/Views/Content/Editor/ChipsTableViewCell.swift
@@ -11,6 +11,8 @@ import WSTagsField
 class ChipsTableViewCell: UITableViewCell {
   @IBOutlet weak var cellImageView: UIImageView!
   @IBOutlet weak var tagsView: WSTagsField!
+  
+  var configuration = Configuration()
 
   static let identifier = "ChipsTableViewCellReuseableIdentifier"
   override func awakeFromNib() {
@@ -33,5 +35,32 @@ extension ChipsTableViewCell: Themeable {
     tagsView.selectedTextColor = theme.colorNumber23()
     tagsView.font = FontDynamicType.caption3.font
     tagsView.padding.left = 0
+  }
+}
+
+extension ChipsTableViewCell {
+  struct Configuration {
+    let theme = ThemeManager.shared.currentTheme
+    var tagsTintColor: UIColor
+    var tagsTextColor: UIColor
+    var tagsSelectedColor: UIColor
+    var tagsSelectedTextColor: UIColor
+    var maximumTags: Int
+    var moreTagTitle: String
+    var moreTagBackgroundColor: UIColor
+    var moreTagBorderColor: UIColor
+    var moreTagTextColor: UIColor
+    
+    init() {
+      tagsTintColor = theme.colorNumber9()
+      tagsTextColor = theme.colorNumber20()
+      tagsSelectedColor = theme.colorNumber25()
+      tagsSelectedTextColor = theme.colorNumber23()
+      maximumTags = 4
+      moreTagTitle = Strings.more()
+      moreTagBackgroundColor = UIColor.white
+      moreTagBorderColor = theme.colorNumber25()
+      moreTagTextColor = theme.colorNumber25()
+    }
   }
 }
