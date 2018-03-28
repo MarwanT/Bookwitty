bb60e1eab17d0726362a544a192885f0d5f33aea
Shafic Hariri <shafic.hariri@keeward.com>
2018-03-28 18:35:45 +0300

    Create and Add TagCard post ViewModel file

---
+++ Bookwitty/ViewModels/Cards/TagCardViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TagCardViewModel.swift
+//  Bookwitty
+//
+//  Created by Shafic Hariri on 3/28/18.
+//  Copyright © 2018 Keeward. All rights reserved.
+//
+
+import Foundation
+
+class TagCardViewModel {
+  
+}
