b8114e146e1d0a9855c36fe6b13b4e83557e0f78
Shafic Hariri <shafic.hariri@keeward.com>
2018-03-28 18:14:22 +0300

    Add tag card post cell file to the project

---
+++ Bookwitty/Components/Nodes/TagCardPostCellNode.swift
@@ -0,0 +1,13 @@
+//
+//  TagCardPostCellNode.swift
+//  Bookwitty
+//
+//  Created by Shafic Hariri on 3/28/18.
+//  Copyright © 2018 Keeward. All rights reserved.
+//
+
+import Foundation
+
+class TagCardPostCellNode {
+
+}
