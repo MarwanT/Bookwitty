e51bda5cbe63ddbae99f99a699cdde9778589493
Shafic Hariri <shafic.hariri@keeward.com>
2018-03-28 20:07:41 +0300

    Develop the TagCard View model logic

--- Bookwitty/ViewModels/Cards/TagCardViewModel.swift
+++ Bookwitty/ViewModels/Cards/TagCardViewModel.swift
@@ -8,6 +8,33 @@
 
 import Foundation
 
-class TagCardViewModel {
-  
+protocol TagCardViewModelDelegate: class {
+  func resourceUpdated(viewModel: TagCardViewModel)
+}
+
+class TagCardViewModel: CardViewModelProtocol {
+  var resource: ModelCommonProperties? {
+    didSet {
+      notifyChange()
+    }
+  }
+
+  weak var delegate: TagCardViewModelDelegate?
+
+  private func notifyChange() {
+    delegate?.resourceUpdated(viewModel: self)
+  }
+
+  func values() -> (title: String?, following: Bool, followers: Int?, reported: Reported) {
+    guard let resource = resource, let tag = resource as? Tag else {
+      return (nil, false, nil, .not)
+    }
+
+    let name = tag.title
+    let following = resource.following
+    let followers = resource.counts?.followers
+    let reported: Reported = DataManager.shared.isReported(resource as? ModelResource)
+
+    return (name, following, followers, reported)
+  }
 }
