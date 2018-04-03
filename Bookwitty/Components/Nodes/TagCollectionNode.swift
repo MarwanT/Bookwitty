923b696b1540bff67f9b86b4a8a832e17fd5c5d4
Marwan Toutounji <marwan.toutounji@keeward.com>
2018-04-03 14:20:03 +0300

    Deselect tags automatically after selection

--- Bookwitty/Components/Nodes/TagCollectionNode.swift
+++ Bookwitty/Components/Nodes/TagCollectionNode.swift
@@ -22,6 +22,8 @@ class TagCollectionNode: ASCellNode {
   let imageNode: ASImageNode
   let collectionNode: ASCollectionNode
   let flowLayout: UICollectionViewFlowLayout
+  
+  var deselectAutomatically: Bool = true
 
   var delegate: TagCollectionNodeDelegate?
 
@@ -98,5 +100,10 @@ extension TagCollectionNode: ASCollectionDataSource, ASCollectionDelegate {
 
   func collectionNode(_ collectionNode: ASCollectionNode, didSelectItemAt indexPath: IndexPath) {
     delegate?.tagCollection(node: self, didSelectItemAt: indexPath.item)
+    if deselectAutomatically {
+      DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(500)) {
+        collectionNode.deselectItem(at: indexPath, animated: false)
+      }
+    }
   }
 }
